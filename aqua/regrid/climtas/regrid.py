1eb5feac8c6afafc145686089b91fef920bf8fe1
Jost von Hardenberg <jost.hardenberg@polito.it>
2023-01-28 22:50:11 +0100

    fixed ifs case

--- aqua/regrid/climtas/regrid.py
+++ aqua/regrid/climtas/regrid.py
@@ -362,7 +362,7 @@ def apply_weights(source_data, weights, weights_matrix=None):
     #    )
 
     # Find dimensions to keep
-    nd = sum([(d not in ['i', 'j', 'x', 'y', 'lon', 'lat', 'longitude', 'latitude', 'cell', 'cells', 'ncells', 'values']) for d in source_data.dims])
+    nd = sum([(d not in ['i', 'j', 'x', 'y', 'lon', 'lat', 'longitude', 'latitude', 'cell', 'cells', 'ncells', 'values', 'value']) for d in source_data.dims])
 
     kept_shape = list(source_data.shape[0:nd])
     kept_dims = list(source_data.dims[0:nd])
