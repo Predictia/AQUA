3f58c9bc8bb9b2f8a6181f93c1e06127716efed8
Jost von Hardenberg <jost.hardenberg@polito.it>
2023-02-05 09:41:17 +0100

    temp branch for reader devel

--- aqua/reader/reader.py
+++ aqua/reader/reader.py
@@ -1,6 +1,7 @@
 import intake
 import xarray as xr
 import os
+from metpy.units import units
 import smmregrid as rg
 from aqua.util import load_yaml
 
@@ -47,16 +48,83 @@ class Reader():
                 print("Success!")
 
             self.regridder = rg.Regridder(weights=self.weights)
+
                
-    def retrieve(self, regrid=False):
+    def retrieve(self, regrid=False, fix=True):
         if self.zoom:
             data = self.cat[self.model][self.exp][self.source](zoom=self.zoom).to_dask()
         else:
             data = self.cat[self.model][self.exp][self.source].to_dask()
         if self.targetgrid and regrid:
             data = self.regridder.regrid(data)
+        if fix:
+            data = self.fixer(data)
         return data
 
     def regrid(self, data):
-        return self.regridder.regrid(data)
+        # save attributes (not conserved by smmregrid)
+        out = self.regridder.regrid(data)
+        out.attrs = data.attrs
+        return out
+    
+    def fixer(self, data):
+        fixes = load_yaml("config/fixes.yaml")
+        model=self.model
 
+        fix = fixes["models"].get(model, None)
+        if not fix:
+            print("No fixes defined for model ", model)
+            return data
+
+        self.deltat = fix.get("deltat", 1)
+        fixd = {}
+        allvars = data.variables
+        for var in fix["vars"]:
+                    shortname = fix["vars"][var]["short_name"]
+                    if var in allvars: 
+                        fixd.update({f"{var}": f"{shortname}"})
+                        src_units = fix["vars"][var].get("src_units", None)
+                        if src_units:
+                            data[var].attrs.update({"units": "degC"})
+                        units = fix["vars"][var]["units"]
+                        if units.count('{'):
+                            units = fixes["defaults"]["units"][units.replace('{','').replace('}','')]                
+                        data[var].attrs.update({"target_units": units})
+                        factor, offset = self.convert_units(data[var].units, units, var)
+                        data[var].attrs.update({"factor": factor})
+                        data[var].attrs.update({"offset": offset})
+
+        allcoords = data.coords
+        for coord in fix["coords"]:
+                    newcoord = fix["coords"][coord]["name"]
+                    newcoord = fixes["defaults"]["coords"][newcoord.replace('{','').replace('}','')] 
+                    if coord in allcoords: 
+                        fixd.update({f"{coord}": f"{newcoord}"})
+
+        return data.rename(fixd)
+
+    def convert_units(self, src, dst, var="input var"):
+        factor = units(src).to_base_units() / units(dst).to_base_units()
+        offset = ((0 * units(src)) - (0 * units(dst))).to(units(dst))
+        
+        if factor.units != units("dimensionless").units:
+            if factor.units == "meter ** 3 / kilogram":
+                # Density of water was missing
+                factor = factor * 1000 * units("kg m-3")
+                print(f"{var}: corrected multiplying by density of water 1000 kg m-3")
+            if factor.units == "meter ** 3 * second / kilogram":
+                # Density of water and accumulation time were missing
+                factor = factor * 1000 * units("kg m-3") / (self.deltat * units("s"))
+                print(f"{var}: corrected multiplying by density of water 1000 kg m-3")
+                print(f"{var}: corrected dividing by accumulation time {self.deltat} s")
+            else:
+                print(f"{var}: incommensurate units converting {src} to {dst} --> {factor.units}")
+
+        if offset.magnitude != 0:
+            factor = None
+            offset = offset.magnitude
+        else:
+            offset = None
+            factor = factor.magnitude
+        return factor, offset
+    
