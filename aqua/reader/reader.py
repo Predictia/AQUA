f8a9058204d818f2f57c822d6659ec5ffaf55573
Paolo Davini <p.davini@isac.cnr.it>
2023-02-28 11:07:03 +0100

    decumulate in notebook + example

--- aqua/reader/reader.py
+++ aqua/reader/reader.py
@@ -2,6 +2,7 @@ import intake
 import xarray as xr
 import os
 from metpy.units import units
+import numpy as np
 import smmregrid as rg
 from aqua.util import load_yaml
 import sys
@@ -257,12 +258,14 @@ class Reader():
             area_file.close()
 
 
-    def retrieve(self, regrid=False, average=False, fix=True, apply_unit_fix=True):
+    def retrieve(self, regrid=False, average=False, decumulate=False, fix=True, apply_unit_fix=True):
         """
         Perform a data retrieve.
         
         Arguments:
             regrid (bool):          if to regrid the retrieved data (False)
+            average (bool):         if to average the retrieved data (False)
+            decumulate (bool):      if to remove the cumulation from data (False)
             fix (bool):             if to perform a fix (var name, units, coord name adjustments) (True)
             apply_unit_fix (bool):  if to already adjust units by multiplying by a factor or adding
                                     an offset (this can also be done later with the `fix_units` method) (True)
@@ -279,7 +282,9 @@ class Reader():
         if self.level is not None:
             data = data.isel({self.vertcoord: self.level})
 
-        # sequence which should be more efficient: averaging - regridding - fixing
+        # sequence which should be more efficient: decumulate - averaging - regridding - fixing
+        if decumulate:
+            data = data.map(self.decumulate, keep_attrs=True)
         if self.freq and average:
             data = self.average(data)
         if self.targetgrid and regrid:
@@ -329,6 +334,62 @@ class Reader():
                 sys.exit('Cant find a frequency to resample')
    
         return out
+    
+    def _check_if_accumulated(self, data):
+
+        """To check if a DataArray is accumulated. 
+        Arbitrary check on the first 20 timesteps"""
+
+        # randomly pick a few timesteps from a gridpoint
+        ndims = [dim for dim in data.dims if data[dim].size > 1][1:]
+        pindex = {dim: 0 for dim in ndims}
+        check = data.isel(pindex).isel(time=slice(None, 20)).diff(dim='time').values
+        return (check >= 0).all()
+
+    
+    def decumulate(self, data, cumulation_time = None):
+        """
+        Test function to remove cumulative effect on IFS fluxes.
+        Cumulation times are estimated from the intervals of the data, but
+        can be specified manually
+
+        Args: 
+            data (xr.DataArray): field to be processed
+            cumulation_time (float): optional, specific cumulation time
+
+        Returns:
+            A xarray.DataArray where the cumulation time has been removed
+        """
+
+        check = self._check_if_accumulated(data)
+
+        if not check: 
+            return data
+        
+        else: 
+
+            # which frequency are the data?
+            if not cumulation_time:
+                cumulation_time = (data.time[1]-data.time[0]).values/np.timedelta64(1, 's')
+
+            # get the derivatives
+            deltas = data.diff(dim='time') / cumulation_time
+
+            # add a first timestep empty to align the original and derived fields
+            zeros = xr.zeros_like(data.isel(time=0))
+            deltas = xr.concat([zeros, deltas], dim = 'time').transpose('time', ...)
+
+            # universal mask based on the change of month (shifted by one timestep) 
+            mask = ~(data['time.month'] != data['time.month'].shift(time=1))
+            mask = mask.shift(time=1, fill_value=False)
+
+            # check which records are kept
+            #print(data.time[~mask])
+
+            # kaboom: exploit where
+            clean=deltas.where(mask, data/cumulation_time)
+
+            return clean
 
 
     def _check_if_regridded(self, data):
