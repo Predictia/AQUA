61f822e0412ba8869f0a4b051f5ccd7cc7372c9a
Nikolay Koldunov <koldunovn@gmail.com>
2023-03-13 02:06:02 +0100

    simple veraion of tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 
 # from aqua import aqua
 
@@ -29,6 +30,23 @@ def reader_instance():
 def test_retrieve(reader_instance):
     data = reader_instance.retrieve(fix=False)
     assert len(data) > 0
+    assert data.a_ice.shape == (2, 3140)
+
+
+def test_regrid(reader_instance):
+    data = reader_instance.retrieve(fix=False)
+    sstr = reader_instance.regrid(data["sst"][0:2, :])
+    assert sstr.shape == (2, 90, 180)
+    assert np.nanmean(sstr[0, :, :].values) == pytest.approx(13.350273936668883)
+    assert np.nanmean(sstr[1, :, :].values) == pytest.approx(13.319211491558276)
+
+
+def test_fldmean(reader_instance):
+    data = reader_instance.retrieve(fix=False)
+    global_mean = reader_instance.fldmean(data.sst[:2, :])
+    assert global_mean.shape == (2,)
+    assert global_mean.values[0] == pytest.approx(17.99434183)
+    assert global_mean.values[1] == pytest.approx(17.98060367)
 
 
 # @pytest.fixture(
