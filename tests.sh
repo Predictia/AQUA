0eb93186a1484e2650424c02527d2e0edcd23062
Nikolay Koldunov <koldunovn@gmail.com>
2023-03-14 11:44:07 +0100

    make local tests

---
+++ tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+file_url="https://swift.dkrz.de/v1/dkrz_035d8f6ff058403bb42f8302e6badfbc/AQUA/AQUA_tests_short.tar.gz"
+file_path="AQUA_tests_short.tar.gz"
+
+if [ ! -f "$file_path" ]; then
+    echo "Downloading file..."
+    curl -o "$file_path" "$file_url"
+    echo "File downloaded."
+    echo "Extracting file..."
+    tar -xzf "$file_path"
+else
+    echo "File already exists."
+fi
+
+python -m pytest ./tests/test_basic.py
+
