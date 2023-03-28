ab59822114e25363a2d622b6ca29efc51836db6d
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-03-28 16:20:02 +0200

    config as parse arg in cli_gribber

--- cli/gribber/cli_gribber.py
+++ cli/gribber/cli_gribber.py
@@ -18,16 +18,8 @@ def parse_arguments(args):
     """
 
     parser = argparse.ArgumentParser(description='AQUA gribber')
-    parser.add_argument('-f', '--file', type=str, required=False, 
+    parser.add_argument('-c', '--config', type=str, required=False, 
                         help='yaml file with the directories')
-    parser.add_argument('-d', '--datadir', type=str, required=False,
-                        help='Directory containing the .grib files')
-    parser.add_argument('-t', '--tmpdir', type=str, required=False,
-                        help='Directory where to store the temporary files')
-    parser.add_argument('-j', '--jsondir', type=str, required=False,
-                        help='Directory where to store the .json files')
-    parser.add_argument('-c', '--catalogdir', type=str, required=False,
-                        help='Directory where to store the catalog yaml files')
     parser.add_argument('-m', '--model', type=str, required=False, 
                         help='Model name')
     parser.add_argument('-e', '--exp', type=str, required=False, 
@@ -50,27 +42,12 @@ if __name__ == '__main__':
 
     args = parse_arguments(sys.argv[1:])
 
-    file = get_arg(args, 'file', None)
+    file = get_arg(args, 'config', None)
 
     # Get arguments
     if file:
         print('Reading yaml file')
-        yamlfile = get_arg(args, 'file', None)
-        dir_file = load_yaml(yamlfile)
-        datadir = dir_file['datadir']
-        tmpdir = dir_file['tmpdir']
-        jsondir = dir_file['jsondir']
-        catalogdir = dir_file['catalogdir']
-    else:
-        print('Reading command line arguments')
-        datadir = get_arg(args, 'datadir', 
-                          '/scratch/b/b382289/tco1279-orca025/nemo_deep/ICMGGc2')
-        tmpdir = get_arg(args, 'tmpdir', 
-                         '/scratch/b/b382289/gribscan')
-        jsondir = get_arg(args, 'jsondir', 
-                          '/work/bb1153/b382289/gribscan-json')
-        catalogdir = get_arg(args, 'catalogdir', 
-                             '/work/bb1153/b382289/AQUA/config/levante/catalog')
+        dir_file = load_yaml(file)
     
     model = get_arg(args, 'model', 'IFS')
     exp = get_arg(args, 'exp', 'tco1279-orca025')
@@ -81,8 +58,7 @@ if __name__ == '__main__':
 
     # Create Gribber object
     gribber = Gribber(model=model, exp=exp, source=source, nprocs=nprocs,
-                        dir={'datadir': datadir, 'tmpdir': tmpdir,
-                             'jsondir': jsondir, 'catalogdir': catalogdir},
+                        dir=dir_file,
                         verbose=verbose, replace=replace)
     
     # Create .catalog entry
