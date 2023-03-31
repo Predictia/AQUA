3c4e48eb26b26dbfc73d1d73a0e1edfbbaac3e92
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-03-31 17:12:59 +0200

    Some linting

--- aqua/lra_generator/lra_generator.py
+++ aqua/lra_generator/lra_generator.py
@@ -6,6 +6,7 @@ from aqua.util import create_folder, generate_random_string
 from dask.distributed import Client, LocalCluster, progress
 from dask.diagnostics import ProgressBar
 
+
 class LRA_Generator():
     """
     Class to generate LRA data at required frequency/resolution
@@ -13,7 +14,7 @@ class LRA_Generator():
     def __init__(self,
                  model=None, exp=None, source=None,
                  var=None, vars=None,
-                 resolution=None, frequency=None,fix=True,
+                 resolution=None, frequency=None, fix=True,
                  outdir=None, tmpdir=None, nproc=1,
                  verbose=False, overwrite=False, dry=True):
         """
@@ -23,20 +24,25 @@ class LRA_Generator():
             model (string):          The model name from the catalog
             exp (string):            The experiment name from the catalog
             source (string):         The sourceid name from the catalog
-            var (str, list):         Variable(s) to be processed and archived in LRA, 
-                                     vars in a synonim
+            var (str, list):         Variable(s) to be processed and archived
+                                     in LRA,vars in a synonim
             resolution (string):     The target resolution for the LRA
-            frequency (string,opt):  The target frequency for averaging the LRA, 
-                                     if no frequency is specified, no time average is performed
+            frequency (string,opt):  The target frequency for averaging the
+                                     LRA, if no frequency is specified,
+                                     no time average is performed
             fix (bool, opt):         True to fix the data, default is True
             outdir (string):         Where the LRA is
-            tmpdir (string):         Where to store temporary files, default is None
+            tmpdir (string):         Where to store temporary files,
+                                     default is None.
                                      Necessary for dask.distributed
             nproc (int, opt):        Number of processors to use. default is 1
-            verbose (bool, opt):     True to print logging messages, default is False
-            overwrite (bool, opt):   True to overwrite existing files in LRA, default is False
-            dry (bool, opt):         False to create the output file, 
-                                     True to just explore the reader operations, default is True        
+            verbose (bool, opt):     True to print logging messages,
+                                     default is False
+            overwrite (bool, opt):   True to overwrite existing files in LRA,
+                                     default is False
+            dry (bool, opt):         False to create the output file,
+                                     True to just explore the reader
+                                     operations, default is True
         """
         # General settings
         self.verbose = verbose
@@ -48,7 +54,7 @@ class LRA_Generator():
         if self.dry:
             if self.verbose:
                 print('IMPORTANT: no file will be created, this is a dry run')
-        
+
         self.nproc = nproc
         self.tmpdir = tmpdir
         if self.nproc > 1:
@@ -57,8 +63,9 @@ class LRA_Generator():
                 print(f'Running dask.distributed with {self.nproc} workers')
             if not self.tmpdir:
                 raise Exception('Please specify tmpdir for dask.distributed.')
-            else: 
-                self.tmpdir = os.path.join(self.tmpdir, generate_random_string(10))
+            else:
+                self.tmpdir = os.path.join(self.tmpdir, 
+                                           generate_random_string(10))
         else:
             self.dask = False
         
@@ -111,17 +118,17 @@ class LRA_Generator():
 
         # Create LRA folder
         if self.frequency:
-            self.outdir = os.path.join(outdir, self.exp, self.resolution, self.frequency)
+            self.outdir = os.path.join(outdir, self.exp, self.resolution,
+                                       self.frequency)
         else:
             self.outdir = os.path.join(outdir, self.exp, self.resolution)
-        create_folder(self.outdir,verbose=self.verbose)
+        create_folder(self.outdir, verbose=self.verbose)
 
         # Initialize variables used by methods
         self.data = None
         self.reader = None
         self.cluster = None
         self.client = None
-        
 
     def retrieve(self):
         """
@@ -135,9 +142,11 @@ class LRA_Generator():
                 print(f'I am going to produce LRA at {self.resolution} resolution...')
 
         # Initialize the reader
-        self.reader = Reader(model=self.model, exp=self.exp, source=self.source,var=self.var,
-                             regrid=self.resolution, freq=self.frequency, configdir="../../config")
-        
+        self.reader = Reader(model=self.model, exp=self.exp, 
+                             source=self.source, var=self.var,
+                             regrid=self.resolution, freq=self.frequency, 
+                             configdir="../../config")
+
         if self.verbose:
             print('Retrieving data...')
         self.data = self.reader.retrieve(fix=self.fix)
@@ -157,7 +166,7 @@ class LRA_Generator():
         if isinstance(self.var, list):
             for var in self.var:
                 self._write_var(var)
-        else: # Only one variable
+        else:  # Only one variable
             self._write_var(self.var)
 
         # Cleaning
@@ -176,13 +185,14 @@ class LRA_Generator():
         """
         Set up dask cluster
         """
-        if self.dask: # self.nproc > 1
+        if self.dask:  # self.nproc > 1
             if self.verbose:
                 print(f'Setting up dask cluster with {self.nproc} workers')
             dask.config.set({'temporary_directory': self.tmpdir})
             if self.verbose:
                 print(f'Temporary directory: {self.tmpdir}')
-            self.cluster = LocalCluster(n_workers=self.nproc,threads_per_worker=1)
+            self.cluster = LocalCluster(n_workers=self.nproc, 
+                                        threads_per_worker=1)
             self.client = Client(self.cluster)
         else:
             self.client = None
@@ -192,7 +202,7 @@ class LRA_Generator():
         """
         Close dask cluster
         """
-        if self.dask: # self.nproc > 1
+        if self.dask:  # self.nproc > 1
             self.client.shutdown()
             self.cluster.close()
             if self.verbose:
@@ -202,7 +212,7 @@ class LRA_Generator():
         """
         Remove temporary directory
         """
-        if self.dask: # self.nproc > 1
+        if self.dask:  # self.nproc > 1
             if self.verbose:
                 print('Removing temporary directory')
             os.removedirs(self.tmpdir)
@@ -227,7 +237,7 @@ class LRA_Generator():
         # Splitting data into yearly files
         years = set(temp_data.time.dt.year.values)
         for year in years:
-            year_data = temp_data.sel(time=temp_data.time.dt.year==year)
+            year_data = temp_data.sel(time=temp_data.time.dt.year == year)
             if self.verbose:
                 print(f'Processing year {year}...')
             # Splitting data into monthly files
@@ -235,15 +245,15 @@ class LRA_Generator():
             for month in months:
                 if self.verbose:
                     print(f'Processing month {month}...')
-                month_data = year_data.sel(time=year_data.time.dt.month==month)
+                month_data = year_data.sel(time=year_data.time.dt.month == month)
 
                 if not self.dry:
                     # Create output file
-                    outfile = os.path.join(self.outdir, 
-                                f'{var}_{self.exp}_{self.resolution}_{self.frequency}_{year}{str(month).zfill(2)}.nc')
+                    outfile = os.path.join(self.outdir,
+                                           f'{var}_{self.exp}_{self.resolution}_{self.frequency}_{year}{str(month).zfill(2)}.nc')
                     if os.path.isfile(outfile) and not self.overwrite:
                         print(f'File {outfile} already exists, skipping...')
-                    else: # File to be written
+                    else:  # File to be written
                         if os.path.exists(outfile):
                             os.remove(outfile)
                             if self.verbose:
@@ -253,8 +263,8 @@ class LRA_Generator():
 
                         # Write data to file, lazy evaluation
                         write_job = month_data.to_netcdf(outfile, 
-                                                        encoding={'time': self.time_encoding},
-                                                        compute=False)
+                                                         encoding={'time': self.time_encoding},
+                                                         compute=False)
 
                         if self.dask:
                             w_job = write_job.persist()
