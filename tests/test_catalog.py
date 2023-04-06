3b7d18e1c514d4b795bb64bffc2c93916a2ecedf
Paolo Davini <p.davini@isac.cnr.it>
2023-04-06 16:34:57 +0200

    including it in the workflow

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -1,15 +1,18 @@
 import pytest
+import xarray
 from aqua import Reader, catalogue
 
 @pytest.fixture(params=[(model, exp, source) 
-                        for model in catalogue() 
+                        for model in catalogue()
                         for exp in catalogue()[model] 
                         for source in catalogue()[model][exp]])
 def reader(request):
     model, exp, source = request.param
     myread = Reader(model=model, exp=exp, source=source, areas=False)
-    myread.retrieve(fix=False)
-    return myread
+    data = myread.retrieve(fix=False)
+    return myread, data
 
 def test_catalog(reader):
-    assert isinstance(reader, Reader)
+    a, b = reader
+    assert isinstance(a, Reader)
+    assert isinstance(b, xarray.Dataset)
