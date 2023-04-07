77de8b4a1bd264c27684ca734f90014de7933209
Paolo Davini <p.davini@isac.cnr.it>
2023-04-07 11:21:48 +0200

    regrid test

--- tests/test_timmean.py
+++ tests/test_timmean.py
@@ -1,3 +1,5 @@
+"""Test for timmean method"""
+
 import pytest
 import numpy as np
 from aqua import Reader
@@ -36,3 +38,15 @@ def test_timmean_monthly_reader():
     assert data['2t'].shape == (nmonths, 9, 18)
     assert len(unique) == nmonths
     assert all(counts == counts[0])
+
+def test_timmean_yearly_reader():
+    """Timmean test for yearly aggregation from Reader directly"""
+    reader = Reader(model="IFS", exp="test-tco79", source='long', freq='yr')
+    data = reader.retrieve(fix=False, timmean=True)
+    assert data['ttr'].shape == (1, 9, 18)
+
+def test_timmean_pandas_reader():
+    """Timmean test for weekly aggregation based on pandas labels from Reader directly"""
+    reader = Reader(model="IFS", exp="test-tco79", source='long', freq='W-MON', var='2t')
+    data = reader.retrieve(timmean=True)
+    assert data['2t'].shape == (29, 9, 18)
