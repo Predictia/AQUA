7d7f3b0170ae104bf16aafd638ef62a54eaad12a
Paolo Davini <p.davini@isac.cnr.it>
2023-04-12 18:10:59 +0200

    updates

--- diagnostics/ecmean/install_ECmean4.sh
+++ diagnostics/ecmean/install_ECmean4.sh
@@ -5,6 +5,8 @@
 
 # flag to clean environ,ent
 do_clean=true
+do_pip=false
+branch=main
 
 # find mamba/conda (to be refined)
 whereconda=$(which mamba | rev | cut -f 3-10 -d"/" | rev)
@@ -12,7 +14,7 @@ source $whereconda/etc/profile.d/conda.sh
 
 # check if you want to remove enviornment and repo
 if [[ $do_clean == true ]] ; then
-         echo "Removing the env..."
+        echo "Removing the env..."
         rm -rf ECmean4
         mamba env remove -n aqua-ecmean
 fi
@@ -21,29 +23,36 @@ sleep 5
 
 # create the env
 if [[ -z $(mamba env list | grep aqua-ecmean) ]] ; then
-        echo "Creating... the env..."
+        echo "Creating the env..."
         mamba env create -f environment-ecmean.yml
 fi
 
 sleep 5
 
-# clone
-if [[ ! -d ECmean4 ]] ; then
-        echo "Cloning..."
-        git clone https://github.com/oloapinivad/ECmean4.git
+# set up env
+conda activate aqua-ecmean
+
+if [[ $do_pip == false ]] ; then
+        # clone
+        if [[ ! -d ECmean4 ]] ; then
+                echo "Cloning..."
+                git clone https://github.com/oloapinivad/ECmean4.git
+        fi
+        
+        sleep 5
+
+        # checkout installable version: we might want to change it 
+        # to have it running on 
+        cd ECmean4
+        git fetch
+        git checkout $branch
+        git pull
+
+        # install ECmean4
+        pip install -e .
+        
+else 
+        pip install ECmean4
 fi
 
-sleep 5
-
-# checkout installable version: we might want to change it 
-# to have it running on 
-cd ECmean4
-git fetch
-git checkout main
-git pull
-
-# create env and install ECmean4
-conda activate aqua-ecmean
-pip install -e .
 conda deactivate
-
