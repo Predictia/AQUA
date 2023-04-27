28d56ef543c1bc36e4191b1184cd82b2215ce404
Natalia Nazarova <natalia.nazarova@polito.it>
2023-04-27 14:53:36 +0200

    Brackets added

--- diagnostics/tropical_rainfall/enviroment/env_setup.sh
+++ diagnostics/tropical_rainfall/enviroment/env_setup.sh
@@ -4,8 +4,8 @@ diag_dir=$(pwd)
 
 conda install  conda-merge 
 cd ../../..
-conda-merge environment.yml  $diag_dir/env-tropical-rainfall.yml > $diag_dir/merged.yml
+conda-merge environment.yml  ${diag_dir}/env-tropical-rainfall.yml > ${diag_dir}/merged.yml
 
-conda env create -f $diag_dir/merged.yml
+conda env create -f ${diag_dir}/merged.yml
 
 #conda activate tropical-rainfall_2
