2217e468e05c00567e51fbd5e0d4b4ca48e6ede9
Supriyo Ghosh <b382397@levante3.lvt.dkrz.de>
2023-05-03 14:34:02 +0200

    new file:   basin-T_S-means-inter.py 	new file:   global-means-interp.py 	new file:   modules.py

---
+++ diagnostics/global-ocean-diagnostics/modules.py
@@ -0,0 +1,46 @@
+import sys
+import os
+
+#package_dir = os.path.abspath('/home/dese28/dese28422/one_pass')
+sys.path.append('/home/b/b382397/diagnostics/one_pass')
+sys.path.append(os.path.abspath("/home/b/b382397/diagnostics/gsv_interface/"))
+#/home/dese28/dese28422/gsv_interface
+# Now you can import your package
+
+
+import os
+import xarray as xr
+import dask as dk
+import numpy as np
+from one_pass.opa import *
+from one_pass.opa import Opa
+# from gsv import GSVRetriever
+
+# gsv = GSVRetriever()
+
+# file_path = "/home/dese28/dese28422/one_pass/config.yml"
+
+monthly_mean_dic = {"stat": "mean",
+    "stat_freq": "monthly",
+    "output_freq": "monthly",
+    "time_step": 1440, # timestep of data in minutes
+    "variable": "thetao",
+    "save": False,
+    "checkpoint": False,
+    "checkpoint_file": "./",
+    "out_filepath": "./"}
+monthly_mean_opa=Opa(monthly_mean_dic)
+
+yearly_mean_dic = {"stat": "mean",
+    "stat_freq": "monthly",
+    "output_freq": "monthly",
+    "time_step": 1440, # timestep of data in minutes
+    "variable": "thetao",
+    "save": False,
+    "checkpoint": False,
+    "checkpoint_file": "./",
+    "out_filepath": "./"}
+yearly_mean_opa=Opa(yearly_mean_dic)
+
+
+
