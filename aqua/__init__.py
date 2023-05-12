0658663b098f3da939b04c86868f11f79a331225
Natalia Nazarova <natalia.nazarova@polito.it>
2023-05-12 22:23:59 +0200

    init file of aqua is updated

--- aqua/__init__.py
+++ aqua/__init__.py
@@ -3,6 +3,7 @@ from .docker import rundiag
 from .gribber import Gribber
 from .lra_generator import LRAgenerator
 from .reader import Reader, catalogue, Streaming, inspect_catalogue
+from .nc2zarr import main
 
 __version__ = '0.0.1'
 
