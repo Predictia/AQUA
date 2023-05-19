45bed483b729d254512104dfa0efc8fbd4a4b862
Paolo Davini <p.davini@isac.cnr.it>
2023-05-19 12:25:12 +0200

    extend the tests

--- tests/test_lra.py
+++ tests/test_lra.py
@@ -1,5 +1,5 @@
-import pytest
 import os, shutil
+import pytest
 import xarray as xr
 from aqua import LRAgenerator
 
@@ -10,8 +10,10 @@ from aqua import LRAgenerator
 def lra_arguments(request):
     return request.param
 
-@pytest.mark.aqua
+@pytest.mark.lra
 class TestLRA():
+    """Class for LRA Tests"""
+
     #test with definitive = False
     def test_definitive_false(self, lra_arguments):
             model, exp, source, var, outdir = lra_arguments
@@ -32,8 +34,20 @@ class TestLRA():
             test.data = month
             test.generate_lra()
             path = os.path.join(os.getcwd(), outdir, "IFS/test-tco79/r100/monthly/2t_test-tco79_r100_monthly_2020.nc")
+            test.check_integrity(varname=var)
             assert os.path.isfile(path)    
             file = xr.open_dataset(path)
             assert len(file.time) == 1
             assert pytest.approx(file['2t'][0,1,1].item()) == 248.0704
             shutil.rmtree(os.path.join(os.getcwd(), outdir))
+
+    #test with definitive = False but with dask init and catalog generator
+    def test_dask_entry(self, lra_arguments):
+            model, exp, source, var, outdir = lra_arguments
+            test = LRAgenerator(model=model, exp=exp, source=source, var=var, outdir=outdir, tmpdir = 'tmpdir',
+                                resolution='r100', frequency='monthly', nproc=2)
+            test.retrieve()
+            test.generate_lra()
+            test.create_catalog_entry()
+            assert os.path.isdir(os.path.join(os.getcwd(), outdir, "IFS/test-tco79/r100/monthly"))
+            shutil.rmtree(os.path.join(os.getcwd(), 'tmpdir'))
