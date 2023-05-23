db4caa8df1f0ecfd0b3501f4920b2b5b0e494a67
Paolo Davini <p.davini@isac.cnr.it>
2023-05-23 12:42:33 +0200

    extend tests

--- tests/test_vertinterp.py
+++ tests/test_vertinterp.py
@@ -9,10 +9,17 @@ def test_vertinterp():
 
     reader = Reader(model='FESOM', exp='test-pi', source='original_3d')
     data = reader.retrieve()
-    select = data['temp'][0,:]
-    interp = reader.vertinterp(select, levels = 10, units = 'm', vert_coord='nz1')
+    select = data.isel(time=0)
+
+    # dataarray access
+    interp = reader.vertinterp(select['temp'], levels = 10, units = 'm', vert_coord='nz1')
     assert pytest.approx(interp[40:41].values[0]) == -1.09438590
 
-    interp = reader.vertinterp(select, levels = [0.1, 0.2, 0.3], units = 'km', vert_coord='nz1')
+    # dataset access
+    interp = reader.vertinterp(select, levels = 10, vert_coord='nz1')
+    assert pytest.approx(interp['temp'][40:41].values[0]) == -1.09438590
+
+    # change unit
+    interp = reader.vertinterp(select['temp'], levels = [0.1, 0.2, 0.3], units = 'km', vert_coord='nz1')
     assert interp.shape == (3, 3140)
 
