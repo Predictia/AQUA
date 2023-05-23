d57e84a7ffbadb97dc7109fbb54dee76488cc139
Jost von Hardenberg <hardenberg@gmail.com>
2023-05-23 14:31:57 +0300

    test if no fdb5 now works

--- aqua/gsv/__init__.py
+++ aqua/gsv/__init__.py
@@ -1,7 +1,12 @@
 import intake  # Import this first to avoid circular imports during discovery.
-#from intake.container import register_container
+# from intake.container import register_container
 
-from .intake_gsv import GSVSource
+# Test if FDB5 binary library is available
+try:
+    from .intake_gsv import GSVSource
+except RuntimeError:
+    print("FDB5 binary library not present on system, disabling FDB support.")
+    pass
 
 try:
     intake.registry.drivers.register_driver('gsv', GSVSource)
