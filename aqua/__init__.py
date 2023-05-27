c8841151d7085dbe183658619dc6a87fb5fde117
Natalia Nazarova <natalia.nazarova@polito.it>
2023-05-27 16:33:36 +0200

    nc2zarr removed, lra_generator added

--- aqua/__init__.py
+++ aqua/__init__.py
@@ -1,9 +1,8 @@
 """AQUA module"""
 from .docker import rundiag
 from .gribber import Gribber
-#from .lra_generator import LRAgenerator, OPAgenerator
+from .lra_generator import LRAgenerator, OPAgenerator
 from .reader import Reader, catalogue, Streaming, inspect_catalogue
-from .nc2zarr import main
 from .slurm import squeue, job, output_dir, scancel, max_resources_per_node
 
 __version__ = '0.0.1'
