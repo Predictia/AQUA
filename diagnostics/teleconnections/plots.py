75b3f831887024f8c2d17fad8dfd63df4747ba14
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-06-19 12:07:58 +0200

    debug index plot function

--- diagnostics/teleconnections/plots.py
+++ diagnostics/teleconnections/plots.py
@@ -5,6 +5,7 @@ import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import xarray as xr
 
+from aqua.logger import log_configure
 
 def set_layout(fig, ax, title=None, xlabel=None, ylabel=None, xlog=False,
                ylog=False, xlim=None, ylim=None):
@@ -112,7 +113,7 @@ def cor_plot(indx, field, plot=True, projection_type='PlateCarree',
 
 
 def index_plot(indx, save=False, outputdir='./', filename='index.png',
-               **kwargs):
+               step=False, loglevel='WARNING', **kwargs):
     """
     Index plot together with a black line at indx=0
 
@@ -124,20 +125,36 @@ def index_plot(indx, save=False, outputdir='./', filename='index.png',
                                 default is './'
         filename (str,opt):     filename for the plot
                                 default is 'index.png'
+        step (bool,opt):        enable or disable step plot,
+                                default is False
+        loglevel (str,opt):     log level for the logger,
+                                default is 'WARNING'
         **kwargs:               additional arguments for set_layout
 
     Returns:
         fig (Figure):           Figure object
         ax (Axes):              Axes object
     """
+    # 0. -- Configure the logger --
+    logger = log_configure(loglevel, 'index_plot')
+
     # 1. -- Generate the figure --
     fig, ax = plt.subplots(figsize=(12, 8))
 
     # 2. -- Plot the index --
-    plt.fill_between(indx.time, indx.values, where=indx.values >= 0,
-                     step="pre", alpha=0.6, color='red')
-    plt.fill_between(indx.time, indx.values, where=indx.values < 0,
-                     step="pre", alpha=0.6, color='blue')
+    if step:
+        logger.warning('Step plot has some issues with fill_between')
+        logger.warning('Steps are not filled if a change in sign occurs')
+
+        plt.fill_between(indx.time, indx.values, where=indx.values >= 0,
+                        step="pre", alpha=0.6, color='red')
+        plt.fill_between(indx.time, indx.values, where=indx.values < 0,
+                        step="pre", alpha=0.6, color='blue')
+    else:
+        plt.fill_between(indx.time, indx.values, where=indx.values >= 0,
+                        alpha=0.6, color='red', interpolate=True)
+        plt.fill_between(indx.time, indx.values, where=indx.values < 0,
+                        alpha=0.6, color='blue', interpolate=True)
     indx.plot.step(ax=ax, color='black', alpha=0.8)
 
     ax.hlines(y=0, xmin=min(indx['time']), xmax=max(indx['time']),
@@ -149,6 +166,7 @@ def index_plot(indx, save=False, outputdir='./', filename='index.png',
     # 4. -- Save the figure --
     if save:
         fig.savefig(outputdir + filename)
+        logger.info('Figure saved in ' + outputdir + filename)
 
     return fig, ax
 
