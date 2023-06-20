babfd4317ae9b8fa90e172829eba6063170b4417
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-06-20 17:00:15 +0200

    missing import, version update

--- diagnostics/teleconnections/teleconnections/__init__.py
+++ diagnostics/teleconnections/teleconnections/__init__.py
@@ -2,6 +2,6 @@
 from .cdo_testing import station_based_cdo, regional_mean_cdo, regional_anomalies_cdo
 from .index import station_based_index, regional_mean_index, regional_mean_anomalies
 from .plots import cor_plot, reg_plot, simple_plot, index_plot
-from .tools import load_namelist
+from .tools import load_namelist, lon_180_to_360
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
