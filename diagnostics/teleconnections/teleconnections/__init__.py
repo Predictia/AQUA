f38aaf0779664c5d2364add391c962f33b73d40b
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-06-21 19:15:53 +0200

    changelog, __all__, v0.0.3

--- diagnostics/teleconnections/teleconnections/__init__.py
+++ diagnostics/teleconnections/teleconnections/__init__.py
@@ -5,4 +5,14 @@ from .plots import cor_plot, reg_plot, simple_plot, index_plot
 from .tc_class import Teleconnection
 from .tools import load_namelist, lon_180_to_360
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
+
+__all__ = ['station_based_cdo', 'regional_mean_cdo', 'regional_anomalies_cdo',
+           'station_based_index', 'regional_mean_index', 'regional_mean_anomalies',
+           'cor_plot', 'reg_plot', 'simple_plot', 'index_plot',
+           'Teleconnection', 'load_namelist', 'lon_180_to_360']
+
+# Change log
+# 0.0.3: Class Teleconnection added
+# 0.0.2: Added package version
+# 0.0.1: Initial version
