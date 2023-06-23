473aa5fae3c0175737fc71d6ab8abdc80038673b
Supriyo Ghosh <supriyo.ghosh@bsc.es>
2023-06-23 19:48:06 +0200

    modified both diag

--- diagnostics/global-ocean-diagnostics/diag_functions.py
+++ diagnostics/global-ocean-diagnostics/diag_functions.py
@@ -12,7 +12,35 @@ def wgt_area_mean(data, latN: float, latS: float,
     wgted_mean=t3gw.mean(("lat","lon"))
     return wgted_mean
 
-def std_anom_from_initial(data, latN: float, latS: float,
+def mean_value_plot(data,title):
+    data=fn.wgt_area_mean(data,-90,90,0,360)
+    fig, (ax1, ax2) = plt.subplots(nrows=1,ncols=2,
+                        figsize=(14,5))
+
+    fig.suptitle(title, fontsize=16)
+    levels=[0,100,500,1000,2000,3000,4000,5000]
+    for level in levels:
+        if level!=0:        
+            data_level=data.sel(lev=slice(None, level)).isel(lev=-1) 
+        else:
+            data_level=data.isel(lev=0)
+        data_level.thetao.plot.line(ax=ax1)
+        data_level.so.plot.line(ax=ax2)
+    ax1.set_title("Temperature", fontsize=14)
+    ax1.set_ylabel("Standardised Units (at the respective level)",fontsize=12)
+    ax1.set_xlabel("Time (in years)",fontsize=12)
+    ax1.legend(["0","100","500","1000","2000","3000","4000","5000"], loc='best')
+
+    ax2.set_title("Salinity", fontsize=14)
+    ax2.set_ylabel("Standardised Units (at the respective level)",fontsize=12)
+    ax2.set_xlabel("Time (in years)",fontsize=12)
+    # ax1.set_legend(["0","100","500","1000","2000","3000","4000","5000"], loc='best')
+    plt.legend(["0","100","500","1000","2000","3000","4000","5000"], loc='best')
+    # plt.savefig('Timeseries-Global-standardized-anomalies.png',transparent=True)
+    return data_level
+
+
+def std_anom_wrt_initial(data, latN: float, latS: float,
                   lonW: float, lonE: float,):
     std_anomaly = xr.Dataset()
     wgted_mean=wgt_area_mean(data, latN, latS, lonW, lonE)
@@ -21,7 +49,15 @@ def std_anom_from_initial(data, latN: float, latS: float,
         std_anomaly[var]= anomaly_from_initial/anomaly_from_initial.std("time")
 
     return std_anomaly
+def std_anom_wrt_time_mean(data, latN: float, latS: float,
+                  lonW: float, lonE: float,):
+    std_anomaly = xr.Dataset()
+    wgted_mean=wgt_area_mean(data, latN, latS, lonW, lonE)
+    for var in list(data.data_vars.keys()):
+        anomaly_from_initial=wgted_mean[var]-wgted_mean[var].mean("time")
+        std_anomaly[var]= anomaly_from_initial/anomaly_from_initial.std("time")
 
+    return std_anomaly
 
 def thetao_so_anom_plot(data, title):
     # And we perform the corresponding hovmoller plot
