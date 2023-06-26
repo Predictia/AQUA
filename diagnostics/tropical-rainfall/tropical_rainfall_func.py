2c7925f1b9263f4643763f7be1127802f959fc9f
Natalia Nazarova <natalia.nazarova@polito.it>
2023-06-26 16:04:46 +0200

    Another test of pre-push strategy

---
+++ diagnostics/tropical-rainfall/tropical_rainfall_func.py
@@ -0,0 +1,40 @@
+def time_interpreter(dataset):
+    """Identifying unit of timestep in the Dataset
+
+    Args:
+        dataset (xarray): The Dataset
+
+    Returns:
+        str: The unit of timestep in input Dataset
+    """
+
+    if dataset['time'].size==1:
+        return 'False. Load more timesteps then one'
+    try:
+        if np.count_nonzero(dataset['time.second'] == dataset['time.second'][0]) == dataset.time.size:
+            if np.count_nonzero(dataset['time.minute'] == dataset['time.minute'][0]) == dataset.time.size:
+                if  np.count_nonzero(dataset['time.hour'] == dataset['time.hour'][0]) == dataset.time.size:
+                    if np.count_nonzero(dataset['time.day'] == dataset['time.day'][0] ) == dataset.time.size or \
+                        np.count_nonzero([dataset['time.day'][i] in [1, 28, 29, 30, 31] for i in range(0, len(dataset['time.day']))]) == dataset.time.size:
+
+                        if np.count_nonzero(dataset['time.month'] == dataset['time.month'][0]) == dataset.time.size:
+                            return 'Y'
+                        else:
+                            return 'M'
+                    else:
+                        return 'D'
+                else:
+                    timestep = dataset.time[1] - dataset.time[0]
+                    n_hours = int(timestep/(60 * 60 * 10**9) )
+                    return str(n_hours)+'H'
+            else:
+                timestep = dataset.time[1] - dataset.time[0]
+                n_minutes = int(timestep/(60  * 10**9) )
+                return str(n_minutes)+'m'
+        else:
+            return 1
+
+    except KeyError and AttributeError:
+        timestep = dataset.time[1] - dataset.time[0]
+        if timestep >=28 and timestep <=31:
+            return 'M'
