2917447eb8375faecd6b6ce3c26158cf55c71c28
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-06-28 14:49:48 +0200

    reg cor main file renamed

--- diagnostics/teleconnections/teleconnections/__init__.py
+++ diagnostics/teleconnections/teleconnections/__init__.py
@@ -2,7 +2,7 @@
 from .cdo_testing import station_based_cdo, regional_mean_cdo, regional_anomalies_cdo
 from .index import station_based_index, regional_mean_index, regional_mean_anomalies
 from .plots import cor_plot, reg_plot, simple_plot, index_plot
-from .regression import reg_evaluation, cor_evaluation
+from .statistics import reg_evaluation, cor_evaluation
 from .tc_class import Teleconnection
 from .tools import load_namelist, lon_180_to_360
 
