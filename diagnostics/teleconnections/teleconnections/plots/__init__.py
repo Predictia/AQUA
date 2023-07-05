287301af54011e25bfcdf8a333a602b21214f4d6
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-07-05 15:52:12 +0200

    maps plot added to init

--- diagnostics/teleconnections/teleconnections/plots/__init__.py
+++ diagnostics/teleconnections/teleconnections/plots/__init__.py
@@ -1,5 +1,6 @@
 from .index_plot import index_plot
+from .maps_plot import maps_plot
 from .plot_utils import minmax_maps, set_layout, plot_box
 
-__all__ = ['index_plot', 'minmax_maps', 'set_layout',
-           'plot_box']
+__all__ = ['index_plot', 'maps_plot',
+           'minmax_maps', 'set_layout', 'plot_box']
