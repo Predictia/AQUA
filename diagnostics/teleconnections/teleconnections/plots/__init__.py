cc7831f967577fe452c243487f73ea4399302efe
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-07-05 15:23:46 +0200

    linting

--- diagnostics/teleconnections/teleconnections/plots/__init__.py
+++ diagnostics/teleconnections/teleconnections/plots/__init__.py
@@ -1,5 +1,5 @@
 from .index_plot import index_plot
 from .plot_utils import minmax_maps, set_layout, plot_box
 
-__all__ = ['index_plot','minmax_maps', 'set_layout',
+__all__ = ['index_plot', 'minmax_maps', 'set_layout',
            'plot_box']
