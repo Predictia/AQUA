7ed4d595658115c3fc9f36be84d78e6136ba27ad
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-07-05 15:22:00 +0200

    index_plot init adaptation

--- diagnostics/teleconnections/teleconnections/plots/__init__.py
+++ diagnostics/teleconnections/teleconnections/plots/__init__.py
@@ -1,3 +1,5 @@
+from .index_plot import index_plot
 from .plot_utils import minmax_maps, set_layout, plot_box
 
-__all__ = ['minmax_maps', 'set_layout', 'plot_box']
+__all__ = ['index_plot','minmax_maps', 'set_layout',
+           'plot_box']
