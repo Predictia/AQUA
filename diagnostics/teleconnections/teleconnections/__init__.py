92893f36dc6cc9795531b072487c3ca1c38f8f26
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-07-05 15:16:46 +0200

    first plot submodule functions

--- diagnostics/teleconnections/teleconnections/__init__.py
+++ diagnostics/teleconnections/teleconnections/__init__.py
@@ -1,20 +1,21 @@
 """teleconnections module"""
 from .cdo_testing import station_based_cdo, regional_mean_cdo, regional_anomalies_cdo
 from .index import station_based_index, regional_mean_index, regional_mean_anomalies
-from .plots import simple_plot, index_plot, comparison_index_plot
+from .plots import minmax_maps, set_layout, plot_box
 from .statistics import reg_evaluation, cor_evaluation
 from .tc_class import Teleconnection
 from .tools import load_namelist, lon_180_to_360
 
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 
 __all__ = ['station_based_cdo', 'regional_mean_cdo', 'regional_anomalies_cdo',
            'station_based_index', 'regional_mean_index', 'regional_mean_anomalies',
-           'simple_plot', 'index_plot', 'comparison_index_plot',
+           'minmax_maps', 'set_layout', 'plot_box',
            'reg_evaluation', 'cor_evaluation',
            'Teleconnection', 'load_namelist', 'lon_180_to_360']
 
 # Change log
+# 0.0.6: plots submodules added
 # 0.0.5: comparison_index_plot, cli for single and multiple datasets added
 # 0.0.4: Added cor_evaluation and reg_evaluation based on sacpy, removed deprecated functions
 # 0.0.3: Class Teleconnection added
