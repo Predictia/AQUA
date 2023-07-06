cd1ec95236e2b32354a0dd682b4894f0b24e2f9c
Paolo Ghinassi <p.ghinassi@isac.cnr.it>
2023-07-06 12:33:28 +0200

    updated init file

--- diagnostics/tropical_cyclones/__init__.py
+++ diagnostics/tropical_cyclones/__init__.py
@@ -4,12 +4,11 @@
 # and associated functions are available directly from the module "tropical_cyclones"
 
 
-from .dummy_class_readerwrapper import DummyDiagnosticWrapper
-from .dummy_class_timeband import DummyDiagnostic
-from .dummy_func import dummy_func
+from .tropical_cyclones import TCs
+from .plotting_TCs import multi_plot, plot_trajectories
 
 # Optional but recommended
 __version__ = '0.0.1'
 
 # This specifies which methods are exported publicly, used by "from dummy import *"
-__all__ = ["DummyDiagnostic", "DummyDiagnosticWrapper", "dummy_func"]
+__all__ = ["TCs", "multi_plot", "plot_trajectories"]
