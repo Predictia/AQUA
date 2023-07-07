331bc00c481176594b7331e09baf118d3e372a4a
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-07-07 09:39:08 +0200

    main init adapted to tools submodule

--- diagnostics/teleconnections/teleconnections/__init__.py
+++ diagnostics/teleconnections/teleconnections/__init__.py
@@ -1,20 +1,28 @@
 """teleconnections module"""
-from .cdo_testing import station_based_cdo, regional_mean_cdo, regional_anomalies_cdo
-from .index import station_based_index, regional_mean_index, regional_mean_anomalies
+from .cdo_testing import station_based_cdo, regional_mean_cdo
+from .cdo_testing import regional_anomalies_cdo
+from .index import station_based_index, regional_mean_index
+from .index import regional_mean_anomalies
 from .plots import minmax_maps, plot_box, index_plot, maps_plot
 from .statistics import reg_evaluation, cor_evaluation
 from .tc_class import Teleconnection
-from .tools import load_namelist, lon_180_to_360
+from .tools import load_namelist, lon_180_to_360, lon_360_to_180
+from .tools import area_selection, wgt_area_mean
+from .tools import get_dataset_config
 
-__version__ = '0.0.6'
+__version__ = '0.0.7'
 
 __all__ = ['station_based_cdo', 'regional_mean_cdo', 'regional_anomalies_cdo',
-           'station_based_index', 'regional_mean_index', 'regional_mean_anomalies',
+           'station_based_index', 'regional_mean_index',
+           'regional_mean_anomalies',
            'minmax_maps', 'plot_box', 'index_plot', 'maps_plot',
            'reg_evaluation', 'cor_evaluation',
-           'Teleconnection', 'load_namelist', 'lon_180_to_360']
+           'Teleconnection', 'load_namelist', 'lon_180_to_360',
+           'lon_360_to_180', 'area_selection',
+           'wgt_area_mean', 'get_dataset_config']
 
 # Change log
+# 0.0.7: CLI refined, get_dataset_config moved to tools, tools refactor
 # 0.0.6: plots submodules added
 # 0.0.5: comparison_index_plot, cli for single and multiple datasets added
 # 0.0.4: Added cor_evaluation and reg_evaluation based on sacpy, removed deprecated functions
