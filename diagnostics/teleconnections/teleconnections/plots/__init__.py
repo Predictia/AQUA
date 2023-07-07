74843440559092f6c812643d89c4d39d9004af3a
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-07-07 16:23:15 +0200

    removed set_layout

--- diagnostics/teleconnections/teleconnections/plots/__init__.py
+++ diagnostics/teleconnections/teleconnections/plots/__init__.py
@@ -1,6 +1,6 @@
 from .index_plot import index_plot
 from .maps_plot import maps_plot, single_map_plot
-from .plot_utils import minmax_maps, set_layout, plot_box
+from .plot_utils import minmax_maps, plot_box
 
 __all__ = ['index_plot', 'maps_plot', 'single_map_plot',
-           'minmax_maps', 'set_layout', 'plot_box']
+           'minmax_maps', 'plot_box']
