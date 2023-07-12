98c8a1ea41972f0b084b1395bd67cf9fa24198fd
ssayedwd <susan.sayed@dwd.de>
2023-07-12 17:47:59 +0200

    fixed issues: comparing climatologies in seasonal plots, plot_map_with_stats() update and init file update

--- diagnostics/atmglobalmean/atm_global_mean.py
+++ diagnostics/atmglobalmean/atm_global_mean.py
@@ -9,7 +9,7 @@ import cartopy.feature as cfeature
 import matplotlib.gridspec as gridspec
 from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
 from aqua import Reader
-
+import pandas as pd
 outputfig = "./output/figs"
 if not os.path.exists(outputfig):
     os.makedirs(outputfig)
@@ -17,21 +17,23 @@ outputdir = "./output/data"
 if not os.path.exists(outputdir):
     os.makedirs(outputdir)
     
-
-def seasonal_bias(dataset1, dataset2, var_name, year, plev, statistic, model_label1, model_label2):
+    
+def seasonal_bias(dataset1, dataset2, var_name, plev, statistic, model_label1, model_label2, start_date1, end_date1, start_date2, end_date2):
     '''
-    Plot the seasonal bias maps between two datasets for a specific variable and year.
+    Plot the seasonal bias maps between two datasets for specific variable and time ranges.
 
     Args:
-
-        dataset1 (xarray.Dataset): The first dataset
-        dataset2 (xarray.Dataset): The second dataset. Note: If this dataset is data_era5 (ERA5 data) it provides a bias calculation with
-                                    respect to the ERA5 climatology from 2000 to 2020. You can choose like for dataset1 OR data_era5
-        var_name (str): The name of the variable to compare (Examples: 2t, tprate, mtntrf, mtnsrf,...)
-        year (int): The year for which to calculate the bias.
+        dataset1 (xarray.Dataset): The first dataset.
+        dataset2 (xarray.Dataset): The second dataset, that will be compared to the first dataset.
+        var_name (str): The name of the variable to compare (e.g., '2t', 'tprate', 'mtntrf', 'mtnsrf', ...).
         plev (float or None): The desired pressure level in Pa. If None, the variable is assumed to be at surface level.
         statistic (str): The desired statistic to calculate for each season. Valid options are: 'mean', 'max', 'min', 'diff', and 'std'.
-        model_label1 and model_label2 (str): The desired labeling for the plot title and the filename for the respective datasets.
+        model_label1 (str): The labeling for the first dataset.
+        model_label2 (str): The labeling for the second dataset.
+        start_date1 (str): The start date of the time range for dataset1 in 'YYYY-MM-DD' format.
+        end_date1 (str): The end date of the time range for dataset1 in 'YYYY-MM-DD' format.
+        start_date2 (str): The start date of the time range for dataset2 in 'YYYY-MM-DD' format.
+        end_date2 (str): The end date of the time range for dataset2 in 'YYYY-MM-DD' format.
 
     Raises:
         ValueError: If an invalid statistic is provided.
@@ -39,27 +41,12 @@ def seasonal_bias(dataset1, dataset2, var_name, year, plev, statistic, model_lab
     Returns:
         A seasonal bias plot.
     '''
-    
+
     var1 = dataset1[var_name]
     var2 = dataset2[var_name]
-    
-    reader_era5 = Reader(model="ERA5", exp="era5", source="monthly")
-    data_era5 = reader_era5.retrieve(fix=True)
-    data_era5 = data_era5.sel(time=slice('2000-01-01', '2020-12-31'))
-    
 
-#     reader_tco2559 = Reader(model = 'IFS', exp = 'tco2559-ng5-cycle3', source = 'lra-r100-monthly')
-#     data_tco2559 = reader_tco2559.retrieve(fix = False)
-
-#     reader_tco1279 = Reader(model="IFS", exp="tco1279-orca025-cycle3",source =  'lra-r100-monthly')
-#     data_tco1279 = reader_tco1279.retrieve(fix = False)
-
-#     reader_icon = Reader(model = "ICON", exp = "ngc3028", source = 'lra-r100-monthly')
-#     data_icon = reader_icon.retrieve(fix = False)
-
-    
-    var1_year = var1.sel(time=var1.time.dt.year == year)
-    var2_year = var2.sel(time=var2.time.dt.year == year)
+    var1_year = var1.sel(time=slice(start_date1, end_date1))
+    var2_year = var2.sel(time=slice(start_date2, end_date2))
 
     # Select the desired pressure level if provided
     if plev is not None:
@@ -70,26 +57,23 @@ def seasonal_bias(dataset1, dataset2, var_name, year, plev, statistic, model_lab
     season_ranges = {'DJF': [12, 1, 2], 'MAM': [3, 4, 5], 'JJA': [6, 7, 8], 'SON': [9, 10, 11]}
     results = []
     for season, months in season_ranges.items():
-        if dataset1 == data_era5:
-            var1_season = var1.sel(time=var1.time.dt.month.isin(months))
-        else:
-            var1_season = var1.sel(time=(var1.time.dt.year == year) & (var1.time.dt.month.isin(months)))
-        var2_season = var2.sel(time=var2.time.dt.month.isin(months))
+        var2_season = var2_year.sel(time=var2_year.time.dt.month.isin(months))
 
         if statistic == 'mean':
-            result_season = var2_season.mean(dim='time') - var1_season.mean(dim='time')
+            result_season = var1_year.sel(time=var1_year.time.dt.month.isin(months)).mean(dim='time') - var2_season.mean(dim='time')
         elif statistic == 'max':
-            result_season = var2_season.max(dim='time') - var1_season.max(dim='time')
+            result_season = var1_year.sel(time=var1_year.time.dt.month.isin(months)).max(dim='time') - var2_season.max(dim='time')
         elif statistic == 'min':
-            result_season = var1_season.min(dim='time') - var2_season.min(dim='time')
+            result_season = var1_year.sel(time=var1_year.time.dt.month.isin(months)).min(dim='time') - var2_season.min(dim='time')
         elif statistic == 'diff':
-            result_season = var1_season - var2_season
+            result_season = var1_year.sel(time=var1_year.time.dt.month.isin(months)) - var2_season
         elif statistic == 'std':
-            result_season = var1_season.std(dim='time') - var2_season.std(dim='time')
+            result_season = var1_year.sel(time=var1_year.time.dt.month.isin(months)).std(dim='time') - var2_season.std(dim='time')
         else:
             raise ValueError("Invalid statistic. Please choose one of 'mean', 'std', 'max', 'min', or 'diff'.")
 
         results.append(result_season)
+
     # Create a cartopy projection
     projection = ccrs.PlateCarree()
 
@@ -97,7 +81,6 @@ def seasonal_bias(dataset1, dataset2, var_name, year, plev, statistic, model_lab
     num_rows = 2
     num_cols = 2
 
-    
     # Plot the bias maps for each season
     fig = plt.figure(figsize=(15, 10))
     gs = gridspec.GridSpec(num_rows, num_cols, figure=fig)
@@ -120,12 +103,10 @@ def seasonal_bias(dataset1, dataset2, var_name, year, plev, statistic, model_lab
         ax.xaxis.set_major_formatter(LongitudeFormatter())
         ax.yaxis.set_major_formatter(LatitudeFormatter())
 
-
         # Plot the bias data using the corresponding cnplot object
-        cnplot = result.plot(ax=ax, cmap='RdBu_r', add_colorbar = False)
+        cnplot = result.plot(ax=ax, cmap='RdBu_r', add_colorbar=False)
         cnplots.append(cnplot)
 
-        # ax.add_feature(cfeature.OCEAN)
         ax.set_title(f'{season}')
         ax.set_xlabel('Longitude')
         ax.set_ylabel('Latitude')
@@ -141,32 +122,23 @@ def seasonal_bias(dataset1, dataset2, var_name, year, plev, statistic, model_lab
     cbar.set_label(f'Bias [{var2.units}]')
 
     # Set the overall figure title
-    # Set the overall title        
-
-    if dataset2 == data_era5:
-
-        if plev is not None:
-            overall_title = f'Bias of {var_name} ({dataset2[var_name].long_name}) [{var2.units}] ({statistic}) at {plev} Pa\n Experiment {model_label1} {year} with respect to ERA5 climatology (2000-2020)'
-        else:
-            overall_title = f'Bias of {var_name} ({dataset2[var_name].long_name}) [{var2.units}] ({statistic})\n Experiment {model_label1}  {year} with respect to ERA5 climatology (2000-2020)'
-                
+    if plev is not None:
+        overall_title = f'Bias of {var_name} ({model_label1}) [{var2.units}] ({statistic}) from ({start_date1}-{end_date1}) at {plev} Pa\n Experiment {model_label1} with respect to  {model_label2} climatology ({start_date2}-{end_date2})'
     else:
-        if plev is not None:
-            overall_title = f'Bias of {var_name} ({dataset2[var_name]}) [{var2.units}] ({statistic})\n Experiment {model_label1}  {year} with respect to {model_label2}'
-        else:
-            overall_title = f'Bias of {var_name} ({dataset2[var_name]}) [{var2.units}] ({statistic}) at {plev} Pa\n Experiment {model_label1}  {year} with respect to {model_label2}'
+        overall_title = f'Bias of {var_name} ({model_label1}) [{var2.units}] ({statistic}) from ({start_date1}-{end_date1})\n Experiment {model_label1} with respect to {model_label2} climatology ({start_date2}-{end_date2})'
 
     # Set the title above the subplots
     fig.suptitle(overall_title, fontsize=14, fontweight='bold')
     plt.subplots_adjust(hspace=0.5)
-    
-    filename = f"{outputfig}/Seasonal_Bias_Plot_{model_label1}_{var_name}_{statistic}_{year}.pdf"
-    plt.savefig(filename, dpi = 300, format = 'pdf')
+
+    # Save the figure
+    filename = f"{outputfig}/Seasonal_Bias_Plot_{model_label1}_{var_name}_{statistic}_{start_date1}_{end_date1}_{start_date2}_{end_date2}.pdf"
+    plt.savefig(filename, dpi=300, format='pdf')
     plt.show()
-                                                                
+
     # Write the data into a NetCDF file
     data_directory = outputdir
-    data_filename = f"Seasonal_Bias_Data_{model_label1}_{var_name}_{statistic}_{year}.nc"
+    data_filename = f"Seasonal_Bias_Data_{model_label1}_{var_name}_{statistic}_{start_date1}_{end_date1}_{start_date2}_{end_date2}.nc"
     data_path = os.path.join(data_directory, data_filename)
 
     data_array = xr.concat(results, dim='season')
@@ -174,13 +146,16 @@ def seasonal_bias(dataset1, dataset2, var_name, year, plev, statistic, model_lab
     data_array.attrs['statistic'] = statistic
     data_array.attrs['dataset1'] = model_label1
     data_array.attrs['dataset2'] = model_label2
-    data_array.attrs['year'] = year
+    data_array.attrs['climatology_range1'] = f'{start_date1}-{end_date1}'
+    data_array.attrs['climatology_range2'] = f'{start_date2}-{end_date2}'
 
     data_array.to_netcdf(data_path)
-    
+
     print(f"The seasonal bias plots have been saved to {outputfig}.")
     print(f"The seasonal bias data has been saved to {outputdir}.")
-    
+
+#---------------------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------------------
 
 def compare_datasets_plev(dataset1, var_name, time_range, model_label):
     """
@@ -232,24 +207,35 @@ def compare_datasets_plev(dataset1, var_name, time_range, model_label):
     filename = f"{outputfig}/Vertical_biases_{model_label}_{var_name}.pdf"
     plt.savefig(filename, dpi = 300, format = 'pdf')
 
-        # Save the data into a NetCDF file
+    # Save the data into a NetCDF file
     filename = f"{outputdir}/Vertical_bias_{model_label}_{var_name}.nc"
     mean_bias.to_netcdf(filename)
     print(f"The vertical bias plots have been saved to {outputfig}.")
     print(f"The vertical bias data has been saved to {outputdir}.")
     
     
-def plot_map_with_stats(dataset, var_name, time_range, model_label):
+#---------------------------------------------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+def plot_map_with_stats(dataset, var_name, start_date, end_date, model_label):
     """
     Plot a map of a chosen variable from a dataset with colorbar and statistics.
 
     Args:
         dataset (xarray.Dataset): The dataset containing the variable.
         var_name (str): The name of the variable to plot.
-        time_range (slice or str): The time range to select from the dataset. Should be a valid slice or a string in 'YYYY-MM-DD' format.
+        start_date (str): The start date of the time range in 'YYYY-MM-DD' format.
+        end_date (str): The end date of the time range in 'YYYY-MM-DD' format.
+        model_label (str): The label for the model.
+        outputdir (str): The directory to save the output files.
     """
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+    
     # Calculate statistics
-    var_data = dataset[var_name].sel(time=time_range).mean(dim='time')
+    var_data = dataset[var_name].sel(time=slice(start_date, end_date)).mean(dim='time')
     weights = np.cos(np.deg2rad(dataset.lat))
     weighted_data = var_data.weighted(weights)
     var_mean = weighted_data.mean(('lon', 'lat')).values.item()
@@ -257,15 +243,6 @@ def plot_map_with_stats(dataset, var_name, time_range, model_label):
     var_min = var_data.min().values.item()
     var_max = var_data.max().values.item()
     
-#         # get the era 5 data for calculating the RMSE (serves as observation data)
-#         reader_era5 = Reader(model="ERA5", exp="era5", source="monthly")
-#         data_era5 = reader_era5.retrieve(fix=True)
-
-#         # Calculate RMSE
-#         obs_data = data_era5[var_name].mean(dim='time')  # Get the observed data for comparison
-#         n = len(var_data.values)
-#         rmse = np.sqrt(((obs_data - var_data) ** 2).mean().values.item())
-
     # Plot the map
     fig = plt.figure(figsize=(10, 8))
     ax = plt.axes(projection=ccrs.PlateCarree())
@@ -273,7 +250,7 @@ def plot_map_with_stats(dataset, var_name, time_range, model_label):
     im = ax.pcolormesh(var_data.lon, var_data.lat, var_data.values, cmap=cmap, transform=ccrs.PlateCarree())
 
     # Set plot title and axis labels
-    ax.set_title(f'Map of {var_name} from {time_range} for {model_label}')
+    ax.set_title(f'Map of {var_name} for {model_label} from {start_date.strftime("%Y-%m-%d")} to {end_date.strftime("%Y-%m-%d")}')
     ax.set_xlabel('Longitude')
     ax.set_ylabel('Latitude')
 
@@ -289,7 +266,22 @@ def plot_map_with_stats(dataset, var_name, time_range, model_label):
     # Display statistics below the plot
     stat_text = f'Mean: {var_mean:.2f} {dataset[var_name].units}    Std: {var_std:.2f}    Min: {var_min:.2f}    Max: {var_max:.2f}'
     ax.text(0.5, -0.3, stat_text, transform=ax.transAxes, ha='center')
+    
+    # Save the plot as a PDF file
+    filename = f"Statistics_maps_{model_label}_{var_name}.pdf"
+    output_path = os.path.join(outputfig, filename)
+    plt.savefig(output_path, dpi=300, format='pdf')
 
+    # Save the data into a NetCDF file
+    data_filename = f"Statistics_Data_{model_label}_{var_name}.nc"
+    data_path = os.path.join(outputdir, data_filename)
 
+    data_array = var_data.to_dataset(name=var_name)
+    data_array.attrs = dataset[var_name].attrs  # Copy attributes from the original dataset
+    data_array.attrs['model_label'] = model_label
+
+    data_array.to_netcdf(data_path)
+     
     plt.show()
-
+    print(f"The plot has been saved to {outputfig}.")
+    print(f"The data has been saved to {outputdir}.")
