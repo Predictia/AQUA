524c284acc53a6a4b43caf7d37b933efefb3c3ae
Paolo Davini <p.davini@isac.cnr.it>
2023-07-13 13:14:05 +0200

    typo

--- aqua/reader/reader.py
+++ aqua/reader/reader.py
@@ -423,7 +423,7 @@ class Reader(FixerMixin, RegridMixin):
             out = data.resample(time=resample_freq).mean()
             # for now, we set initial time of the averaging period following ECMWF standard
             # HACK: we ignore hours/sec to uniform the output structure
-            proper_time = data.time.resample(time=resample_freq).min()
+            #proper_time = data.time.resample(time=resample_freq).min()
             #out['time'] = np.array(proper_time.values, dtype='datetime64[h]')
             out['time'] = data.time.resample(time=resample_freq).min().dt.floor('D')
         except ValueError:
