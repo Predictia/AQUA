bd8f89ba9d7561cc6a1a3fac363f30be661dde13
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-07-13 15:44:44 +0200

    added an HACK for FESOM, see #375

--- diagnostics/teleconnections/teleconnections/tc_class.py
+++ diagnostics/teleconnections/teleconnections/tc_class.py
@@ -205,6 +205,14 @@ class Teleconnection():
                     self.data = self.reader.timmean(self.data)
                     self.logger.info('Time aggregated to {}'.format(self.freq))
 
+            #  HACK for FESOM data:
+            #  data have double times and the odd ones are NaNs (see issue #375)
+            if self.model == 'FESOM':
+                try:
+                    self.logger.warning('Trying to fix FESOM data, see issue #375')
+                    self.data = self.data.isel(time=slice(0, None, 2))
+                except ValueError:
+                    return
             return
         else:
             try:
