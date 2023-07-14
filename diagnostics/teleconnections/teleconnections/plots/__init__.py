ca0c3fc421cde485bcc781fd1ae44b5d671e800e
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-07-14 12:51:03 +0200

    all functions in __all__ now

--- diagnostics/teleconnections/teleconnections/plots/__init__.py
+++ diagnostics/teleconnections/teleconnections/plots/__init__.py
@@ -3,4 +3,5 @@ from .maps_plot import maps_plot, single_map_plot, maps_diffs_plot
 from .plot_utils import minmax_maps, plot_box, add_cyclic_lon
 
 __all__ = ['index_plot', 'maps_plot', 'single_map_plot',
-           'maps_diffs_plot', 'minmax_maps', 'plot_box']
+           'maps_diffs_plot', 'minmax_maps', 'plot_box',
+           'add_cyclic_lon']
