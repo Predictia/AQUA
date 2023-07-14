5b393734d373e28a1eb60ce49d85d4a175207596
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-07-14 11:49:41 +0200

    telec init update with new maps routines

--- diagnostics/teleconnections/teleconnections/__init__.py
+++ diagnostics/teleconnections/teleconnections/__init__.py
@@ -4,7 +4,7 @@ from .cdo_testing import regional_anomalies_cdo
 from .index import station_based_index, regional_mean_index
 from .index import regional_mean_anomalies
 from .plots import minmax_maps, plot_box, index_plot, maps_plot
-from .plots import single_map_plot
+from .plots import single_map_plot, maps_diffs_plot
 from .statistics import reg_evaluation, cor_evaluation
 from .tc_class import Teleconnection
 from .tools import load_namelist, lon_180_to_360, lon_360_to_180
@@ -17,15 +17,16 @@ __all__ = ['station_based_cdo', 'regional_mean_cdo', 'regional_anomalies_cdo',
            'station_based_index', 'regional_mean_index',
            'regional_mean_anomalies',
            'minmax_maps', 'plot_box', 'index_plot', 'maps_plot',
-           'single_map_plot',
+           'single_map_plot', 'maps_diffs_plot',
            'reg_evaluation', 'cor_evaluation',
            'Teleconnection', 'load_namelist', 'lon_180_to_360',
            'lon_360_to_180', 'area_selection',
            'wgt_area_mean', 'get_dataset_config']
 
 # Change log
+# 0.0.9: mapa_diff_plot and add_cyclic_lon added to plots module
 # 0.0.8: regression and correlation refactored
-#        Added the possibility to evaluate them with a different variable 
+#        Added the possibility to evaluate them with a different variable
 #        from the variable used to evaluate the index
 # 0.0.7: CLI refined, get_dataset_config moved to tools, tools refactor
 # 0.0.6: plots submodules added
