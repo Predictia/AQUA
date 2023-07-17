dff227f1e252017f3af13e6eabe26c286139cc66
Matteo Nurisso <m.nurisso@isac.cnr.it>
2023-07-17 14:24:08 +0200

    exception test

--- tests/test_vertinterp.py
+++ tests/test_vertinterp.py
@@ -24,3 +24,19 @@ def test_vertinterp():
     # change unit
     interp = reader.vertinterp(select['ocpt'], levels=[0.1, 0.2, 0.3], units='km', vert_coord='nz1')
     assert interp.shape == (3, 3140)
+
+@pytest.mark.aqua
+def test_vertinterp_exceptions():
+    """"Test exceptions for vertical interpolation"""
+
+    reader = Reader(model='FESOM', exp='test-pi', source='original_3d', loglevel=loglevel)
+    data = reader.retrieve()
+    select = data.isel(time=0)
+
+    # wrong vert_coord
+    with pytest.raises(KeyError):
+        reader.vertinterp(select['ocpt'], levels=10, units='m', vert_coord='nz2')
+
+    # no levels
+    with pytest.raises(KeyError):
+        reader.vertinterp(select['ocpt'], units='m', vert_coord='nz1')
