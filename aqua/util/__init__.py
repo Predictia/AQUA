d424cfa122d66c936e8e547a536d69a51f0d0daf
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-07-18 16:22:18 +0200

    graphics utilities

--- aqua/util/__init__.py
+++ aqua/util/__init__.py
@@ -2,10 +2,12 @@
 
 from .config import get_config_dir, get_machine, get_reader_filenames
 from .eccodes import read_eccodes_dic, read_eccodes_def
+from .graphics import add_cyclic_lon, plot_box, minmax_maps
 from .util import generate_random_string, get_arg, create_folder, file_is_complete
 from .yaml import load_yaml, dump_yaml, load_multi_yaml
 
 __all__ = ['get_config_dir', 'get_machine', 'get_reader_filenames',
            'read_eccodes_dic', 'read_eccodes_def',
+           'add_cyclic_lon', 'plot_box', 'minmax_maps',
            'generate_random_string', 'get_arg', 'create_folder', 'file_is_complete',
            'load_yaml', 'dump_yaml', 'load_multi_yaml']
