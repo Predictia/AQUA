4a0dea8e7515adb68aea47b14946f5c0228aab7e
Paolo Davini <p.davini@isac.cnr.it>
2023-07-24 16:15:04 +0300

    further fix now working

--- config/machines/lumi/installation/lumi_install.sh
+++ config/machines/lumi/installation/lumi_install.sh
@@ -5,6 +5,11 @@
 # 2. Install AQUA framework and diagnostics.
 #    This is the default option if no flag is provided.
 
+# Usage
+# bash lumi_install.sh 
+
+set -e
+
 #####################################################################
 # Getting the installation type from flags
 # Check if the script is called with at least one argument (the flag)
@@ -30,6 +35,9 @@ fi
 # Begin of user input
 machine=lumi 
 user=$USER # change this to your username if automatic detection fails
+MAMBADIR="$HOME/mambaforge" #check if $HOME does not exist
+load_aqua_file="$HOME/load_aqua.sh" #check if $HOME does not exist
+
 
 # define AQUA path
 if [[ -z "${AQUA}" ]]; then
@@ -42,9 +50,9 @@ fi
 
 # define installation path
 if $diag; then
-  export INSTALLATION_PATH="/users/${user}/mambaforge/aqua_common"
+  export INSTALLATION_PATH="$MAMBADIR/aqua_common"
 else
-  export INSTALLATION_PATH="/users/${user}/mambaforge/aqua"
+  export INSTALLATION_PATH="$MAMBADIR/aqua"
 fi
 echo "Installation path has been set to ${INSTALLATION_PATH}"
 
@@ -146,7 +154,7 @@ else
       mkdir -p "${INSTALLATION_PATH}"
     else
       echo "Deletion cancelled."
-      return 1
+      exit 1
     fi
     echo "Installing AQUA..."
     install_aqua
@@ -155,39 +163,51 @@ else
   fi
 fi
 
-# check if the line is already present in the load_aqua.sh file
-if ! grep -q 'module use /project/project_465000454/devaraju/modules/LUMI/22.08/C' ~/load_aqua.sh; then
-  # if not, append it to the end of the file
-  echo 'module use /project/project_465000454/devaraju/modules/LUMI/22.08/C' >> ~/load_aqua.sh
-  echo 'module purge' >> ~/load_aqua.sh
-  echo 'module load pyfdb/0.0.2-cpeCray-22.08' >> ~/load_aqua.sh
-  echo 'module load ecCodes/2.30.0-cpeCray-22.08' >> ~/load_aqua.sh
-  echo 'module load python-climatedt/3.11.3-cpeCray-22.08.lua' >> ~/load_aqua.sh
-  
+# check if load_aqua_file exist and clean it
+if [ -f "$load_aqua_file" ]; then
+  read -p "Existing ${load_aqua_file} found. Would you like to remove it? Safer to say yes (y/n) " -n 1 -r
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    rm $load_aqua_file
+    echo "Existing ${load_aqua_file} removed."
+  elif [[ $REPLY =~ ^[Nn]$ ]]; then
+    echo "Keeping the old $load_aqua_file"
+  else
+    echo "Invalid response. Please enter 'y' or 'n'."
+  fi
+fi
+
+#if ! grep -q 'module use /project/project_465000454/devaraju/modules/LUMI/22.08/C'  "~/load_aqua.sh" ; then
+if [ ! -f $load_aqua_file ] ; then
+  echo 'module use /project/project_465000454/devaraju/modules/LUMI/22.08/C' >> $load_aqua_file
+  echo 'module purge' >> $load_aqua_file
+  echo 'module load pyfdb/0.0.2-cpeCray-22.08' >> $load_aqua_file
+  echo 'module load ecCodes/2.30.0-cpeCray-22.08' >> $load_aqua_file
+  echo 'module load python-climatedt/3.11.3-cpeCray-22.08.lua' >> $load_aqua_file
+    
   # Config FDB: check load_modules_lumi.sh on GSV repo https://earth.bsc.es/gitlab/digital-twins/de_340/gsv_interface/-/blob/main/load_modules_lumi.sh
-  echo 'export FDB5_CONFIG_FILE=/scratch/project_465000454/igonzalez/fdb-test/config.yaml' >> ~/load_aqua.sh
+  echo 'export FDB5_CONFIG_FILE=/scratch/project_465000454/igonzalez/fdb-test/config.yaml' >>  $load_aqua_file
   echo "exports for FDB5 added to .bashrc. Please run 'source ~/.bashrc' to load the new configuration."
 
   # Config GSV: check load_modules_lumi.sh on GSV repo https://earth.bsc.es/gitlab/digital-twins/de_340/gsv_interface/-/blob/main/load_modules_lumi.sh
-  echo 'export GSV_WEIGHTS_PATH=/scratch/project_465000454/igonzalez/gsv_weights' >> ~/load_aqua.sh
-  echo 'export GSV_TEST_FILES=/scratch/project_465000454/igonzalez/gsv_test_files' >> ~/load_aqua.sh
-  echo 'export GRID_DEFINITION_PATH=/scratch/project_465000454/igonzalez/grid_definitions' >> ~/load_aqua.sh
-  echo "export for GSV has been added to .bashrc. Please run 'source ~/load_aqua.sh' to load the new configuration."
+  echo 'export GSV_WEIGHTS_PATH=/scratch/project_465000454/igonzalez/gsv_weights' >>  $load_aqua_file
+  echo 'export GSV_TEST_FILES=/scratch/project_465000454/igonzalez/gsv_test_files' >> $load_aqua_file
+  echo 'export GRID_DEFINITION_PATH=/scratch/project_465000454/igonzalez/grid_definitions' >>  $load_aqua_file
+  echo "export for GSV has been added to .bashrc. Please run 'source  $load_aqua_file' to load the new configuration."
 
   # Install path
-  echo "# AQUA installation path" >> ~/load_aqua.sh
-  echo 'export PATH="'$INSTALLATION_PATH'/bin:$PATH"' >> ~/load_aqua.sh
-  echo "export PATH has been added to .bashrc. Please run 'source ~/load_aqua.sh' to load the new configuration."
+  echo "# AQUA installation path" >>  $load_aqua_file
+  echo 'export PATH="'$INSTALLATION_PATH'/bin:$PATH"' >>  $load_aqua_file
+  echo "export PATH has been added to .bashrc. Please run 'source $load_aqua_file' to load the new configuration."
 else
-  echo "A load_aqua.sh is already available in your home!"
+  echo "A $(basename $load_aqua_file) is already available in your home. Nothing to add!"
 fi
 
 # ask if you want to add this to the bash profile
-read -p "Would you like to source load_aqua.sh in your .bash_profile? (y/n) " -n 1 -r
+read -p "Would you like to source $load_aqua_file in your .bash_profile? (y/n) " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  if ! grep -q 'source  ~/load_aqua.sh' ~/.bash_profile; then
-    echo 'source  ~/load_aqua.sh' >> ~/.bash_profile
+  if ! grep -q "source  $load_aqua_file" ~/.bash_profile; then
+    echo "source  $load_aqua_file" >> ~/.bash_profile
     echo 'load_aqua.sh added to your .bash_profile.'
   else
     echo 'load_aqua.sh is already in your bash profile, not adding it again!'
