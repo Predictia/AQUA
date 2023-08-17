aeb7067565c2207b45a2ef87f5d0fd3c5f12c888
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-08-17 15:27:45 +0200

    class adapted to namelist loading

--- diagnostics/teleconnections/teleconnections/tc_class.py
+++ diagnostics/teleconnections/teleconnections/tc_class.py
@@ -19,7 +19,7 @@ from aqua.reader import Reader
 from teleconnections.index import station_based_index, regional_mean_anomalies
 from teleconnections.plots import index_plot
 from teleconnections.statistics import reg_evaluation, cor_evaluation
-from teleconnections.tools import load_namelist
+from teleconnections.tools import TeleconnectionsConfig
 
 
 class Teleconnection():
@@ -136,9 +136,9 @@ class Teleconnection():
             configdir (str, optional): Path to diagnostics configuration folder.
                                        If None, the default diagnostics folder is used.
         """
+        config = TeleconnectionsConfig(configdir=configdir)
 
-        self.namelist = load_namelist(diagname='teleconnections',
-                                      configdir=configdir)
+        self.namelist = config.load_namelist()
         self.logger.info('Namelist loaded')
 
     def _reader(self, **kwargs):
