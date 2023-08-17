9ae537aab20e72d7b50487de6241f5ce98a376b8
Jost von Hardenberg <hardenberg@gmail.com>
2023-08-17 15:25:46 +0200

    init imports

--- aqua/gsv/__init__.py
+++ aqua/gsv/__init__.py
@@ -2,7 +2,6 @@ import intake  # Import this first to avoid circular imports during discovery.
 # from intake.container import register_container
 
 from .intake_gsv import GSVSource
-from .timeutil import compute_date, compute_date_steps
 
 try:
     intake.registry.drivers.register_driver('gsv', GSVSource)
