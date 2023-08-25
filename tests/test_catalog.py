5173cef44352de5c2772b001eed3bc00264ddaf4
Jost von Hardenberg <hardenberg@gmail.com>
2023-08-25 10:56:20 +0200

    skip test fdb in catalog

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -23,6 +23,8 @@ def reader(request):
         pytest.skip()
     if model == 'ERA5':
         pytest.skip()
+    if model == 'IFS' and source == 'fdb':  # there is another test for that
+        pytest.skip()
     myread = Reader(model=model, exp=exp, source=source, areas=False,
                     fix=False)
     data = myread.retrieve()
