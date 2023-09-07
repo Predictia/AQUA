4b84875411ebe932213fd377cf2db50fbf742dee
Matteo Nurisso <m.nurisso@isac.cnr.it>
2023-09-07 14:58:57 +0200

    sci_tool tests first tentative

--- tests/test_sci_tools.py
+++ tests/test_sci_tools.py
@@ -6,58 +6,79 @@ from aqua.util import area_selection
 
 @pytest.fixture
 def sample_data():
-    # Create a sample DataArray for testing
+    """Create a sample DataArray for testing"""
     data = xr.DataArray(
-        np.random.rand(6,6),
+        np.random.rand(6, 6),
         coords={'lat': [10, 20, 30, 40, 50, 60], 'lon': [40, 50, 60, 70, 80, 90]},
         dims=['lat', 'lon']
     )
     return data
 
-def test_valid_selection(sample_data):
-    # Test with valid latitude and longitude ranges
-    lat_range = [15, 25]
+@pytest.mark.aqua
+def test_valid_selection_no_brd(sample_data):
+    """Test with valid latitude and longitude ranges, no box_brd"""
+    lat_range = [10, 30]
     lon_range = [45, 55]
-    result = area_selection(sample_data, lat=lat_range, lon=lon_range, box_brd=True)
+    result = area_selection(sample_data, lat=lat_range, lon=lon_range,
+                            box_brd=False)
 
     assert result is not None
-    assert np.isnan(result.sel(lat=10))
-    #assert result.lon.values.tolist() == [50]
+    assert np.isnan(result.sel(lat=10, lon=40).values)
+    assert np.isnan(result.sel(lat=60, lon=90).values)
 
-def test_invalid_lat_lon(sample_data):
-    # Test with invalid latitude and longitude ranges
-    lat_range = [25, 15]  # Invalid, should raise a ValueError
-    lon_range = [55, 45]  # Invalid, should raise a ValueError
 
-    with pytest.raises(ValueError):
-        area_selection(sample_data, lat=lat_range, lon=lon_range, box_brd=True)
+@pytest.mark.aqua
+def test_valid_selection(sample_data):
+    """Test with valid latitude and longitude ranges"""
+    lat_range = [15, 25]
+    lon_range = [45, 55]
+    result = area_selection(sample_data, lat=lat_range, lon=lon_range,
+                            box_brd=True)
 
+    assert result is not None
+    assert np.isnan(result.sel(lat=10, lon=40).values)
+    assert np.isnan(result.sel(lat=60, lon=90).values)
+
+
+# TODO: check behaviour if lat or lon are None in the source code
+# @pytest.mark.aqua
+# def test_invalid_lat_lon(sample_data):
+#     """Test with invalid latitude and longitude ranges"""
+#     lat_range = [10, 70]  # Invalid, should raise a ValueError
+
+#     with pytest.raises(ValueError):
+#         area_selection(sample_data, lat=lat_range, lon=None, box_brd=True)
+
+
+@pytest.mark.aqua
 def test_missing_lat_lon(sample_data):
-    # Test when both lat and lon are None, should raise a ValueError
+    """Test when both lat and lon are None, should raise a ValueError"""
     with pytest.raises(ValueError):
         area_selection(sample_data, lat=None, lon=None, box_brd=True)
 
-def test_invalid_lat_order(sample_data):
-    # Test with lat in descending order, should raise a ValueError
-    lat_range = [25, 15]
 
+@pytest.mark.aqua
+def test_missing_lat_lon_coords():
+    """Test with missing lat or lon coordinates, should raise an KeyError"""
+    data_missing_lat = xr.DataArray(np.random.rand(3, 3),
+                                    coords={'lon': [40, 50, 60]},
+                                    dims=['lat', 'lon'])
+    data_missing_lon = xr.DataArray(np.random.rand(3, 3),
+                                    coords={'lat': [10, 20, 30]},
+                                    dims=['lat', 'lon'])
+
+    with pytest.raises(KeyError):
+        area_selection(data_missing_lat, lat=[15, 25], lon=[45, 55],
+                       box_brd=True)
+
+    with pytest.raises(KeyError):
+        area_selection(data_missing_lon, lat=[15, 25], lon=[45, 55],
+                       box_brd=True)
+
+
+@pytest.mark.aqua
+def test_missing_data():
+    """Test with missing data, should raise a ValueError"""
     with pytest.raises(ValueError):
-        area_selection(sample_data, lat=lat_range, lon=[45, 55], box_brd=True)
-
-def test_invalid_lon_order(sample_data):
-    # Test with lon in descending order, should raise a ValueError
-    lon_range = [55, 45]
-
-    with pytest.raises(ValueError):
-        area_selection(sample_data, lat=[15, 25], lon=lon_range, box_brd=True)
-
-def test_missing_lat_lon_coords(sample_data):
-    # Test with missing lat or lon coordinates, should raise an AttributeError
-    data_missing_lat = xr.DataArray(np.random.rand(3, 3), coords={'lon': [40, 50, 60]}, dims=['lat', 'lon'])
-    data_missing_lon = xr.DataArray(np.random.rand(3, 3), coords={'lat': [10, 20, 30]}, dims=['lat', 'lon'])
-
-    with pytest.raises(AttributeError):
-        area_selection(data_missing_lat, lat=[15, 25], lon=[45, 55], box_brd=True)
-
-    with pytest.raises(AttributeError):
-        area_selection(data_missing_lon, lat=[15, 25], lon=[45, 55], box_brd=True)
+        area_selection(None, lat=[15, 25], lon=[45, 55],
+                       box_brd=True)
