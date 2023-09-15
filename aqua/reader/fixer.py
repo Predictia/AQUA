e94b29b408d5e595ed68afe6ec2b9b2f77afa8a0
Jost von Hardenberg <hardenberg@gmail.com>
2023-09-16 00:49:34 +0300

    add hourly-native and update merge

--- aqua/reader/fixer.py
+++ aqua/reader/fixer.py
@@ -66,7 +66,8 @@ class FixerMixin():
             final_fixes = default_fixes
             for item in fixes.keys():
                 if item == 'vars':
-                    final_fixes[item] = {**default_fixes[item], **fixes[item]}
+                    final_fixes[item] = default_fixes[item].copy()
+                    final_fixes[item].update(fixes[item])
                 else:
                     final_fixes[item] = fixes[item]
 
