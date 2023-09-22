295f36044d2a7809c9ff8ba088dfea906d3f859f
mnurisso <m.nurisso@isac.cnr.it>
2023-09-22 11:33:54 +0300

    load_multi extended

--- aqua/util/yaml.py
+++ aqua/util/yaml.py
@@ -8,41 +8,42 @@ from string import Template
 import xarray as xr
 from collections import defaultdict
 from ruamel.yaml import YAML
+from aqua.logger import log_configure
 
 
-def load_multi_yaml(folder_path, definitions=None):
+def load_multi_yaml(folder_path, definitions=None,
+                    filename=None, **kwargs):
     """
     Load and merge all yaml files located in a given folder
     into a single dictionary.
 
     Args:
-        folder_path(str):   The path of the folder containing the yaml
-                            files to be merged.
-        definitions (str or dict, optional): name of the section containing string template
-                                             definitions or a dictionary with the same
+        folder_path(st or list):   The path of the folders containing the yaml
+                                   files to be merged.
+        definitions (str or dict, opt): name of the section containing string template
+                                        definitions or a dictionary with the same
+        filename (str, optional):   the name of the file to be merged.
+                                    If None, all the yaml files in the folders will be merged.
+                                    If a string is passed, only the yaml file with
+                                    the matching name will be merged.
+
+    Keyword Args:
+        loglevel (str, optional): the loglevel to be used, default is 'WARNING'
 
     Returns:
         A dictionary containing the merged contents of all the yaml files.
     """
     yaml = YAML()  # default, if not specified, is 'rt' (round-trip)
 
-    # Helper function to read combined yaml
-    def load_merge(folder_path, definitions):
-        merged_dict = defaultdict(dict)
-        for filename in os.listdir(folder_path):
-            if filename.endswith(('.yml', '.yaml')):
-                file_path = os.path.join(folder_path, filename)
-                yaml_dict = load_yaml(file_path, definitions)
-                for key, value in yaml_dict.items():
-                    merged_dict[key].update(value)
-        return dict(merged_dict)
-    
     if isinstance(definitions, str):  # if definitions is a string we need to read twice
-        yaml_dict = load_merge(folder_path, None)
+        yaml_dict = _load_merge(folder_path=folder_path, definitions=None,
+                                filename=filename, **kwargs)  # Read without definitions
         definitions = yaml_dict.get(definitions)
-        yaml_dict = load_merge(folder_path, definitions)  # Read again with definitions
+        yaml_dict = _load_merge(folder_path, definitions,
+                                filename=filename, **kwargs)  # Read again with definitions
     else:  # if a dictionary or None has been passed for definitions we read only once
-        yaml_dict = load_merge(folder_path, definitions)
+        yaml_dict = _load_merge(folder_path, definitions,
+                                filename=filename, **kwargs)
 
     return yaml_dict
 
@@ -58,12 +59,12 @@ def load_yaml(infile, definitions=None):
     Returns:
         A dictionary with the yaml file keys
     """
-    
+
     if not os.path.exists(infile):
-        sys.exit(f'ERROR: {infile} not found: you need to have this configuration file!')
-         
+        raise ValueError(f'ERROR: {infile} not found: you need to have this configuration file!')
+
     yaml = YAML(typ='rt')  # default, if not specified, is 'rt' (round-trip)
-    
+
     cfg = None
     # Load the YAML file as a text string
     with open(infile, 'r', encoding='utf-8') as file:
@@ -72,7 +73,7 @@ def load_yaml(infile, definitions=None):
     if isinstance(definitions, str):  # if it is a string extract from original yaml, else it is directly a dict
         cfg = yaml.load(yaml_text)
         definitions = cfg.get(definitions)
-    
+
     if definitions:
         # perform template substitution
         template = Template(yaml_text).safe_substitute(definitions)
@@ -166,3 +167,66 @@ def _operation(token, xdataset):
             token[x - 1] = name
             del token[x:x + 2]
     return replacer
+
+
+def _load_merge(folder_path,
+                definitions,
+                filename=None,
+                merged_dict=None,
+                loglevel='WARNING'):
+    """
+    Helper function for load_merge_yaml.
+    Load and merge yaml files located in a given folder
+    into a single dictionary.
+
+    It can merge specific file if a filename is provided
+    and it can create a new dictionary or update an existing one,
+    if merged_dict is provided.
+
+    Args:
+        folder_path(str or list):   The path of the folders containing the yaml
+                                    files to be merged.
+        definitions (str or dict, optional): name of the section containing string template
+                                                definitions or a dictionary with the same
+        filename (str, optional): the name of the file to be merged. If None, all the yaml
+                                  files in the folders will be merged.
+                                  If a string is passed, only the yaml file with
+                                  the matching name will be merged.
+        merged_dict (dict, optional): the dictionary to be updated
+        loglevel (str, optional): the loglevel to be used
+
+    Returns:
+        A dictionary containing the merged contents of all the yaml files.
+    """
+    logger = log_configure(log_name='yaml', log_level=loglevel)
+
+    if merged_dict is None:
+        logger.debug('Creating a new dictionary')
+        merged_dict = defaultdict(dict)
+    else:
+        logger.debug('Updating an existing dictionary')
+
+    if isinstance(folder_path, str):
+        folder_path = [folder_path]
+
+    for folder in folder_path:
+        if not os.path.exists(folder):
+            raise ValueError(f'ERROR: {folder} not found: it is required to have this folder!')
+        else:  # folder exists
+            if filename is None:  # Merging all the existing yaml files in the folder
+                for filename in os.listdir(folder):
+                    if filename.endswith(('.yml', '.yaml')):
+                        file_path = os.path.join(folder, filename)
+                        yaml_dict = load_yaml(file_path, definitions)
+                        for key, value in yaml_dict.items():
+                            merged_dict[key].update(value)
+            else:  # Merging a specific file
+                file_path = os.path.join(folder, filename)
+                yaml_dict = load_yaml(file_path, definitions)
+                for key, value in yaml_dict.items():
+                    merged_dict[key].update(value)
+
+    logger.debug('Dictionary updated')
+    logger.debug(f'Keys: {merged_dict.keys()}')
+
+    return dict(merged_dict)
