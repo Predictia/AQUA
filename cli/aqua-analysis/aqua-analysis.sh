a0d68192573d4ede8f8aceb3300e4ab24c28e940
mnurisso <m.nurisso@isac.cnr.it>
2023-09-28 11:10:38 +0300

    squeeze arguments

--- cli/aqua-analysis/aqua-analysis.sh
+++ cli/aqua-analysis/aqua-analysis.sh
@@ -25,6 +25,8 @@ teleconnection=false
 # End of user defined variables
 # -----------------------------
 
+args="--model $model --exp $exp --source $source"
+
 # use $AQUA if defined otherwise use aqua
 if [[ -z "${AQUA}" ]]; then
   echo "AQUA path is not defined, using user defined aqua in the script"
@@ -35,42 +37,42 @@ fi
 
 if [ "$atmglobalmean" = true ] ; then
   echo "Running atmglobalmean"
-  python $aqua/diagnostics/atmglobalmean/cli/cli_atmglobalmean.py --model $model --exp $exp --source $source --outputdir $outputdir/atmglobalmean
+  python $aqua/diagnostics/atmglobalmean/cli/cli_atmglobalmean.py $args --outputdir $outputdir/atmglobalmean
 fi
 
 if [ "$dummy" = true ] ; then
   echo "Running dummy"
-  python $aqua/diagnostics/dummy/cli/cli_dummy.py --model $model --exp $exp --source $source --outputdir $outputdir/dummy
+  python $aqua/diagnostics/dummy/cli/cli_dummy.py $args --outputdir $outputdir/dummy
 fi
 
 if [ "$ecmean" = true ] ; then
   echo "Running ecmean"
-  python $aqua/diagnostics/ecmean/cli/cli_ecmean.py --model $model --exp $exp --source $source --outputdir $outputdir/ecmean
+  python $aqua/diagnostics/ecmean/cli/cli_ecmean.py $args --outputdir $outputdir/ecmean
 fi
 
 if [ "$global_time_series" = true ] ; then
   echo "Running global_time_series"
-  python $aqua/diagnostics/global_time_series/cli/cli_global_time_series.py --model $model --exp $exp --source $source --outputdir $outputdir/global_time_series
+  python $aqua/diagnostics/global_time_series/cli/cli_global_time_series.py $args --outputdir $outputdir/global_time_series
 fi
 
 if [ "$ocean3d" = true ] ; then
   echo "Running ocean3d"
-  python $aqua/diagnostics/ocean3d/cli/cli_ocean3d.py --model $model --exp $exp --source $source --outputdir $outputdir/ocean3d
+  python $aqua/diagnostics/ocean3d/cli/cli_ocean3d.py $args --outputdir $outputdir/ocean3d
 fi
 
 if [ "$radiation" = true ] ; then
   echo "Running radiation"
-  python $aqua/diagnostics/radiation/cli/cli_radiation.py --model $model --exp $exp --source $source --outputdir $outputdir/radiation
+  python $aqua/diagnostics/radiation/cli/cli_radiation.py $args --outputdir $outputdir/radiation
 fi
 
 if [ "$seaice" = true ] ; then
   echo "Running seaice"
-  python $aqua/diagnostics/seaice/cli/cli_seaice.py --model $model --exp $exp --source $source --outputdir $outputdir/seaice
+  python $aqua/diagnostics/seaice/cli/cli_seaice.py $args --outputdir $outputdir/seaice
 fi
 
 if [ "$teleconnection" = true ] ; then
   echo "Running teleconnection"
-  python $aqua/diagnostics/teleconnection/cli/cli_teleconnection.py --model $model --exp $exp --source $source --outputdir $outputdir/teleconnection
+  python $aqua/diagnostics/teleconnection/cli/cli_teleconnection.py $args --outputdir $outputdir/teleconnection
 fi
 
 echo "Finished"
