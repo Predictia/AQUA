e85f723a0e1ff41e243ebbe0426dc602a0a3a5e0
Matteo Nurisso <m.nurisso@isac.cnr.it>
2023-10-17 17:32:45 +0200

    removed duplicate single map plot function

--- diagnostics/teleconnections/teleconnections/tools/__init__.py
+++ diagnostics/teleconnections/teleconnections/tools/__init__.py
@@ -1,3 +1,4 @@
+"""Tools for teleconnections diagnostics."""
 from .cli_tools import get_dataset_config
 from .sci_tools import area_selection, wgt_area_mean
 from .sci_tools import lon_180_to_360, lon_360_to_180
