81d3a2c2529a9c0ba15ae6687e4546b57dc4db81
Paolo Ghinassi <p.ghinassi@isac.cnr.it>
2023-10-24 16:32:48 +0200

    version removed

--- diagnostics/tropical_cyclones/tropical_cyclones/plots/__init__.py
+++ diagnostics/tropical_cyclones/tropical_cyclones/plots/__init__.py
@@ -2,8 +2,5 @@
 
 from .plotting_TCs import multi_plot, plot_trajectories
 
-# Optional but recommended
-__version__ = '0.1.0'
-
 # This specifies which methods are exported publicly, used by "from tropical cyclones import *"
 __all__ = ["multi_plot", "plot_trajectories"]
