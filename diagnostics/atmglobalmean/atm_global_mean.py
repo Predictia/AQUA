2763ad9a29e39004a12c986d2fcc53bdc0820ea1
Natalia Nazarova <natalia.nazarova@polito.it>
2023-10-25 15:38:05 +0200

    The time selection is modified to the optional. The 'try/except' block is added to the code, which involves the 'plev' coordinate.

--- diagnostics/atmglobalmean/atm_global_mean.py
+++ diagnostics/atmglobalmean/atm_global_mean.py
@@ -12,9 +12,13 @@ from aqua import Reader
 import pandas as pd
 import datetime
 from aqua.util import create_folder
+from aqua.logger import log_configure
 
+loglevel: str = 'debug'
+logger = log_configure(log_level=loglevel, log_name='Atmglobalmean')
 
-def seasonal_bias(dataset1, dataset2, var_name, plev, statistic, model_label1, model_label2, start_date1, end_date1, start_date2, end_date2, outputdir, outputfig):
+def seasonal_bias(dataset1=None, dataset2=None, var_name=None, plev=None, statistic=None, model_label1=None, model_label2=None, 
+                  start_date1=None, end_date1=None, start_date2=None, end_date2=None, outputdir=None, outputfig=None):
     '''
     Plot the seasonal bias maps between two datasets for specific variable and time ranges.
 
@@ -41,13 +45,21 @@ def seasonal_bias(dataset1, dataset2, var_name, plev, statistic, model_label1, m
     var1 = dataset1[var_name]
     var2 = dataset2[var_name]
 
-    var1_climatology = var1.sel(time=slice(start_date1, end_date1)).groupby('time.month').mean(dim='time')
-    var2_climatology = var2.sel(time=slice(start_date2, end_date2)).groupby('time.month').mean(dim='time')
+    if start_date1 is not None or end_date1 is not None:
+        var1 = var1.sel(time=slice(start_date1, end_date1))
+    if start_date2 is not None or end_date2 is not None:
+        var2 = var2.sel(time=slice(start_date2, end_date2))
+
+    var1_climatology = var1.groupby('time.month').mean(dim='time')
+    var2_climatology = var2.groupby('time.month').mean(dim='time')
 
     # Select the desired pressure level if provided
-    if plev is not None:
+    #if plev is not None:
+    try:
         var1_climatology = var1_climatology.sel(plev=plev)
         var2_climatology = var2_climatology.sel(plev=plev)
+    except KeyError:
+        logger.warning("The dataset does not have a 'plev' coordinate.")
 
     # Calculate the desired statistic for each season
     season_ranges = {'DJF': [12, 1, 2], 'MAM': [3, 4, 5], 'JJA': [6, 7, 8], 'SON': [9, 10, 11]}
@@ -132,36 +144,35 @@ def seasonal_bias(dataset1, dataset2, var_name, plev, statistic, model_label1, m
     fig.suptitle(overall_title, fontsize=14, fontweight='bold')
     plt.subplots_adjust(hspace=0.5)
 
-    create_folder(folder=str(outputfig), loglevel='WARNING')
+    if outputfig is not None:
+        create_folder(folder=str(outputfig), loglevel='WARNING')
+        # Save the figure
+        filename = f"{outputfig}Seasonal_Bias_Plot_{model_label1}_{var_name}_{statistic}_{start_date1}_{end_date1}_{start_date2}_{end_date2}.pdf"
+        plt.savefig(filename, dpi=300, format='pdf')
+        logger.info(f"The seasonal bias plots have been saved to {outputfig}.")
+    else:
+        plt.show()
 
-    # Save the figure
-    filename = f"{outputfig}Seasonal_Bias_Plot_{model_label1}_{var_name}_{statistic}_{start_date1}_{end_date1}_{start_date2}_{end_date2}.pdf"
-    plt.savefig(filename, dpi=300, format='pdf')
-    plt.show()
+    if outputdir is not None:
+        create_folder(folder=str(outputdir), loglevel='WARNING')
+        # Write the data into a NetCDF file
+        data_directory = outputdir
+        data_filename = f"Seasonal_Bias_Data_{model_label1}_{var_name}_{statistic}_{start_date1}_{end_date1}_{start_date2}_{end_date2}.nc"
+        data_path = os.path.join(data_directory, data_filename)
 
-    create_folder(folder=str(outputdir), loglevel='WARNING')
-    # Write the data into a NetCDF file
-    data_directory = outputdir
-    data_filename = f"Seasonal_Bias_Data_{model_label1}_{var_name}_{statistic}_{start_date1}_{end_date1}_{start_date2}_{end_date2}.nc"
-    data_path = os.path.join(data_directory, data_filename)
+        data_array = xr.concat(results, dim='season')
+        data_array.attrs = var1.attrs  # Copy attributes from var1 to the data_array
+        data_array.attrs['statistic'] = statistic
+        data_array.attrs['dataset1'] = model_label1
+        data_array.attrs['dataset2'] = model_label2
+        data_array.attrs['climatology_range1'] = f'{start_date1}-{end_date1}'
+        data_array.attrs['climatology_range2'] = f'{start_date2}-{end_date2}'
 
-    data_array = xr.concat(results, dim='season')
-    data_array.attrs = var1.attrs  # Copy attributes from var1 to the data_array
-    data_array.attrs['statistic'] = statistic
-    data_array.attrs['dataset1'] = model_label1
-    data_array.attrs['dataset2'] = model_label2
-    data_array.attrs['climatology_range1'] = f'{start_date1}-{end_date1}'
-    data_array.attrs['climatology_range2'] = f'{start_date2}-{end_date2}'
+        data_array.to_netcdf(data_path)
+        logger.info(f"The seasonal bias data has been saved to {outputdir}.")
 
-    data_array.to_netcdf(data_path)
-
-    print(f"The seasonal bias plots have been saved to {outputfig}.")
-    print(f"The seasonal bias data has been saved to {outputdir}.")
-
-#---------------------------------------------------------------------------------------------------------------------------------------
-#---------------------------------------------------------------------------------------------------------------------------------------
-
-def compare_datasets_plev(dataset1, dataset2, var_name, start_date1, end_date1, start_date2, end_date2, model_label1, model_label2, outputdir, outputfig):
+def compare_datasets_plev(dataset1=None, dataset2=None, var_name=None, start_date1=None, end_date1=None, 
+                          start_date2=None, end_date2=None, model_label1=None, model_label2=None, outputdir=None, outputfig=None):
     """
     Compare two datasets and plot the zonal bias for a selected model time range with respect to the second dataset.
 
@@ -179,64 +190,75 @@ def compare_datasets_plev(dataset1, dataset2, var_name, start_date1, end_date1, 
         A zonal bias plot.
     """
     # Convert start and end dates to datetime objects
-    start1 = datetime.datetime.strptime(start_date1, "%Y-%m-%d")
-    end1 = datetime.datetime.strptime(end_date1, "%Y-%m-%d")
-    start2 = datetime.datetime.strptime(start_date2, "%Y-%m-%d")
-    end2 = datetime.datetime.strptime(end_date2, "%Y-%m-%d")
-
+    if start_date1 is not None or end_date1 is not None:
+        start1 = datetime.datetime.strptime(start_date1, "%Y-%m-%d")
+        end1 = datetime.datetime.strptime(end_date1, "%Y-%m-%d")
+    else:
+        start1 = str(dataset1["time.year"][0].values) if len(dataset1.sel(time=str(dataset1["time.year"][0].values)).time) == 12 \
+                    else str(dataset1["time.year"][0].values + 1)
+        end1 = str(dataset1["time.year"][-1].values) if len(dataset1.sel(time=str(dataset1["time.year"][-1].values)).time) == 12 \
+                 else str(dataset1["time.year"][-1].values -1)
     # Select the data for the given time ranges
     dataset1 = dataset1.sel(time=slice(start1, end1))
-    dataset2 = dataset2.sel(time=slice(start2, end2))
+
+    if start_date2 is not None or end_date2 is not None:
+        start2 = datetime.datetime.strptime(start_date2, "%Y-%m-%d")
+        end2 = datetime.datetime.strptime(end_date2, "%Y-%m-%d")
+    else:
+        start2 = str(dataset2["time.year"][0].values) if len(dataset2.sel(time=str(dataset2["time.year"][0].values)).time) == 12 \
+                    else str(dataset2["time.year"][0].values + 1)
+        end2 = str(dataset2["time.year"][-1].values) if len(dataset2.sel(time=str(dataset2["time.year"][-1].values)).time) == 12 \
+                 else str(dataset2["time.year"][-1].values -1)
+    dataset2 = dataset2.sel(time=slice(start2, end2)) 
 
     # Calculate the bias between dataset1 and dataset2
     bias = dataset1[var_name] - dataset2[var_name].mean(dim='time')
 
-    # Get the pressure levels and coordinate values
-    plev = bias['plev'].values
-    coord_values = bias['lat'].values
+    try:
+        # Get the pressure levels and coordinate values
+        plev = bias['plev'].values
+        coord_values = bias['lat'].values
+        # Calculate the mean bias along the time axis
+        mean_bias = bias.mean(dim='time')
 
-    # Calculate the mean bias along the time axis
-    mean_bias = bias.mean(dim='time')
+        # Create the z-values for the contour plot
+        coord_values_2d, plev_2d = np.meshgrid(coord_values, plev)
+        z_values = np.mean(mean_bias, axis=2)
 
-    # Create the z-values for the contour plot
-    coord_values_2d, plev_2d = np.meshgrid(coord_values, plev)
-    z_values = np.mean(mean_bias, axis=2)
+        # Create the plot
+        fig, ax = plt.subplots(figsize=(10, 8))
+        cax = ax.contourf(coord_values_2d, plev_2d, z_values, cmap='RdBu_r')
+        ax.set_title(f'Bias of {var_name} Experiment {model_label1} with respect to {model_label2} \n Selected model time range: {start_date1} to {end_date1}. Reference time range: {start_date2} to {end_date2}')
+        ax.set_yscale('log')
+        ax.set_ylabel('Pressure Level (Pa)')
+        ax.set_xlabel('Latitude')
+        ax.invert_yaxis()
+        ax.set_xlim(-90, 90)
 
-    create_folder(folder=str(outputfig), loglevel='WARNING')
-    # Create the plot
-    fig, ax = plt.subplots(figsize=(10, 8))
-    cax = ax.contourf(coord_values_2d, plev_2d, z_values, cmap='RdBu_r')
-    ax.set_title(f'Bias of {var_name} Experiment {model_label1} with respect to {model_label2} \n Selected model time range: {start_date1} to {end_date1}. Reference time range: {start_date2} to {end_date2}')
-    ax.set_yscale('log')
-    ax.set_ylabel('Pressure Level (Pa)')
-    ax.set_xlabel('Latitude')
-    ax.invert_yaxis()
-    ax.set_xlim(-90, 90)
+        # Add colorbar
+        cbar = fig.colorbar(cax)
+        cbar.set_label(f'{var_name} [{dataset1[var_name].units}]')
 
-    # Add colorbar
-    cbar = fig.colorbar(cax)
-    cbar.set_label(f'{var_name} [{dataset1[var_name].units}]')
+        if outputfig is not None:
+            create_folder(folder=str(outputfig), loglevel='WARNING')
+            # Save the plot as a PDF file
+            filename = f"Vertical_biases_{model_label1}_{model_label2}_{var_name}_{start_date1}_{end_date1}_{start_date2}_{end_date2}.pdf"
+            output_path = os.path.join(outputfig, filename)
+            plt.savefig(output_path, dpi=300, format='pdf')
+            logger.info(f"The zonal bias plot for a selected models have been saved to {outputfig}.")
+        else:
+            plt.show()
 
-    # Save the plot as a PDF file
-    filename = f"Vertical_biases_{model_label1}_{model_label2}_{var_name}_{start_date1}_{end_date1}_{start_date2}_{end_date2}.pdf"
-    output_path = os.path.join(outputfig, filename)
-    plt.savefig(output_path, dpi=300, format='pdf')
+        if outputdir is not None:
+            create_folder(folder=str(outputdir), loglevel='WARNING')
+            # Save the data into a NetCDF file
+            filename = f"{outputdir}/Vertical_bias_{model_label1}_{model_label2}_{var_name}_{start_date1}_{end_date1}_{start_date2}_{end_date2}.nc"
+            mean_bias.to_netcdf(filename)
+            logger.info(f"The zonal bias for a selected models has been saved to {outputdir}.")
+    except KeyError:
+        logger.warning("The dataset does not have a 'plev' coordinate. The function 'compare_datasets_plev' is terminated.")
 
-    create_folder(folder=str(outputdir), loglevel='WARNING')
-    # Save the data into a NetCDF file
-    filename = f"{outputdir}/Vertical_bias_{model_label1}_{model_label2}_{var_name}_{start_date1}_{end_date1}_{start_date2}_{end_date2}.nc"
-    mean_bias.to_netcdf(filename)
-
-    plt.show()
-    print(f"The vertical bias plots have been saved to {outputfig}.")
-    print(f"The vertical bias data has been saved to {outputdir}.")
-
-#---------------------------------------------------------------------------------------------------------------------------------------
-#---------------------------------------------------------------------------------------------------------------------------------------
-
-
-
-def plot_map_with_stats(dataset, var_name, start_date, end_date, model_label, outputdir, outputfig):
+def plot_map_with_stats(dataset=None, var_name=None, start_date=None, end_date=None, model_label=None, outputdir=None, outputfig=None):
     """
     Plot a map of a chosen variable from a dataset with colorbar and statistics.
 
@@ -248,9 +270,14 @@ def plot_map_with_stats(dataset, var_name, start_date, end_date, model_label, ou
         model_label (str): The label for the model.
         outputdir (str): The directory to save the output files.
     """
-    start_date = pd.to_datetime(start_date)
-    end_date = pd.to_datetime(end_date)
-
+    if start_date is not None or end_date is not None:
+        start_date = pd.to_datetime(start_date)
+        end_date = pd.to_datetime(end_date)
+    else:
+        start_date = str(dataset["time.year"][0].values) if len(dataset.sel(time=str(dataset["time.year"][0].values)).time) == 12 \
+                    else str(dataset["time.year"][0].values + 1)
+        end_date = str(dataset["time.year"][-1].values) if len(dataset.sel(time=str(dataset["time.year"][-1].values)).time) == 12 \
+                 else str(dataset["time.year"][-1].values -1)
     # Calculate statistics
     var_data = dataset[var_name].sel(time=slice(start_date, end_date)).mean(dim='time')
     weights = np.cos(np.deg2rad(dataset.lat))
@@ -259,15 +286,14 @@ def plot_map_with_stats(dataset, var_name, start_date, end_date, model_label, ou
     var_std = var_data.std().values.item()
     var_min = var_data.min().values.item()
     var_max = var_data.max().values.item()
-
+    
     # Plot the map
     fig = plt.figure(figsize=(10, 8))
     ax = plt.axes(projection=ccrs.PlateCarree())
     cmap = 'RdBu_r'  # Choose a colormap (reversed)
     im = ax.pcolormesh(var_data.lon, var_data.lat, var_data.values, cmap=cmap, transform=ccrs.PlateCarree())
-
     # Set plot title and axis labels
-    ax.set_title(f'Map of {var_name} for {model_label} from {start_date.strftime("%Y-%m-%d")} to {end_date.strftime("%Y-%m-%d")}')
+    ax.set_title(f'Map of {var_name} for {model_label} from {pd.to_datetime(start_date).strftime("%Y-%m-%d")} to {pd.to_datetime(end_date).strftime("%Y-%m-%d")}')
     ax.set_xlabel('Longitude')
     ax.set_ylabel('Latitude')
 
@@ -284,23 +310,25 @@ def plot_map_with_stats(dataset, var_name, start_date, end_date, model_label, ou
     stat_text = f'Mean: {var_mean:.2f} {dataset[var_name].units}    Std: {var_std:.2f}    Min: {var_min:.2f}    Max: {var_max:.2f}'
     ax.text(0.5, -0.3, stat_text, transform=ax.transAxes, ha='center')
 
-    create_folder(folder=str(outputfig), loglevel='WARNING')
-    # Save the plot as a PDF file
-    filename = f"Statistics_maps_{model_label}_{var_name}.pdf"
-    output_path = os.path.join(outputfig, filename)
-    plt.savefig(output_path, dpi=300, format='pdf')
+    if outputfig is not None:
+        create_folder(folder=str(outputfig), loglevel='WARNING')
+        # Save the plot as a PDF file
+        filename = f"Statistics_maps_{model_label}_{var_name}.pdf"
+        output_path = os.path.join(outputfig, filename)
+        plt.savefig(output_path, dpi=300, format='pdf')
+        logger.info(f"Plot a map of a chosen variable have been saved to {outputfig}.")
+    else:
+        plt.show()
 
-    create_folder(folder=str(outputdir), loglevel='WARNING')
-    # Save the data into a NetCDF file
-    data_filename = f"Statistics_Data_{model_label}_{var_name}.nc"
-    data_path = os.path.join(outputdir, data_filename)
+    if outputdir is not None:
+        create_folder(folder=str(outputdir), loglevel='WARNING')
+        # Save the data into a NetCDF file
+        data_filename = f"Statistics_Data_{model_label}_{var_name}.nc"
+        data_path = os.path.join(outputdir, data_filename)
 
-    data_array = var_data.to_dataset(name=var_name)
-    data_array.attrs = dataset[var_name].attrs  # Copy attributes from the original dataset
-    data_array.attrs['model_label'] = model_label
+        data_array = var_data.to_dataset(name=var_name)
+        data_array.attrs = dataset[var_name].attrs  # Copy attributes from the original dataset
+        data_array.attrs['model_label'] = model_label
 
-    data_array.to_netcdf(data_path)
- 
-    plt.show()
-    print(f"The plot has been saved to {outputfig}.")
-    print(f"The data has been saved to {outputdir}.")
+        data_array.to_netcdf(data_path)
+        logger.info(f"A chosen variable has been saved to {outputdir}.")
