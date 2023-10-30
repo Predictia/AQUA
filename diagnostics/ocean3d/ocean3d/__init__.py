00f778e6d6949a75f527263a6344753f9f952691
Matteo Nurisso <matteo.nurisso@gmail.com>
2023-10-30 12:51:03 +0100

    installation working

--- diagnostics/ocean3d/ocean3d/__init__.py
+++ diagnostics/ocean3d/ocean3d/__init__.py
@@ -5,12 +5,16 @@ from .ocean_circulation import *
 from .ocean_drifts import *
 
 # Optional but recommended
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
-# This specifies which methods are exported publicly, used by "from dummy import *"
+# This specifies which methods are exported publicly"
 __all__ = ["hovmoller_lev_time_plot",
            "time_series_multilevs",
            "multilevel_t_s_trend_plot",
            "zonal_mean_trend_plot",
            "plot_spatial_mld_clim",
            "plot_stratification"]
+
+# CHANGELOG
+# 0.0.2: Added CLI and refinement
+# 0.0.1: Initial commit
