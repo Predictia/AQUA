24ab96a9e43813626468b8040605a3a36d33d0fa
Natalia Nazarova <natalia.nazarova@polito.it>
2023-11-17 14:20:43 +0100

    The logger is added to the script.

--- cli/weights/generate_weights_for_catalog.py
+++ cli/weights/generate_weights_for_catalog.py
@@ -4,29 +4,31 @@
 resos = [None] # or specify ['r025', 'r100']
 
 from aqua import Reader
-from aqua.slurm import slurm 
+from aqua.slurm import slurm, get_job_status, waiting_for_slurm_response
 from aqua.util import ConfigPath
 from aqua.logger import log_configure
 
 if __name__ == '__main__':
+    
     # Initializing the logger
+    loglevel = 'WARNING'
     logger = log_configure(log_level=loglevel, log_name='Weights Submitter')
-        
-    machine_name = ConfigPath().machine
+    
+    logger.info('Running weights weekly submitter...')
+
     # Job initialization 
-    if machine_name=='levante':        
-        slurm.job(cores=16, memory="200 GB", queue=None, walltime='08:00:00',
-            jobs=1, path_to_output='.', loglevel='WARNING')
-    elif machine_name=='lumi':
+    if ConfigPath().machine=='levante':        
+        slurm.job(cores=16, memory="200 GB", walltime='08:00:00', jobs=1, path_to_output='.', loglevel=loglevel)
+    elif ConfigPath().machine=='lumi':
         slurm.job()
-    #elif machine_name=='mafalda':
+    #elif ConfigPath().machine=='mafalda':
     #    slurm.job()
 
     waiting_for_slurm_response(10)
 
     model = inspect_catalogue()
 
-    for i in range(0, 60):
+    for i in range(0, 120):
         if get_job_status() == 'R':
             logger.info('The weight weekly submitter is started to run!')
             for reso in resos:
@@ -36,10 +38,14 @@ if __name__ == '__main__':
                         source = inspect_catalogue(model = m, exp = e)
                         for s in source:
                             for zoom in range(0, 9):
-                                reader = Reader(model=m, exp=e, source=s, regrid=reso, zoom=zoom)
+                                try:
+                                    reader = Reader(model=m, exp=e, source=s, regrid=reso, zoom=zoom)
+                                except Exception as e:
+                                    # Handle other exceptions
+                                    logger.error(f"For the source {m} {e} {s} {reso} {zoom} an unexpected error occurred: {e}")
             break
         else:
             logger.info('The job is waiting in the queue')
             waiting_for_slurm_response(60)
-            
 
+    logger.info('The weights weekly submitter is terminated.')
