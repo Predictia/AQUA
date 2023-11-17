17240e4dc79db6f425bf38d413e3d2b4b73670cb
Natalia Nazarova <natalia.nazarova@polito.it>
2023-11-17 14:21:51 +0100

    The 'slurm_util' script (i.e., 'get_job_status' and 'waiting_for_slurm_response' functions) is added.

--- aqua/slurm/__init__.py
+++ aqua/slurm/__init__.py
@@ -1,3 +1,3 @@
-from .slurm import squeue, job, output_dir, scancel, max_resources_per_node
-
-__all__ = ['squeue', 'job', 'output_dir', 'scancel', 'max_resources_per_node']
+from .slurm import Slurm
+from .slurm_util import get_job_status, waiting_for_slurm_response
+__all__ = ['Slurm', 'get_job_status', 'waiting_for_slurm_response']
