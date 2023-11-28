2e055358455cde5829eee883db58c19c508a6bc6
Natalia Nazarova <natalia.nazarova@polito.it>
2023-11-28 18:55:42 +0100

    The clip is modified: slurm initialization is explicit in cli for Levante and Lumi.

--- cli/weights/run_weekly_submitter.sh
+++ cli/weights/run_weekly_submitter.sh
@@ -8,13 +8,39 @@ source $whereconda/etc/profile.d/conda.sh
 # activate conda environment
 conda activate aqua_common
 
-while true; do
-    # Your commands to run your job
-    /usr/bin/env python3 generate_weights_for_catalog.py
-    # Sleep for one week (adjust the time as needed)
-    sleep 604800  # 604800 seconds = 1 week
+machine=$(python get_machine.py)
+echo "Machine name: $machine"
 
+while true; do
+    if [ "$machine" == 'levante' ] || [ "$machine" == 'lumi' ]; then    
+        # Create a SLURM job script
+        job_script="tmp.sh"
+        echo "#!/bin/bash" > "$job_script"
+        if [ "$machine" == 'levante' ]; then
+            echo "#SBATCH --account=bb1153" >> "$job_script"
+            echo "#SBATCH --partition=shared" >> "$job_script"
+        else
+            echo "#SBATCH --partition=standard" >> "$job_script"
+            echo "#SBATCH --account=project_465000454" >> "$job_script"
+        fi
+        echo "#SBATCH --job-name=weights" >> "$job_script"
+        echo "#SBATCH --output=weights_%j.out" >> "$job_script"
+        echo "#SBATCH --error=weights_%j.log" >> "$job_script"
+        echo "#SBATCH --nodes=1" >> "$job_script"
+        echo "#SBATCH --ntasks-per-node=1" >> "$job_script" #16
+        echo "#SBATCH --time=0:10:00" >> "$job_script"
+        echo "#SBATCH --mem=10G" >> "$job_script"
+        # Add your commands
+        echo "echo 'Hello from SLURM job!'" >> "$job_script"
+        echo "/usr/bin/env python3 generate_weights_for_catalog.py" >> "$job_script"
+        # Submit the SLURM job
+        sbatch "$job_script"
+    else
+        /usr/bin/env python3 generate_weights_for_catalog.py
+    fi
+    # Sleep for one week (adjust the time as needed)
+    ##sleep 604800  # 604800 seconds = 1 week
     # Resubmit the job to Slurm
-    sbatch $0
+    #sbatch $0
     exit  # Exit the current instance of the script after submitting
 done
