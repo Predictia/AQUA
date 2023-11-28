5dc1fb2ee34b62f293dde2e3abb28a8d27960903
Natalia Nazarova <natalia.nazarova@polito.it>
2023-11-28 20:31:33 +0100

    The script is getting submitted to the slurm queue directly without creating a temporary file.

--- cli/weights/run_weekly_submitter.sh
+++ cli/weights/run_weekly_submitter.sh
@@ -16,31 +16,35 @@ echo "Machine name: $machine"
 
 while true; do
     if [ "$machine" == 'levante' ] || [ "$machine" == 'lumi' ]; then    
-        # Create a SLURM job script
-        job_script="tmp.sh"
-        echo "#!/bin/bash" > "$job_script"
         if [ "$machine" == 'levante' ]; then
-            echo "#SBATCH --account=bb1153" >> "$job_script"
-            echo "#SBATCH --partition=shared" >> "$job_script"
+            account='bb1153'
+            partition='shared'
         else
-            echo "#SBATCH --partition=standard" >> "$job_script"
-            echo "#SBATCH --account=project_465000454" >> "$job_script"
+            account='project_465000454'
+            partition='standard'
         fi
-        echo "#SBATCH --job-name=weights" >> "$job_script"
-        echo "#SBATCH --output=weights_%j.out" >> "$job_script"
-        echo "#SBATCH --error=weights_%j.log" >> "$job_script"
-        echo "#SBATCH --nodes=1" >> "$job_script"
-        echo "#SBATCH --ntasks-per-node=1" >> "$job_script" #16
-        echo "#SBATCH --time=0:10:00" >> "$job_script"
-        echo "#SBATCH --mem=10G" >> "$job_script"
-        # Add your commands
-        echo "echo 'Hello from SLURM job!'" >> "$job_script"
-        echo "/usr/bin/env python3 generate_weights_for_catalog.py" >> "$job_script"
-        # Submit the SLURM job
-        sbatch "$job_script"
+
+        # Submit the SLURM job directly
+        sbatch <<EOL
+#!/bin/bash
+#SBATCH --account=$account
+#SBATCH --partition=$partition
+#SBATCH --job-name=we
+#SBATCH --output=weights_%j.out
+#SBATCH --error=weights_%j.log
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=0:10:00
+#SBATCH --mem=10G
+
+echo 'Hello from SLURM job!'
+/usr/bin/env python3 generate_weights_for_catalog.py
+EOL
+
     else
         /usr/bin/env python3 generate_weights_for_catalog.py
     fi
+
     
     # Use an if statement to decide whether to resubmit
     if $should_resubmit; then
