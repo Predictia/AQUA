9329643cbbc4ffebfbb39a0982b4a6f5f7a396a8
Paolo Davini <p.davini@isac.cnr.it>
2023-12-06 11:52:42 +0200

    small change

--- aqua/reader/reader.py
+++ aqua/reader/reader.py
@@ -401,9 +401,9 @@ class Reader(FixerMixin, RegridMixin):
                         raise KeyError("You are asking for variables which we cannot find in the catalog!") from e
 
         if ffdb:
-            data = log_history(data, f"Retrieved from {self.model}-{self.exp}-{self.source} using FDB.")
+            data = log_history(data, f"Retrieved from {self.model}_{self.exp}_{self.source} using FDB.")
         else:
-            data = log_history(data, f"Retrieved from {self.model}-{self.exp}-{self.source} using file from disk.")
+            data = log_history(data, f"Retrieved from {self.model}_{self.exp}_{self.source} using file from disk.")
         
         # sequence which should be more efficient: decumulate - averaging - regridding - fixing
 
