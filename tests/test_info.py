76481870dfe258f0b0a06a62b3df46ff3f7fb6cb
mnurisso <m.nurisso@isac.cnr.it>
2023-12-07 21:10:24 +0200

    removing gsv that is now stucking the tests

--- tests/test_info.py
+++ tests/test_info.py
@@ -24,18 +24,3 @@ def test_info_intake(capsys):
     assert "dims: {'ncells': 120184, 'time': 6}" in captured.out
     assert "data_vars: {'sst': ['lon', 'lat', 'level', 'time']}" in captured.out
     assert "coords: ('lon', 'lat', 'level', 'time')" in captured.out
-
-
-@pytest.mark.gsv
-def test_info_gsv(capsys):
-    """Test the info method of the Reader class for fdb sources"""
-    reader = Reader(model='IFS', exp='test-fdb', source="fdb", regrid='r100')
-    reader.info()
-    captured = capsys.readouterr()
-
-    assert "Reader for model IFS, experiment test-fdb, source fdb" in captured.out
-    assert "This experiment has expID" in captured.out
-    assert "fdb_path: /app/config.yaml" in captured.out
-    assert "Metadata:" in captured.out
-    assert "source_grid_name: lon-lat" in captured.out
-    assert "GSV request for this source:" in captured.out
