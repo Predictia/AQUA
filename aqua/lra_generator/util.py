21e9e8b801dec0594a1b9da8bfde11cbc903a4db
mnurisso <m.nurisso@isac.cnr.it>
2023-12-13 12:54:47 +0200

    regrid.yaml removed from opa lra tools

--- aqua/lra_generator/util.py
+++ aqua/lra_generator/util.py
@@ -1,22 +1,55 @@
 """Utility module for LRA/OPA"""
 
 import os
-import copy
-import logging
 from aqua.util import dump_yaml, load_yaml
 from aqua.util import ConfigPath
+from aqua.logger import log_configure
 
 
-def opa_catalog_entry(datadir, model, exp, source, frequency='monthly'):
+def opa_catalog_entry(datadir, model, exp, source, frequency='monthly',
+                      loglevel='WARNING'):
     """
     Create an entry in the AQUA catalog based on the presence of output from OPA in datadir
     to be used by the LRA generator in both source and regrid yaml
+
+    Args:
+        datadir (str): path to the directory containing the data
+        model (str): name of the model
+        exp (str): name of the experiment
+        source (str): name of the origin source
+        frequency (str, opt): frequency of the data, default is 'monthly'
+        loglevel (str, opt): logging level, default is 'WARNING'
     """
+    logger = log_configure(log_level=loglevel, log_name='opa_catalog_entry')
 
     entry_name = 'opa'
-    logging.warning('Creating catalog entry %s %s %s', model, exp, entry_name)
+    logger.warning('Creating catalog entry %s %s %s', model, exp, entry_name)
+
+    # load the catalog experiment file
+    Configurer = ConfigPath()
+    configdir = Configurer.configdir
+    machine = Configurer.machine
+
+    # find the catalog of my experiment
+    catalogfile = os.path.join(configdir, 'machines', machine,
+                               'catalog', model, exp + '.yaml')
+
+    # load, add the block and close
+    cat_file = load_yaml(catalogfile)
+
+    # read the grid info from the origin source
+    if source in cat_file['sources']:
+        try:
+            grid = cat_file['sources'][source]['metadata']['source_grid_name']
+        except KeyError:
+            logger.error('Cannot find source grid name in catalog, assuming lon-lat')
+            grid = 'lon-lat'
+    else:
+        raise KeyError('Cannot find source information in catalog')
 
     # define the block to be uploaded into the catalog
+    description = 'OPA output from %s' % source
+
     block_cat = {
         'driver': 'netcdf',
         'args': {
@@ -25,6 +58,10 @@ def opa_catalog_entry(datadir, model, exp, source, frequency='monthly'):
             'xarray_kwargs': {
                 'decode_times': True
             }
+        },
+        'description': description,
+        'metadata': {
+            'source_grid_name': grid,
         }
     }
 
@@ -41,34 +78,8 @@ def opa_catalog_entry(datadir, model, exp, source, frequency='monthly'):
     #     }
     #     block_cat.update(block_zoom)
 
-    Configurer = ConfigPath()
-    configdir = Configurer.configdir
-    machine = Configurer.machine
-
-    # find the catalog of my experiment
-    catalogfile = os.path.join(configdir, 'machines', machine,
-                               'catalog', model, exp + '.yaml')
-
-    # load, add the block and close
-    cat_file = load_yaml(catalogfile)
+    # add the block to the catalog
     cat_file['sources'][entry_name] = block_cat
     dump_yaml(outfile=catalogfile, cfg=cat_file)
-
-    # find the regrid of my experiment
-    regridfile = os.path.join(configdir, 'machines', machine,
-                              'regrid.yaml')
-    cat_file = load_yaml(regridfile)
-    dictexp = cat_file['sources'][model][exp]
-    if source in dictexp:
-        regrid_entry = dictexp[entry_name]
-    elif 'default' in dictexp:
-        logging.warning('No entry found for source %s, assuming the default', source)
-        regrid_entry = dictexp['default']
-    else:
-        raise KeyError('Cannot find experiment information regrid file')
-
-    cat_file['sources'][model][exp][entry_name] = copy.deepcopy(regrid_entry)
-
-    dump_yaml(outfile=regridfile, cfg=cat_file)
 
     return entry_name
