93a38c0e55e59d895fb2aa446dc931807e7d619a
Natalia Nazarova <natalia.nazarova@polito.it>
2023-12-14 19:45:28 +0200

    Revert changes in slurm files to main branch (i.e., 'aqua/slurm/__init__.py' and 'aqua/slurm/slurm.py').

--- aqua/slurm/__init__.py
+++ aqua/slurm/__init__.py
@@ -1,3 +1,3 @@
 from .slurm import squeue, job, output_dir, scancel, max_resources_per_node
-from .slurm_util import get_job_status, waiting_for_slurm_response
-__all__ = ['squeue', 'job', 'output_dir', 'scancel', 'max_resources_per_node', 'get_job_status', 'waiting_for_slurm_response']
+
+__all__ = ['squeue', 'job', 'output_dir', 'scancel', 'max_resources_per_node']
