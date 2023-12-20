abad1eb241f9eb841c2f0b57016531bc47c243a5
Natalia Nazarova <natalia.nazarova@polito.it>
2023-12-21 00:10:01 +0100

    The 'run_weights_submitter.sh' file is resctructured, all important variables are moved to config file.

--- cli/weights/run_weights_submitter.sh
+++ cli/weights/run_weights_submitter.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 set -e
 
-# Check a condition (replace this with your actual condition)
-should_resubmit=false
-nproc='16'
-walltime='08:00:00'
-memory='200G'
-
 script_dir=$(dirname "${BASH_SOURCE[0]}")
 
 # Read the machine name from the YAML file
-machine=$(grep -E "^machine:" "$script_dir/../../config/config-aqua.yaml" | awk '{print $2}')
-# Now $machine contains the name of the machine
-echo "Machine name: $machine"
+machine=$(python -c "
+import yaml
+with open('$script_dir/../../config/config-aqua.yaml') as f:
+    config = yaml.safe_load(f)
+    print(config['machine'])
+")
+echo "Machine Name: $machine"
+
+read -r nproc nodes walltime memory lumi_version account partition run_on_sunday < <(python -c "
+import yaml
+with open('$script_dir/config/weights_config.yml') as f:
+    config = yaml.safe_load(f)['compute_resources']
+    print(config['nproc'], config['nodes'], config['walltime'], config['memory'], config['lumi_version'], \
+    config['account']['$machine'], config['partition']['$machine'], config['run_on_sunday'])
+")
+
+if [ "$run_on_sunday" == "True" ]; then
+    begin_time=$(date -d "next Sunday 21:00" +"%Y-%m-%dT%H:%M:%S")
+else
+    begin_time=$(date -d "now + 1 minute" +"%Y-%m-%dT%H:%M:%S")
+fi
+echo "Begin run time: $begin_time"
+echo "Number of processes (nproc): $nproc"
+echo "Number of nodes: $nodes"
+echo "Walltime: $walltime"
+echo "Memory: $memory"
+echo "Account: $account"
+echo "Partition: $partition"
 
 # if machine is levante use mamba/conda
 if [ $machine == "levante" ]; then
@@ -27,31 +46,24 @@ fi
 function load_environment_AQUA() {
         # Load env modules on LUMI
     module purge
-    module use LUMI/23.03
+    module use LUMI/$lumi_version
 }
 
-while true; do
-    if [ "$machine" == 'levante' ] || [ "$machine" == 'lumi' ]; then    
-        if [ "$machine" == 'levante' ]; then
-            account='bb1153'
-            partition='shared'
-        else
-            account='project_465000454'
-            partition='small'
-        fi
-        # Submit the SLURM job directly
-        sbatch <<EOL
+if [ "$machine" == 'levante' ] || [ "$machine" == 'lumi' ]; then
+    # Submit the SLURM job with submission_time variable
+    sbatch --begin="$begin_time" <<EOL
 #!/bin/bash
 #SBATCH --account=$account
 #SBATCH --partition=$partition
 #SBATCH --job-name=weights
 #SBATCH --output=weights_%j.out
 #SBATCH --error=weights_%j.log
-#SBATCH --nodes=1
+#SBATCH --nodes=$nodes
 #SBATCH --ntasks-per-node=$nproc
 #SBATCH --time=$walltime
 #SBATCH --mem=$memory
 
+echo "Submission time: $submission_time"
 echo 'Hello from SLURM job!'
 
 # if machine is lumi use modules
@@ -64,14 +76,6 @@ fi
 /usr/bin/env python3 "$script_dir/generate_weights.py" --nproc=$nproc
 EOL
 
-    else
-        /usr/bin/env python3 "$script_dir/generate_weights.py" --nproc=$nproc
-    fi
-    # Use an if statement to decide whether to resubmit
-    if $should_resubmit; then
-        # Sleep for one week (adjust the time as needed)
-        sleep 604800  # 604800 seconds = 1 week
-        sbatch "$0"  # Resubmit the job to Slurm
-    fi
-    exit  # Exit the current instance of the script after submitting
-done
+else
+    /usr/bin/env python3 "$script_dir/generate_weights.py" --nproc=$nproc
+fi
