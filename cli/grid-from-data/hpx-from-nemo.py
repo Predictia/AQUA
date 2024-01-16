76b74a574ef13ddcb9a6d9ba4b4ee47c029bd9e7
mnurisso <m.nurisso@isac.cnr.it>
2024-01-16 18:07:48 +0200

    typo

--- cli/grid-from-data/hpx-from-nemo.py
+++ cli/grid-from-data/hpx-from-nemo.py
@@ -71,7 +71,7 @@ if __name__ == '__main__':
 
     # setting output filename
     tgt = config['tgt']
-    filename_tgt = tgt + '/' + model_name + '_hpz' + str(zoom) + 
+    filename_tgt = tgt + '/' + model_name + '_hpz' + str(zoom)
     if nested:
         filename_tgt = filename_tgt + '_nested_oce'
     else:
