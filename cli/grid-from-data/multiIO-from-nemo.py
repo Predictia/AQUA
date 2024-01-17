ab96ba251609718d2251c48c81653a26966be4e2
mnurisso <m.nurisso@isac.cnr.it>
2024-01-17 14:01:00 +0200

    multiIO nemo grid generation tool

--- cli/grid-from-data/multiIO-from-nemo.py
+++ cli/grid-from-data/multiIO-from-nemo.py
@@ -5,7 +5,6 @@ AQUA command line tool to create an healpix grid from an oceanic file
 """
 import argparse
 import sys
-from cdo import Cdo
 from aqua import Reader
 from aqua.util import load_yaml, get_arg, create_folder
 from aqua.logger import log_configure
@@ -28,9 +27,9 @@ def parse_arguments(args):
 if __name__ == '__main__':
     args = parse_arguments(sys.argv[1:])
     loglevel = get_arg(args, 'loglevel', 'WARNING')
-    logger = log_configure(log_level=loglevel, log_name='hpx-from-nemo')
+    logger = log_configure(log_level=loglevel, log_name='multiIO-from-nemo')
 
-    file = get_arg(args, 'config', 'config-hpx-nemo.yaml')
+    file = get_arg(args, 'config', 'config-multiIO-nemo.yaml')
     logger.info('Reading configuration from %s', file)
     config = load_yaml(file)
 
@@ -42,54 +41,25 @@ if __name__ == '__main__':
     logger.info('Retrieving %s from %s %s %s', var, model, exp, source)
 
     # Configuration needed to save the file
-    tmp = config['tmp']
+    tgt = config['tgt']
     model_name = config['model_name']
-    zoom = config['zoom']
     model3d = config['3d']
-    nested = config['nested']
 
     # Create Reader object
     reader = Reader(model=model, exp=exp, source=source,
                     areas=False, fix=False, loglevel=loglevel)
     data = reader.retrieve(var=var)
 
+    # Save data in a netcdf file
+    create_folder(tgt)
     if model3d:
-        logger.debug("Modifying level axis attributes as Z")
-        data['level'].attrs['axis'] = 'Z'
-
-    # Save data in a netcdf file on the temporary directory
-    create_folder(tmp)
-    filename = tmp + '/' + model + '_' + exp + '_' + source + '_' + var + '.nc'
+        grid_name = 'nemo-multiIO-r025-3d'
+    else:
+        grid_name = 'nemo-multiIO-r025'
+    filename = tgt + '/' + grid_name + '.nc'
 
     logger.info('Saving data in %s', filename)
-    data[var].isel(time=0).to_netcdf(filename)
-
-    # CDO command to set the grid
-    cdo = Cdo()
-
-    # cdo command setup:
-    nside = 2**zoom
-    if nested:
-        grid_name = 'hp' + str(nside) + '_nested'
-    else:  # ring
-        grid_name = 'hp' + str(nside) + '_ring'
-
-    # setting output filename
-    tgt = config['tgt']
-    create_folder(tgt)
-    filename_tgt = tgt + '/' + model_name + '_hpz' + str(zoom)
-    if nested:
-        filename_tgt = filename_tgt + '_nested_oce'
-    else:
-        filename_tgt = filename_tgt + '_ring_oce'
-
-    if model3d:
-        filename_tgt = filename_tgt + '_level.nc'
-    else:
-        filename_tgt = filename_tgt + '.nc'
-
-    logger.info('Setting grid %s', grid_name)
-    logger.info('Saving data in %s', filename_tgt)
-    cdo.setgrid(grid_name, input=filename, output=filename_tgt, options="-f nc4 -z zip")
+    data[var].isel(time=0).to_netcdf(filename, format='NETCDF4', engine='netcdf4',
+                                     encoding={var: {'zlib': True, 'complevel': 9}})
 
     logger.info('Done')
