2e371cd7345a55a155089ca4b09785f273ebf437
Natalia Nazarova <natalia.nazarova@polito.it>
2024-01-25 16:12:09 +0100

    The `lumi_version` is added to the `activate_aqua` function.

--- cli/util/activate_aqua.sh
+++ cli/util/activate_aqua.sh
@@ -1,24 +1,7 @@
 source logger.sh
+source get_machine.sh
 
-function get_machine() {
-    # Determine the directory of the current script
-    script_dir=$(dirname "${BASH_SOURCE[0]}")
-    machine=$(python -c "
-try:
-    # Read and log the machine name from the YAML configuration file
-    import yaml
-    with open('$script_dir/../../config/config-aqua.yaml') as f:
-        config = yaml.safe_load(f)
-        print(config['machine'])
-except Exception as e:
-    print('Error:', e)
-")
-    if [[ $machine == Error:* ]]; then
-        log_message ERROR "Error reading machine name: ${machine#Error: }"
-    fi
-    echo $machine
-}
-
+lumi_version=23.03
 # Function to load environment on LUMI
 function load_environment_AQUA() {
     # Load env modules on LUMI
