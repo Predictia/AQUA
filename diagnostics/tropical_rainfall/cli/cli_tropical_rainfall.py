29755129c459dd14d2b219870f093e42bf5d20dd
Natalia Nazarova <natalia.nazarova@polito.it>
2024-01-28 11:28:21 +0100

    The client file is updated: the time averaging and regrinding are added if necessary.

--- diagnostics/tropical_rainfall/cli/cli_tropical_rainfall.py
+++ diagnostics/tropical_rainfall/cli/cli_tropical_rainfall.py
@@ -1,7 +1,8 @@
 import sys
+sys.path.append('../')
+from src.tropical_rainfall_tools import ToolsClass
 import os
 import argparse
-
 from aqua.util import load_yaml, get_arg
 from aqua import Reader
 from aqua.logger import log_configure
@@ -30,8 +31,6 @@ def parse_arguments(args):
 
 
 if __name__ == '__main__':
-
-    print('Running tropical rainfall diagnostic...')
     args = parse_arguments(sys.argv[1:])
 
     # change the current directory to the one of the CLI so that relative path works
@@ -65,11 +64,19 @@ if __name__ == '__main__':
     model = get_arg(args, 'model', config['data']['model'])
     exp = get_arg(args, 'exp', config['data']['exp'])
     source = get_arg(args, 'source', config['data']['source'])
-
     logger.debug(f"Accessing {model} {exp} {source} data")
 
+    freq = config['data']['freq']
+    regrid = config['data']['regrid']
+    s_year = config['data']['s_year']
+    f_year = config['data']['f_year']
+
+    machine = config['machine']
+    logger.info(f"The machine is {machine}")
+
     path_to_output = get_arg(
-        args, 'outputdir', config['path']['path_to_output'])
+        args, 'outputdir', config['path'][machine])
+
     if path_to_output is not None:
         path_to_netcdf = os.path.join(
             path_to_output, 'netcdf/'+model+'_'+exp+'_'+source+'/')
@@ -97,30 +104,52 @@ if __name__ == '__main__':
     loc = config['plot']['loc']
     pdf_format = config['plot']['pdf_format']
 
-    reader = Reader(model=model, exp=exp, source=source, loglevel=loglevel)
-    data = reader.retrieve(var=model_variable)
+    diag = Tropical_Rainfall(trop_lat=trop_lat, num_of_bins=num_of_bins,
+                             first_edge=first_edge, width_of_bin=width_of_bin, loglevel=loglevel)
+    reader = Reader(model=model, exp=exp, source=source, loglevel=loglevel, regrid=regrid)
+    full_dataset = reader.retrieve(var=model_variable)
 
+    test_sample = full_dataset.isel(time=slice(1, 11))
+    regrid_bool = ToolsClass().check_need_for_regridding(test_sample, regrid)
+    freq_bool = ToolsClass().check_need_for_time_averaging(test_sample, freq)
+
+    first_year_in_dataset = full_dataset['time.year'][0].values
+    last_year_in_dataset = full_dataset['time.year'][-1].values
+
+    s_year = first_year_in_dataset if s_year is None else max(s_year, first_year_in_dataset)
+    f_year = last_year_in_dataset if f_year is None else min(f_year, last_year_in_dataset)
+
+    for year in range(s_year, f_year):
+        data_per_year = full_dataset.sel(time=slice(str(year), str(year)))
+        if data_per_year.time.size != 0:
+            for x in range(1, 12):
+                data = data_per_year.sel(time=slice(str(year)+'-'+str(x), str(year)+'-'+str(x+1)))
+                if freq_bool:
+                    data = reader.timmean(data, freq=freq)
+                if regrid_bool:
+                    data = reader.regrid(data)
+                try:
+                    diag.histogram(data, model_variable=model_variable,
+                                   path_to_histogram=path_to_netcdf+"/"+regrid+'_'+freq+'/histograms/',
+                                   threshold = 30, name_of_file=regrid+'_'+freq)
+                except Exception as e:
+                    # Handle other exceptions
+                    logger.error(f"An unexpected error occurred: {e}")
+                logger.debug(f"Current Status: {x}/12 months processed in year {year}.")
+        else:
+            logger.warning("The specified year is not present in the dataset. " +
+                           "Ensure the time range in your selection accurately reflects the available " +
+                           "data. Check dataset time bounds and adjust your time selection parameters accordingly.")
+
+    logger.info("The histograms are calculated and saved in storage.")
     try:
-        diag = Tropical_Rainfall(trop_lat=trop_lat,       num_of_bins=num_of_bins,
-                                 first_edge=first_edge,   width_of_bin=width_of_bin, loglevel=loglevel)
-        hist = diag.histogram(data, model_variable=model_variable,  new_unit=new_unit,
-                              path_to_histogram=path_to_netcdf+'histograms/', name_of_file=name_of_netcdf)
-
         hist_merged = diag.merge_list_of_histograms(
-            path_to_histograms=path_to_netcdf+'histograms/',  all=True)
+            path_to_histograms=path_to_netcdf+"/"+regrid+'_'+freq+'/histograms/', all=True)
+
         diag.histogram_plot(hist_merged, figsize=figsize, new_unit=new_unit,
                             legend=legend, color=color, xmax=xmax, plot_title=plot_title, loc=loc,
                             path_to_pdf=path_to_pdf, pdf_format=pdf_format, name_of_file=name_of_pdf)
-        logger.warning("The histogram is calculated, plotted, and saved in storage.")
-    except ZeroDivisionError as zd_error:
-        # Handle ZeroDivisionError
-        logger.error(f"ZeroDivisionError occurred: {zd_error}")
-    except ValueError as value_error:
-        # Handle ValueError
-        logger.error(f"ValueError occurred: {value_error}")
-    except KeyError as key_error:
-        # Handle KeyError
-        logger.error(f"KeyError occurred: {key_error}")
+        logger.info("The histograms are plotted and saved in storage.")
     except FileNotFoundError as file_error:
         # Handle FileNotFoundError
         logger.error(f"FileNotFoundError occurred: {file_error}")
