2b29b32c4e5c5dbd0c893a8ea0a1386da821af07
Natalia Nazarova <natalia.nazarova@polito.it>
2024-02-15 01:21:39 +0100

    The cli script is debugged.

--- diagnostics/tropical_rainfall/cli/cli_tropical_rainfall.py
+++ diagnostics/tropical_rainfall/cli/cli_tropical_rainfall.py
@@ -11,7 +11,7 @@ from aqua.logger import log_configure
 def parse_arguments(args):
     """Parse command line arguments"""
 
-    parser = argparse.ArgumentParser(description='Tropical Rainfall CLI')
+    parser = argparse.ArgumentParser(description='Trop. Rainfall CLI')
     parser.add_argument('-c', '--config', type=str,
                         help='yaml configuration file')
     parser.add_argument('-l', '--loglevel', type=str,
@@ -87,7 +87,7 @@ if __name__ == '__main__':
 
     loglevel = get_arg(
         args, 'loglevel', config['class_attributes']['loglevel'])
-    logger = log_configure(log_name="Tropical Rainfall CLI",
+    logger = log_configure(log_name="Trop. Rainfall CLI",
                            log_level=loglevel)
 
     model = get_arg(args, 'model', config['data']['model'])
@@ -101,6 +101,8 @@ if __name__ == '__main__':
     regrid = config['data']['regrid']
     s_year = config['data']['s_year']
     f_year = config['data']['f_year']
+    s_month = config['data']['s_month']
+    f_month = config['data']['f_month']
 
     machine = config['machine']
     logger.info(f"The machine is {machine}")
@@ -131,7 +133,7 @@ if __name__ == '__main__':
     figsize = config['plot']['figsize']
     legend = config['plot']['legend']
     xmax = config['plot']['xmax']
-    plot_title = model+'_'+exp+'_'+source
+    plot_title = f"{model} {exp} {source} {regrid} {freq}"
     loc = config['plot']['loc']
     pdf_format = config['plot']['pdf_format']
 
@@ -158,23 +160,33 @@ if __name__ == '__main__':
 
     s_year = first_year_in_dataset if s_year is None else max(s_year, first_year_in_dataset)
     f_year = last_year_in_dataset if f_year is None else min(f_year, last_year_in_dataset)
+    logger.debug(f"s_year/f_year are: {s_year}/{f_year}.")
 
-    for year in range(s_year, f_year):
-        data_per_year = full_dataset.sel(time=slice(str(year), str(year)))
+    s_month = 1 if s_month is None else s_month
+    f_month = 12 if f_month is None else f_month
+
+    legend = f"{model} {exp} {source}" if legend is None else legend
+    xmax = diag.num_of_bins*diag.width_of_bin if xmax is None else xmax
+    logger.debug(f"xmax is : {xmax} {new_unit}.")
+
+    for year in range(s_year, f_year+1):
+        data_per_year = full_dataset.sel(time=str(year))
         if data_per_year.time.size != 0:
-            for x in range(1, 12):
-                data = data_per_year.sel(time=slice(str(year)+'-'+str(x), str(year)+'-'+str(x+1)))
-                if freq_bool:
-                    data = reader.timmean(data, freq=freq)
-                if regrid_bool:
-                    data = reader.regrid(data)
+            for x in range(s_month, f_month+1):
                 try:
+                    data = data_per_year.sel(time=str(year)+'-'+str(x))
+                    if freq_bool:
+                        data = reader.timmean(data, freq=freq)
+                    if regrid_bool:
+                        data = reader.regrid(data)
                     diag.histogram(data, model_variable=model_variable,
-                                   path_to_histogram=path_to_netcdf+"/"+regrid+'_'+freq+'/histograms/',
+                                   path_to_histogram=path_to_netcdf+f"{regrid}/{freq}/histograms/",
                                    threshold = 30, name_of_file=regrid+'_'+freq)
+                except KeyError:
+                    pass
                 except Exception as e:
-                    # Handle other exceptions
-                    logger.error(f"An unexpected error occurred: {e}")
+                        # Handle other exceptions
+                        logger.error(f"An unexpected error occurred: {e}")
                 logger.debug(f"Current Status: {x}/12 months processed in year {year}.")
         else:
             logger.warning("The specified year is not present in the dataset. " +
@@ -183,12 +195,31 @@ if __name__ == '__main__':
 
     logger.info("The histograms are calculated and saved in storage.")
     try:
-        hist_merged = diag.merge_list_of_histograms(
-            path_to_histograms=path_to_netcdf+"/"+regrid+'_'+freq+'/histograms/', all=True)
+        hist_merged = diag.merge_list_of_histograms(path_to_histograms=path_to_netcdf+f"{regrid}/{freq}/histograms/", all=True,
+                                                    start_year=s_year, end_year=f_year, start_month=s_month, end_month=f_month)
 
-        diag.histogram_plot(hist_merged, figsize=figsize, new_unit=new_unit,
+        add = diag.histogram_plot(hist_merged, figsize=figsize, new_unit=new_unit,
                             legend=legend, color=color, xmax=xmax, plot_title=plot_title, loc=loc,
                             path_to_pdf=path_to_pdf, pdf_format=pdf_format, name_of_file=name_of_pdf)
+
+        mswep_folder_path = f'/work/bb1153/b382267/observations/MSWEP/{regrid}/{freq}'
+
+        # Check if the folder exists
+        if not os.path.exists(mswep_folder_path):
+            logger.error(f"Error: The folder for MSWEP data with resolution '{regrid}' "
+                         f"and frequency '{freq}' does not exist. Histograms for the "
+                         "desired resolution and frequency have not been computed yet.")
+        else:
+            obs_merged = diag.merge_list_of_histograms(path_to_histograms=mswep_folder_path, all=True, start_year=s_year, end_year=f_year,
+                                                       start_month=s_month, end_month=f_month)
+            logger.info(f"The MSWEP data with resolution '{regrid}' and frequency '{freq}' are prepared for comparison.")
+
+            diag.histogram_plot(hist_merged, figsize=figsize, new_unit=new_unit, add=add,
+                                linewidth=2*diag.plots.linewidth, linestyle='--', color='tab:red',
+                                legend=f"MSWEP", xmax=xmax, plot_title=plot_title, loc=loc,
+                                path_to_pdf=path_to_pdf, pdf_format=pdf_format, name_of_file=name_of_pdf)
+
+
         logger.info("The histograms are plotted and saved in storage.")
     except FileNotFoundError as file_error:
         # Handle FileNotFoundError
