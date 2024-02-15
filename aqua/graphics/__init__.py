bac6d7d4196cf787175c864792608009933b02dd
mnurisso <m.nurisso@isac.cnr.it>
2024-02-15 13:25:05 +0200

    function exposed in the init

--- aqua/graphics/__init__.py
+++ aqua/graphics/__init__.py
@@ -1,4 +1,4 @@
 """Graphics package for Aqua."""
-from .single_map import plot_single_map
+from .single_map import plot_single_map, plot_single_map_diff
 
-__all__ = ["plot_single_map"]
+__all__ = ["plot_single_map", "plot_single_map_diff"]
