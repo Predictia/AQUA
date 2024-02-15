2ef75961847f8ac4bdb0e717ccf8966fb2206317
Natalia Nazarova <natalia.nazarova@polito.it>
2024-02-15 20:49:29 +0100

    The mtpr_cli class is implemented since we must share multiple file configuration values.

--- diagnostics/tropical_rainfall/cli/cli_tropical_rainfall.py
+++ diagnostics/tropical_rainfall/cli/cli_tropical_rainfall.py
@@ -6,7 +6,8 @@ import argparse
 from aqua.util import load_yaml, get_arg
 from aqua import Reader
 from aqua.logger import log_configure
-
+sys.path.insert(0, '../../')
+from tropical_rainfall import Tropical_Rainfall
 
 def parse_arguments(args):
     """Parse command line arguments"""
@@ -64,190 +65,159 @@ def load_configuration(file_path):
     config = load_yaml(file_path)
     return config
 
-def get_config_values(config: dict, section: str, *keys: str) -> tuple:
+
+def adjust_year_range_based_on_dataset(full_dataset, start_year=None, final_year=None):
     """
-    Extract specified keys from a config section.
+    Adjusts the start and end years for processing based on the dataset's time range and optional user inputs.
     """
-    return tuple(config[section].get(key) for key in keys)
+    # Extract the first and last year from the dataset's time dimension
+    try:
+        first_year_in_dataset = full_dataset['time'].dt.year.values[0]
+        last_year_in_dataset = full_dataset['time'].dt.year.values[-1]
+    except AttributeError:
+        raise ValueError("The dataset must have a 'time' dimension with datetime64 data.")
 
-def get_config_and_log_values(config: dict, args, logger):
-    """
-    Extracts configuration and logging values from arguments and config dictionary.
-
-    Args:
-        config (dict): Configuration dictionary.
-        args (argparse.Namespace): Parsed command line arguments.
-        logger (Logger): Logger object for logging.
-
-    Returns:
-        tuple: Extracted values necessary for further processing.
-    """
-    model = get_arg(args, 'model', config['data']['model'])
-    exp = get_arg(args, 'exp', config['data']['exp'])
-    source = get_arg(args, 'source', config['data']['source'])
-    loglevel = get_arg(args, 'loglevel', config['logger']['loglevel'])
-    nproc = get_arg(args, 'nproc', config['compute_resources']['nproc'])
-    machine = config['machine']
-    path_to_output = get_arg(args, 'outputdir', config['path'][machine])
-
-    if path_to_output is not None:
-        path_to_netcdf = os.path.join(path_to_output, f'netcdf/{model}_{exp}_{source}/')
-        path_to_pdf = os.path.join(path_to_output, f'pdf/{model}_{exp}_{source}/')
-    else:
-        path_to_netcdf = path_to_pdf = None
-
-    logger.debug(f"The machine is {machine}")
-    logger.debug(f"Accessing {model} {exp} {source} data")
-    logger.debug(f"NetCDF folder: {path_to_netcdf}")
-    logger.debug(f"PDF folder: {path_to_pdf}")
-    return model, exp, source, loglevel, nproc, machine, path_to_netcdf, path_to_pdf
-
-def calculate_histogram_by_months(diag=None, config=None, logger=None, args=None):
-    model, exp, source, loglevel, nproc, machine, path_to_netcdf, path_to_pdf = get_config_and_log_values(config, args, logger)
-
-    freq, regrid, s_year, f_year, s_month, f_month = get_config_values(config, 'data', 'freq', 'regrid', 's_year', 'f_year',
-                                                                       's_month', 'f_month')
-    reader = Reader(model=model, exp=exp, source=source, loglevel=loglevel, regrid=regrid, nproc=nproc)
+    # Adjust start_year based on the dataset's range or user input
+    start_year = first_year_in_dataset if start_year is None else max(start_year, first_year_in_dataset)
     
-    trop_lat, num_of_bins, first_edge, width_of_bin, model_variable, new_unit = get_config_values(config, 'class_attributes',
-                                                                                                  'trop_lat', 'num_of_bins',
-                                                                                                  'first_edge', 'width_of_bin',
-                                                                                                  'model_variable', 'new_unit')
-    full_dataset = reader.retrieve(var=model_variable)
+    # Adjust final_year based on the dataset's range or user input
+    final_year = last_year_in_dataset if final_year is None else min(final_year, last_year_in_dataset)
 
-    test_sample = full_dataset.isel(time=slice(1, 11))
-    tools = ToolsClass()
-
-    if isinstance(regrid, str):
-        regrid_bool = tools.check_need_for_regridding(test_sample, regrid)
-    else:
-        regrid_bool = False
-
-    if isinstance(freq, str):
-        freq_bool = tools.check_need_for_time_averaging(test_sample, freq)
-    else:
-        freq_bool = False
-
-    first_year_in_dataset = full_dataset['time.year'][0].values
-    last_year_in_dataset = full_dataset['time.year'][-1].values
-
-    s_year = first_year_in_dataset if s_year is None else max(s_year, first_year_in_dataset)
-    f_year = last_year_in_dataset if f_year is None else min(f_year, last_year_in_dataset)
-    logger.debug(f"s_year/f_year are: {s_year}/{f_year}.")
-
-    s_month = 1 if s_month is None else s_month
-    f_month = 12 if f_month is None else f_month
-
-    for year in range(s_year, f_year+1):
-        data_per_year = full_dataset.sel(time=str(year))
-        if data_per_year.time.size != 0:
-            for x in range(s_month, f_month+1):
-                try:
-                    data = data_per_year.sel(time=str(year)+'-'+str(x))
-                    if freq_bool:
-                        data = reader.timmean(data, freq=freq)
-                    if regrid_bool:
-                        data = reader.regrid(data)
-                    diag.histogram(data, model_variable=model_variable,
-                                   path_to_histogram=path_to_netcdf+f"{regrid}/{freq}/histograms/",
-                                   threshold = 30, name_of_file=regrid+'_'+freq)
-                except KeyError:
-                    pass
-                except Exception as e:
-                        # Handle other exceptions
-                        logger.error(f"An unexpected error occurred: {e}")
-                logger.debug(f"Current Status: {x}/{f_month} months processed in year {year}.")
+    return start_year, final_year
+     
+class MTPR_CLI:
+    def __init__(self, config, args):
+        self.freq = config['data']['freq']
+        self.regrid = config['data']['regrid']
+        self.s_year = config['data']['s_year']
+        self.f_year = config['data']['f_year']
+        self.s_month = config['data']['s_month']
+        self.f_month = config['data']['f_month']
+        
+        self.trop_lat = config['class_attributes']['trop_lat']
+        self.num_of_bins = config['class_attributes']['num_of_bins']
+        self.first_edge = config['class_attributes']['first_edge']
+        self.width_of_bin = config['class_attributes']['width_of_bin']
+        self.model_variable = config['class_attributes']['model_variable']
+        self.new_unit = config['class_attributes']['new_unit']
+        
+        self.color  = config['plot']['color']
+        self.figsize = config['plot']['figsize']
+        self.xmax = config['plot']['xmax']
+        self.loc = config['plot']['loc'] 
+        self.pdf_format = config['plot']['pdf_format']
+        
+        self.model = get_arg(args, 'model', config['data']['model'])
+        self.exp = get_arg(args, 'exp', config['data']['exp'])
+        self.source = get_arg(args, 'source', config['data']['source'])
+        self.loglevel = get_arg(args, 'loglevel', config['logger']['loglevel'])
+        
+        nproc = get_arg(args, 'nproc', config['compute_resources']['nproc'])
+        machine = config['machine']
+        path_to_output = get_arg(args, 'outputdir', config['path'][machine])
+        self.logger = log_configure(log_name="Trop. Rainfall CLI", log_level=self.loglevel)
+        
+        if path_to_output is not None:
+            self.path_to_netcdf = os.path.join(path_to_output, f'netcdf/{self.model}_{self.exp}_{self.source}/')
+            self.path_to_pdf = os.path.join(path_to_output, f'pdf/{self.model}_{self.exp}_{self.source}/')
         else:
-            logger.warning("The specified year is not present in the dataset. " +
-                           "Ensure the time range in your selection accurately reflects the available " +
-                           "data. Check dataset time bounds and adjust your time selection parameters accordingly.")
+            self.path_to_netcdf = self.path_to_pdf = None
+        
+        self.reader = Reader(model=self.model, exp=self.exp, source=self.source, loglevel=self.loglevel, regrid=self.regrid, nproc=nproc)
+        self.diag = Tropical_Rainfall(trop_lat=self.trop_lat, num_of_bins=self.num_of_bins, first_edge=self.first_edge, 
+                                      width_of_bin=self.width_of_bin, loglevel=self.loglevel)
+        self.tools = ToolsClass(loglevel=self.loglevel)
+        
+    def need_regrid_timmean(self, full_dataset):
+        """Determines whether regridding or time averaging is needed for a dataset."""
+        test_sample = full_dataset.isel(time=slice(1, 5))
+        # Check for the need of regridding
+        regrid_bool = False
+        if isinstance(self.regrid, str):
+            regrid_bool = self.tools.check_need_for_regridding(test_sample, self.regrid)
+        # Check for the need of time averaging
+        freq_bool = False
+        if isinstance(self.freq, str):
+            freq_bool = self.tools.check_need_for_time_averaging(test_sample, self.freq)
+        return regrid_bool, freq_bool
+        
+    def calculate_histogram_by_months(self):
+        full_dataset = self.reader.retrieve(var=self.model_variable)
+        regrid_bool, freq_bool = self.need_regrid_timmean(full_dataset)
+        
+        s_year, f_year = adjust_year_range_based_on_dataset(full_dataset, start_year=self.s_year, final_year=self.f_year)
+        s_month = 1 if self.s_month is None else self.s_month
+        f_month = 12 if self.f_month is None else self.f_month
 
-    logger.info("The histograms are calculated and saved in storage.")
-    plot_title = f"{model} {exp} {source} {regrid} {freq}"
-    return path_to_netcdf+f"{regrid}/{freq}/histograms/",  plot_title
+        for year in range(s_year, f_year+1):
+            data_per_year = full_dataset.sel(time=str(year))
+            if data_per_year.time.size != 0:
+                for x in range(s_month, f_month+1):
+                    try:
+                        data = data_per_year.sel(time=str(year)+'-'+str(x))
+                        if freq_bool:
+                            data = self.reader.timmean(data, freq=self.freq)
+                        if regrid_bool:
+                            data = self.reader.regrid(data)
+                        self.diag.histogram(data, model_variable=self.model_variable,
+                                    path_to_histogram=self.path_to_netcdf+f"{self.regrid}/{self.freq}/histograms/",
+                                    threshold = 30, name_of_file=f"{self.regrid}_{self.freq}")
+                        self.logger.debug(f"The path to file is: {self.path_to_netcdf}{self.regrid}/{self.freq}/histograms/.")
+                    except KeyError:
+                        pass
+                    except Exception as e:
+                            # Handle other exceptions
+                            self.logger.error(f"An unexpected error occurred: {e}")
+                    self.logger.debug(f"Current Status: {x}/{f_month} months processed in year {year}.")
+            else:
+                self.logger.warning("The specified year is not present in the dataset. " +
+                            "Ensure the time range in your selection accurately reflects the available " +
+                            "data. Check dataset time bounds and adjust your time selection parameters accordingly.")
+        self.logger.info("The histograms are calculated and saved in storage.")
+        return None
 
-def plot_histograms(diag=None, config=None, logger=None, args=None, path_to_histograms=None, plot_title=None):
-    model, exp, source, loglevel, nproc, machine, path_to_netcdf, path_to_pdf = get_config_and_log_values(config, args, logger)
-    legend = f"{model} {exp} {source}"
-    
-    freq, regrid, s_year, f_year, s_month, f_month = get_config_values(config, 'data', 'freq', 'regrid', 's_year', 'f_year',
-                                                                       's_month', 'f_month')
-    trop_lat, num_of_bins, first_edge, width_of_bin, model_variable, new_unit = get_config_values(config, 'class_attributes',
-                                                                                                  'trop_lat', 'num_of_bins',
-                                                                                                  'first_edge', 'width_of_bin',
-                                                                                                  'model_variable', 'new_unit')
-    color, figsize, xmax, loc, pdf_format = get_config_values(config, 'plot', 'color', 'figsize', 'xmax',
-                                                                      'loc', 'pdf_format')
-    name_of_pdf = model+'_'+exp+'_'+source
-    
+    def plot_histograms(self):
+        plot_title = f"{self.model} {self.exp} {self.source} {self.regrid} {self.freq}"
+        legend = f"{self.model} {self.exp} {self.source}"
+        name_of_pdf =f"{self.model}_{self.exp}_{self.source}"
+        
+        self.logger.debug(f"The path to file is: {self.path_to_netcdf}{self.regrid}/{self.freq}/histograms/.")
+        hist_merged = self.diag.merge_list_of_histograms(path_to_histograms=self.path_to_netcdf+f"{self.regrid}/{self.freq}/histograms/",
+                                                    all=True, start_year=self.s_year, end_year=self.f_year,
+                                                    start_month=self.s_month, end_month=self.f_month)
+        
+        add = self.diag.histogram_plot(hist_merged, figsize=self.figsize, new_unit=self.new_unit,
+                            legend=legend, color=self.color, xmax=self.xmax, plot_title=plot_title, loc=self.loc,
+                            path_to_pdf=self.path_to_pdf, pdf_format=self.pdf_format, name_of_file=name_of_pdf)
 
-    xmax = diag.num_of_bins*diag.width_of_bin if xmax is None else xmax
-    logger.debug(f"xmax is : {xmax} {new_unit}.")
+        
+        mswep_folder_path = f'/work/bb1153/b382267/observations/MSWEP/{self.regrid}/{self.freq}'
+        # Check if the folder exists
+        if not os.path.exists(mswep_folder_path):
+            self.logger.error(f"Error: The folder for MSWEP data with resolution '{self.regrid}' "
+                            f"and frequency '{self.freq}' does not exist. Histograms for the "
+                            "desired resolution and frequency have not been computed yet.")
+        else:
+            obs_merged = self.diag.merge_list_of_histograms(path_to_histograms=mswep_folder_path, all=True, start_year=self.s_year, end_year=self.f_year,
+                                                        start_month=self.s_month, end_month=self.f_month)
+            self.logger.info(f"The MSWEP data with resolution '{self.regrid}' and frequency '{self.freq}' are prepared for comparison.")
 
-    hist_merged = diag.merge_list_of_histograms(path_to_histograms=path_to_histograms, all=True,
-                                                start_year=s_year, end_year=f_year, start_month=s_month, end_month=f_month)
-    
-    add = diag.histogram_plot(hist_merged, figsize=figsize, new_unit=new_unit,
-                        legend=legend, color=color, xmax=xmax, plot_title=plot_title, loc=loc,
-                        path_to_pdf=path_to_pdf, pdf_format=pdf_format, name_of_file=name_of_pdf)
-
-    
-    mswep_folder_path = f'/work/bb1153/b382267/observations/MSWEP/{regrid}/{freq}'
-    
-    # Check if the folder exists
-    if not os.path.exists(mswep_folder_path):
-        logger.error(f"Error: The folder for MSWEP data with resolution '{regrid}' "
-                        f"and frequency '{freq}' does not exist. Histograms for the "
-                        "desired resolution and frequency have not been computed yet.")
-    else:
-        obs_merged = diag.merge_list_of_histograms(path_to_histograms=mswep_folder_path, all=True, start_year=s_year, end_year=f_year,
-                                                    start_month=s_month, end_month=f_month)
-        logger.info(f"The MSWEP data with resolution '{regrid}' and frequency '{freq}' are prepared for comparison.")
-
-        diag.histogram_plot(hist_merged, figsize=figsize, new_unit=new_unit, add=add,
-                            linewidth=2*diag.plots.linewidth, linestyle='--', color='tab:red',
-                            legend=f"MSWEP", xmax=xmax, plot_title=plot_title, loc=loc,
-                            path_to_pdf=path_to_pdf, pdf_format=pdf_format, name_of_file=name_of_pdf)
-
-
-    logger.info("The histograms are plotted and saved in storage.")
-    
+            self.diag.histogram_plot(hist_merged, figsize=self.figsize, new_unit=self.new_unit, add=add,
+                                linewidth=2*self.diag.plots.linewidth, linestyle='--', color='tab:red',
+                                legend=f"MSWEP", xmax=self.xmax,  loc=self.loc, plot_title=plot_title,
+                                path_to_pdf=self.path_to_pdf, pdf_format=self.pdf_format, name_of_file=name_of_pdf)
+        self.logger.info("The histograms are plotted and saved in storage.")
+        
 def main():
     """Main function to orchestrate the tropical rainfall CLI operations."""
     args = parse_arguments(sys.argv[1:])
     validate_arguments(args)
-
-    # change the current directory to the one of the CLI so that relative path works
-    abspath = os.path.abspath(__file__)
-    dname = os.path.dirname(abspath)
-    if os.getcwd() != dname:
-        os.chdir(dname)
-        print(f'Moving from current directory to {dname} to run!')
-
-    try:
-        sys.path.insert(0, '../../')
-        from tropical_rainfall import Tropical_Rainfall
-    except ImportError as import_error:
-        print(f"ImportError occurred: {import_error}")
-        sys.exit(0)
-    except Exception as custom_error:
-        print(f"CustomError occurred: {custom_error}")
-        sys.exit(0)
-    else:
-        print("Modules imported successfully.")
-
+    
     config = load_configuration(get_arg(args, 'config', 'trop_rainfall_config.yml'))
-    loglevel = get_arg(args, 'loglevel', config['logger']['loglevel'])
-    logger = log_configure(log_name="Trop. Rainfall CLI", log_level=loglevel)
-    trop_lat, num_of_bins, first_edge, width_of_bin, model_variable, new_unit = get_config_values(config, 'class_attributes',
-                                                                                                  'trop_lat', 'num_of_bins',
-                                                                                                  'first_edge', 'width_of_bin',
-                                                                                                  'model_variable', 'new_unit')
-    diag = Tropical_Rainfall(trop_lat=trop_lat, num_of_bins=num_of_bins,
-                             first_edge=first_edge, width_of_bin=width_of_bin, loglevel=loglevel)
-
-    path_to_histograms, plot_title = calculate_histogram_by_months(diag=diag, config=config, logger=logger, args=args)
-    plot_histograms(diag=diag, config=config, logger=logger, args=args, path_to_histograms=path_to_histograms, plot_title=plot_title)
+    
+    mtpr_cli = MTPR_CLI(config, args)
+    mtpr_cli.calculate_histogram_by_months()
+    mtpr_cli.plot_histograms()
 
 if __name__ == '__main__':
     main()
