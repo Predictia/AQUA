57b13757af8c20987f17f70fb42ab2f384418896
mnurisso <m.nurisso@isac.cnr.it>
2024-02-20 14:05:06 +0200

    gregory as a class

--- diagnostics/global_time_series/__init__.py
+++ diagnostics/global_time_series/__init__.py
@@ -1,13 +1,14 @@
-from .gregory import plot_gregory
+from .gregory import GregoryPlot
 from .timeseries import Timeseries
 
 __version__ = "0.1.0"
 
-__all__ = ["plot_gregory",
+__all__ = ["GregoryPlot",
            "Timeseries"]
 
 # Changelog:
 
+# 0.1.1: Gregory plot as a class
 # 0.1.0: complete refactory of the timeseries as a class
 # 0.0.5: support for reference data in Gregory plot
 # 0.0.4: gregory plot in a separate file
