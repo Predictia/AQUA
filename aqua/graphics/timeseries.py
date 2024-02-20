266841a87e793bbd26f055e1a5dfd8d6287ce266
mnurisso <m.nurisso@isac.cnr.it>
2024-02-20 11:34:01 +0200

    error handling in plot

--- aqua/graphics/timeseries.py
+++ aqua/graphics/timeseries.py
@@ -2,6 +2,7 @@
 Function to plot timeseries and reference data,
 both with monthly and annual aggregation options
 """
+from aqua.logger import log_configure
 import matplotlib.pyplot as plt
 
 
@@ -13,6 +14,7 @@ def plot_timeseries(monthly_data=None,
                     std_annual_data=None,
                     data_labels: list = None,
                     ref_label: str = None,
+                    loglevel: str = 'WARNING',
                     **kwargs):
     """
     monthly_data and annual_data are list of xr.DataArray
@@ -28,6 +30,7 @@ def plot_timeseries(monthly_data=None,
         std_annual_data (xr.DataArray): standard deviation of the reference annual data
         data_labels (list of str): labels for the data
         ref_label (str): label for the reference data
+        loglevel (str): logging level
 
     Keyword Arguments:
         figsize (tuple): size of the figure
@@ -36,6 +39,7 @@ def plot_timeseries(monthly_data=None,
     Returns:
         fig, ax (tuple): tuple containing the figure and axis objects
     """
+    logger = log_configure(loglevel, 'PlotTimeseries')
     fig_size = kwargs.get('figsize', (10, 5))
     fig, ax = plt.subplots(1, 1, figsize=fig_size)
 
@@ -46,37 +50,55 @@ def plot_timeseries(monthly_data=None,
     for i in range(len(monthly_data)):
         color = color_list[i]
         if monthly_data:
-            mon_data = monthly_data[i]
-            if data_labels:
-                label = data_labels[i]
-                label += ' monthly'
-            mon_data.plot(ax=ax, label=label, color=color)
+            try:
+                mon_data = monthly_data[i]
+                if data_labels:
+                    label = data_labels[i]
+                    label += ' monthly'
+                mon_data.plot(ax=ax, label=label, color=color)
+            except Exception as e:
+                logger.debug(f"Error plotting monthly data: {e}")
+
+    for i in range(len(annual_data)):
+        color = color_list[i]
         if annual_data:
-            ann_data = annual_data[i]
-            if data_labels:
-                label = data_labels[i]
-                label += ' annual'
-            ann_data.plot(ax=ax, label=label, color=color, linestyle='--')
+            try:
+                ann_data = annual_data[i]
+                if data_labels:
+                    label = data_labels[i]
+                    label += ' annual'
+                ann_data.plot(ax=ax, label=label, color=color, linestyle='--')
+            except Exception as e:
+                logger.debug(f"Error plotting annual data: {e}")
 
     if ref_monthly_data is not None:
-        ref_monthly_data.plot(ax=ax, label=ref_label + ' monthly', color='black', lw=0.6)
-        if std_monthly_data is not None:
-            std_monthly_data.compute()
-            ax.fill_between(ref_monthly_data.time,
-                            ref_monthly_data - 2.*std_monthly_data.sel(month=ref_monthly_data["time.month"]),
-                            ref_monthly_data + 2.*std_monthly_data.sel(month=ref_monthly_data["time.month"]),
-                            facecolor='grey', alpha=0.25)
+        try:
+            ref_monthly_data.plot(ax=ax, label=ref_label + ' monthly', color='black', lw=0.6)
+            if std_monthly_data is not None:
+                std_monthly_data.compute()
+                ax.fill_between(ref_monthly_data.time,
+                                ref_monthly_data - 2.*std_monthly_data.sel(month=ref_monthly_data["time.month"]),
+                                ref_monthly_data + 2.*std_monthly_data.sel(month=ref_monthly_data["time.month"]),
+                                facecolor='grey', alpha=0.25)
+        except Exception as e:
+            logger.debug(f"Error plotting monthly std data: {e}")
 
     if ref_annual_data is not None:
-        ref_annual_data.plot(ax=ax, label=ref_label + ' annual', color='black', linestyle='--', lw=0.6)
-        if std_annual_data is not None:
-            std_annual_data.compute()
-            ax.fill_between(ref_annual_data.time,
-                            ref_annual_data - 2.*std_annual_data,
-                            ref_annual_data + 2.*std_annual_data,
-                            facecolor='black', alpha=0.2)
+        try:
+            ref_annual_data.plot(ax=ax, label=ref_label + ' annual', color='black', linestyle='--', lw=0.6)
+            if std_annual_data is not None:
+                std_annual_data.compute()
+                ax.fill_between(ref_annual_data.time,
+                                ref_annual_data - 2.*std_annual_data,
+                                ref_annual_data + 2.*std_annual_data,
+                                facecolor='black', alpha=0.2)
+        except Exception as e:
+            logger.debug(f"Error plotting annual std data: {e}")
 
     ax.legend(fontsize='small')
+    ax.grid(axis="x", color="k")
+    ax.spines["right"].set_visible(False)
+    ax.spines["top"].set_visible(False)
 
     title = kwargs.get('title', None)
     if title:
