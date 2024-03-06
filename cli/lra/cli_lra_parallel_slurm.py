270c0bc230f4069afc95e03f35ce90c6176f8b59
Paolo Davini <p.davini@isac.cnr.it>
2024-03-06 10:07:40 +0200

    new features

--- cli/lra/cli_lra_parallel_slurm.py
+++ cli/lra/cli_lra_parallel_slurm.py
@@ -9,37 +9,60 @@ import subprocess
 import argparse
 import re
 import sys
-import yaml
 from aqua.util import load_yaml, get_arg
 
-def submit_sbatch(model, exp, source, varname, yaml_file,
-                  workers, definitive, overwrite, dependency=None):
+def is_job_running(job_name, username):
+    """verify that a job name is not already submitted in the slurm queue"""
+    # Run the squeue command to get the list of jobs
+    output = subprocess.run(['squeue', '-u', username, '--format', '%j'], 
+                            capture_output=True, check=True)
+    output = output.stdout.decode('utf-8').splitlines()[1:]
+    
+    # Parse the output to check if the job name is in the list
+    return job_name in output
+
+def submit_sbatch(model, exp, source, varname, slurm_dict, yaml_file,
+                  workers=1, definitive=False, overwrite=False, dependency=None):
     
     """
     Submit a sbatch script for the LRA CLI with basic options
-    """
 
-    # this is still optional, we can read from configuration file the default partition properties
-    # for now it is still hardcoded with LUMI default
-    with open(yaml_file, 'r') as stream:
-        config = yaml.safe_load(stream)
+    args:
+        model: model to be processed
+        exp: exp to be processed
+        source: source to be processed
+        varname: varname to be processed
+        slurm_dict: dictionary with slurm optiosn
+        yaml_file: config file for submission
+        workers: dask workers
+        definitive: produce the LRA
+        overwrite: overwrite the LRA
+        dependency: jobid on which dependency of slurm is built
+
+    Return
+        jobid
+    """
 
     # create identifier for each model-exp-source-var tuple
     job_name = "_".join([model, exp, source, varname])
+    full_job_name = 'lra-generator_' + job_name
+
+    # username
+    username = slurm_dict.get('username', 'padavini')
 
     # Construct basic sbatch command
     sbatch_cmd = [
         'sbatch',
-        '--partition=' + config.get('partition', 'ju-strategic'),
-        '--job-name=' + config.get('job_name', 'lra-generator_' + job_name),
-        '--output=' + config.get('output', 'log/overnight-lra_' + job_name + '_%j.out'),
-        '--error=' + config.get('error', 'log/overnight-lra_' + job_name + '_%j.err'),
-        '--account=' + config.get('account', 'project_465000454'),
-        '--nodes=' + str(config.get('nodes', 1)),
-        '--ntasks-per-node=' + str(config.get('ntasks_per_node', workers)),
-        '--time=' + config.get('time', '08:00:00')
+        '--partition=' + slurm_dict.get('partition', 'small'),
+        '--job-name=' + slurm_dict.get('job_name', full_job_name),
+        '--output=log/overnight-lra_' + job_name + '_%j.out',
+        '--error=log/overnight-lra_' + job_name + '_%j.err',
+        '--account=' + slurm_dict.get('account', 'project_465000454'),
+        '--nodes=' + str(slurm_dict.get('nodes', 1)),
+        '--ntasks-per-node=' + str(slurm_dict.get('ntasks_per_node', workers)),
+        '--time=' + slurm_dict.get('time', '08:00:00'),
+        '--mem=' + slurm_dict.get('mem', '256G')
         #'--priority=low',
-        #'--mem=' + config.get('mem', '256G')
     ]
 
     if dependency is not None:
@@ -60,17 +83,22 @@ def submit_sbatch(model, exp, source, varname, yaml_file,
     sbatch_cmd.append('-w')
     sbatch_cmd.append(str(workers))
 
+    if is_job_running(full_job_name, username):
+        print(f'The job is {job_name} is already running, will not resubmit')
+        return 0
+
     # Execute sbatch command
     if definitive:
         if overwrite:
             sbatch_cmd.append('-o')
         sbatch_cmd.append('-d')
-        result = subprocess.run(sbatch_cmd,  capture_output = True).stdout.decode('utf-8')
+        result = subprocess.run(sbatch_cmd, capture_output = True, check=True).stdout.decode('utf-8')
         jobid = re.findall(r'\b\d+\b', result)[-1]
-    else:
-        print(sbatch_cmd)
+        return jobid
 
-    return jobid
+    #print(sbatch_cmd)
+    return 0
+
 
 def parse_arguments(arguments):
     """
@@ -79,7 +107,9 @@ def parse_arguments(arguments):
 
     parser = argparse.ArgumentParser(description='AQUA LRA parallel SLURM generator')
     parser.add_argument('-c', '--config', type=str,
-                        help='yaml configuration file', required=True)
+                        help='AQUA yaml configuration file', required=True)
+    parser.add_argument('-s', '--slurm', type=str,
+                        help='SLURM yaml configuration file')
     parser.add_argument('-d', '--definitive', action="store_true",
                         help='definitive run with files creation')
     parser.add_argument('-o', '--overwrite', action="store_true",
@@ -95,7 +125,8 @@ def parse_arguments(arguments):
 if __name__ == '__main__':
 
     args = parse_arguments(sys.argv[1:])
-    file = get_arg(args, 'config', None)
+    config_file = get_arg(args, 'config', None)
+    slurm_file = get_arg(args, 'slurm', None)
     workers = get_arg(args, 'workers', 8)
     definitive = get_arg(args, 'definitive', False)
     overwrite = get_arg(args, 'overwrite', False)
@@ -103,7 +134,9 @@ if __name__ == '__main__':
     print('Reading configuration yaml file..')
 
     # loading the usual configuration file
-    config = load_yaml(file)
+    config = load_yaml(config_file)
+
+    slurm = config.get('slurm', {})
 
     # sbatch looping
     COUNT = 0 # to count job
@@ -119,5 +152,7 @@ if __name__ == '__main__':
                         PARENT_JOB = str(jobid)
                     COUNT = COUNT + 1
                     print(' '.join(['Submitting', model, exp, source, varname]))
-                    jobid = submit_sbatch(model, exp, source, varname, file,
-                            workers, definitive, overwrite, dependency=PARENT_JOB)
+                    jobid = submit_sbatch(model=model, exp=exp, source=source, varname=varname,
+                                          slurm_dict=slurm, yaml_file=config_file,
+                                          workers=workers, definitive=definitive,
+                                          overwrite=overwrite, dependency=PARENT_JOB)
