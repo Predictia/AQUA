e4f57b0ff0c573ad4d59b7a4c67e94e34f3d8de7
Paolo Davini <p.davini@isac.cnr.it>
2024-03-13 14:00:15 +0200

    LRA memorysampler monitoring by default

--- aqua/lra_generator/lra_generator.py
+++ aqua/lra_generator/lra_generator.py
@@ -13,6 +13,7 @@ import numpy as np
 import pandas as pd
 from dask.distributed import Client, LocalCluster, progress, performance_report
 from dask.diagnostics import ProgressBar
+from dask.distributed.diagnostics import MemorySampler
 from aqua.logger import log_configure, log_history
 from aqua.reader import Reader
 from aqua.util import create_folder, generate_random_string
@@ -490,22 +491,16 @@ class LRAgenerator():
 
                 month_data = year_data.sel(time=year_data.time.dt.month == month)
 
-                # HACK: check for ifs wrong fluxes only
-                #if len(month_data.time)>1:
-                #    month_data = check_correct_ifs_fluxes(month_data, loglevel=self.loglevel)
-                #if month_data.isnull().all():
-                #    self.logger.warning('All the records are null for month %s, skipping this...', month)
-                #    continue
-                #month_data = self.reader.regrid(month_data)
-                #month_data = self._remove_regridded(month_data)
-
-                self.logger.debug(month_data.mean().values)
-                self.logger.debug(month_data)
+                #self.logger.debug(month_data.mean().values)
+                #self.logger.debug(month_data)
 
                 # real writing
                 if self.definitive:
                     tmpfile = self.get_filename(var, year = year, month = month, tmp = True)
+                    schunk = time()
                     self.write_chunk(month_data, tmpfile)
+                    tchunk = time() - schunk
+                    self.logger.info('Chunk execution time: %.2f', tchunk)
 
                     # check everything is correct
                     filecheck = file_is_complete(tmpfile, loglevel=self.loglevel)
@@ -542,6 +537,7 @@ class LRAgenerator():
                                    compute=False)
 
         if self.dask:
+            # optional full stack dashboard to html
             if self.performance_reporting:
                 filename = f"dask-{self.model}-{self.exp}-{self.source}-{self.nproc}.html"
                 with performance_report(filename=filename):
@@ -549,9 +545,16 @@ class LRAgenerator():
                     progress(w_job)
                     del w_job
             else:
-                w_job = write_job.persist()
-                progress(w_job)
-                del w_job
+                # memory monitoring is always operating
+                ms = MemorySampler()
+                with ms.sample('chunk'):
+                    w_job = write_job.persist()
+                    progress(w_job)
+                    del w_job
+                array_data = np.array(vars(ms)['samples']['chunk'])
+                avg_mem = np.mean(array_data[:, 1])/1e9
+                max_mem = np.max(array_data[:, 1])/1e9
+                self.logger.info('Avg memory used: %.2f GiB, Peak memory used: %.2f GiB', avg_mem, max_mem)
                 
         else:
             with ProgressBar():
