62a18010ee4ce017e8708fc21b5355ce4ae95a94
Paolo Davini <p.davini@isac.cnr.it>
2024-04-02 18:02:31 +0200

    add timmean test

--- tests/test_timmean.py
+++ tests/test_timmean.py
@@ -3,6 +3,7 @@
 import pytest
 import numpy as np
 from aqua import Reader
+import pytest
 
 loglevel = "DEBUG"
 
@@ -69,6 +70,15 @@ class TestTimmean():
         assert avg['ttr'].shape == (1, 9, 18) # 1 year was computed
         assert avg['ttr'].time[0].values == np.datetime64('2020-07-01T00:00:00.000000000')
 
+    def test_timmean_monthly_center_time(self):
+        """Timmean test for monthly aggregation with center_time=True"""
+        reader = Reader(model="IFS", exp="test-tco79", source='long',
+                        fix=False, loglevel=loglevel)
+        data = reader.retrieve(var='2t')
+        avg = reader.timmean(data, freq='monthly', center_time=True)
+        assert avg['2t'].shape == (8, 9, 18)
+        assert avg['2t'].time[1].values == np.datetime64('2020-02-15T00:00:00.000000000')
+
     def test_timmean_daily_center_time(self):
         """Timmean test for daily aggregation with center_time=True and exclude_incomplete=True"""
         reader = Reader(model="IFS", exp="test-tco79", source='long',
@@ -77,13 +87,38 @@ class TestTimmean():
         avg = reader.timmean(data, freq='daily', center_time=True, exclude_incomplete=True)
         assert avg['2t'].shape == (197, 9, 18)
         assert avg['2t'].time[1].values == np.datetime64('2020-01-21T12:00:00.000000000')
-
-    def test_timmean_pandas(self):
+        
+    def test_timmean_pandas_accessor(self):
         """Timmean test for weekly aggregation based on pandas labels"""
         reader = Reader(model="IFS", exp="test-tco79", source='long',
                         loglevel=loglevel)
-
         data = reader.retrieve(var='2t')
-        avg = reader.timmean(data, freq='W-MON')
-
+        avg = data.aqua.timmean(freq='W-MON')
         assert avg['2t'].shape == (29, 9, 18)
+
+    def test_timmean_time_bounds(self):
+        """Test for _timmean method with time_bounds=True"""
+        reader = Reader(model="IFS", exp="test-tco79", source='long',
+                        loglevel=loglevel)
+        data = reader.retrieve(var='2t')
+        avg = reader.timmean(data, freq='monthly', time_bounds=True)
+        assert 'time_bnds' in avg
+        assert avg['2t'].shape == (8, 9, 18)
+        assert avg['time_bnds'].shape == (avg['2t'].shape[0], 2)
+        assert np.all(avg['time_bnds'].isel(bnds=0) <= avg['time_bnds'].isel(bnds=1))
+    
+    def test_timmean_invalid_frequency(self):
+        """Test for _timmean method with an invalid frequency"""
+        reader = Reader(model="IFS", exp="test-tco79", source='long',
+                        loglevel=loglevel)
+        data = reader.retrieve(var='2t')
+        with pytest.raises(ValueError, match=r'Cant find a frequency to resample, aborting!'):
+            reader.timmean(data, freq='invalid')
+
+    def test_timmean_single_timestep(self):
+        """Test for _timmean method with a single timestep"""
+        reader = Reader(model="IFS", exp="test-tco79", source='long')
+        data = reader.retrieve(var='2t')
+        with pytest.warns(UserWarning, match=r'A single timestep is available, is this correct?'):
+            avg = reader.timmean(data, freq='monthly')
+        assert avg.shape == (1, 9, 18)
