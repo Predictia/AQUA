32979a9203162999e7c17389f289a159a6fae451
Paolo Davini <p.davini@isac.cnr.it>
2024-04-09 12:28:20 +0300

    version

--- aqua/__init__.py
+++ aqua/__init__.py
@@ -7,7 +7,7 @@ from .reader import Reader, catalogue, Streaming, inspect_catalogue
 from .slurm import squeue, job, output_dir, scancel, max_resources_per_node
 from .accessor import AquaAccessor
 
-__version__ = '0.7.3'
+__version__ = '0.8'
 
 __all__ = ["rundiag",
            "plot_single_map", "plot_maps", "plot_single_map_diff", "plot_timeseries",
