c1809c239b743c55a78e5a19c3c344c83bb89d25
Paolo Davini <p.davini@isac.cnr.it>
2024-04-10 09:08:59 +0200

    add loggin to zarr

--- aqua/util/zarr.py
+++ aqua/util/zarr.py
@@ -2,8 +2,9 @@ import os
 import json
 from kerchunk.hdf import SingleHdf5ToZarr
 from kerchunk.combine import MultiZarrToZarr
+from aqua import log_configure
 
-def create_zarr(filelist, outfile):
+def create_zarr(filelist, outfile, loglevel='WARNING'):
     """
     Create a Zarr file from a list of HDF5/NetCDF files.
 
@@ -15,20 +16,24 @@ def create_zarr(filelist, outfile):
         None
     """
 
+    logger = log_configure(log_level=loglevel, log_name='zarr creation')
+
+    logger.info('Creating Zarr file from %s', filelist)
     singles = [SingleHdf5ToZarr(filepath, inline_threshold=0).translate() for filepath in sorted(filelist)]
 
+    logger.info('Combining Zarr files')
     mzz = MultiZarrToZarr(
         singles,
         concat_dims=["time"],
         identical_dims=['lat', 'lon']
     )
 
+    logger.info('Translating Zarr files to json')
     out = mzz.translate()
 
     # Dump to file
-    print('Create json file...')
+    logger.info('Dumping to file JSON %s', outfile)
     if os.path.exists(outfile):
         os.remove(outfile)
-
     with open(outfile, "w") as outfile:
         json.dump(out, outfile)
