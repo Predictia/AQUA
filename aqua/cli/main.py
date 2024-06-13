13e6f5eaa4ae882a23e6239c9203a66ae636aa0e
Paolo Davini <p.davini@isac.cnr.it>
2024-06-13 15:37:58 +0300

    lost commit

--- aqua/cli/main.py
+++ aqua/cli/main.py
@@ -11,7 +11,7 @@ from aqua.util import load_yaml, dump_yaml, load_multi_yaml
 from aqua.logger import log_configure
 from aqua.util import ConfigPath
 from aqua.cli.parser import parse_arguments
-from aqua.util.util import HiddenPrints
+from aqua.util.util import HiddenPrints, to_list
 from aqua import __path__ as pypath
 from aqua import catalogue
 
@@ -423,7 +423,10 @@ class AquaConsole():
 
         self.logger.info('Setting catalog name to %s', catalog)
         cfg = load_yaml(self.configfile)
-        cfg['catalog'] = catalog
+        if cfg['catalog'] != 'lumi': #HACK lumi is default catalog
+            cfg['catalog'] = [catalog] + to_list(cfg['catalog'])
+        else:
+            cfg['catalog'] = catalog
         dump_yaml(self.configfile, cfg)
 
     def remove(self, args):
