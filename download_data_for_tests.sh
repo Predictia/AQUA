1d436ba2072739953aa68dc4eb8f04b6fda05def
Jost von Hardenberg <hardenberg@gmail.com>
2024-07-10 15:27:53 +0300

    update ci workflow

--- download_data_for_tests.sh
+++ download_data_for_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-file_url="https://swift.dkrz.de/v1/dkrz_a973e394-5f24-4f4d-8bbf-1a83bd387ccb/AQUA/framework/AQUA_testsv3.tar.gz?temp_url_sig=592e0d372b31279d26bafb09f228c5af3857bc96&temp_url_expires=2027-02-02T15:29:08Z"
-file_path="AQUA_testsv3.tar.gz"
+file_url="https://swift.dkrz.de/v1/dkrz_a973e394-5f24-4f4d-8bbf-1a83bd387ccb/AQUA/framework/AQUA_testsv4.tar.gz?temp_url_sig=0edd32127555f6c9ff3e0eb750746d622d1d8d4f&temp_url_expires=2025-01-06T11:35:45Z"
+file_path="AQUA_testsv4.tar.gz"
 
 if [ ! -f "$file_path" ]; then
     echo "Downloading file..."
