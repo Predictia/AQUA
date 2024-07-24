ca96d6e529b28187640448e8e7b1547de5f11f1d
Paolo Davini <p.davini@isac.cnr.it>
2024-07-24 09:00:59 +0200

    upgrade logger

--- aqua/cli/main.py
+++ aqua/cli/main.py
@@ -62,12 +62,12 @@ class AquaConsole():
 
         # Set the log level
         if args.very_verbose or (args.verbose and args.very_verbose):
-            loglevel = 'DEBUG'
+            self.loglevel = 'DEBUG'
         elif args.verbose:
-            loglevel = 'INFO'
+            self.loglevel = 'INFO'
         else:
-            loglevel = 'WARNING'
-        self.logger = log_configure(loglevel, 'AQUA')
+            self.loglevel = 'WARNING'
+        self.logger = log_configure(self.loglevel, 'AQUA')
 
         command = args.command
         method = self.command_map.get(command, parser_dict['main'].print_help)
@@ -229,7 +229,7 @@ class AquaConsole():
                 sys.exit(1)
 
             # Ensure the target directory exists using create_folder
-            create_folder(target_directory, loglevel="WARNING")
+            create_folder(target_directory, loglevel=loglevel)
 
             if not os.path.exists(target_file):
                 self.logger.debug('Copying from %s to %s', source_file, target_file)
@@ -567,7 +567,7 @@ class AquaConsole():
     def _check(self, silent=False):
         """check installation"""
 
-        checklevel = 'ERROR' if silent else 'WARNING'
+        checklevel = 'ERROR' if silent else self.loglevel
         try:
             self.configpath = ConfigPath(loglevel=checklevel).configdir
             self.configfile = os.path.join(self.configpath, 'config-aqua.yaml')
