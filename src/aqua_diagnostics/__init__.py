006d4af72888c2230f1c0231538f04286aa7f407
silviacaprioli <silvietta.caprioli@gmail.com>
2024-10-24 15:05:26 +0300

    add Radiation into init

--- src/aqua_diagnostics/__init__.py
+++ src/aqua_diagnostics/__init__.py
@@ -1,8 +1,9 @@
 from .timeseries import GregoryPlot, SeasonalCycle, Timeseries
 from .global_biases import GlobalBiases
+from .radiation import Radiation
 
 __version__ = "0.0.1"
 
-__all__ = ["GregoryPlot", "SeasonalCycle", "Timeseries", "GlobalBiases"]
+__all__ = ["GregoryPlot", "SeasonalCycle", "Timeseries", "GlobalBiases", "Radiation"]
 
 
