142af75bd1bbba3ef12b16077c910290b6535533
silviacaprioli <silvietta.caprioli@gmail.com>
2024-10-29 17:49:21 +0200

    general cli polishing

--- src/aqua_diagnostics/global_biases/cli/cli_global_biases.py
+++ src/aqua_diagnostics/global_biases/cli/cli_global_biases.py
@@ -1,4 +1,4 @@
-# All necessary import for a cli diagnostic
+# Imports for CLI diagnostic
 import sys
 import os
 import argparse
@@ -11,159 +11,145 @@ from aqua.logger import log_configure
 from aqua.diagnostics import GlobalBiases
 
 def parse_arguments(args):
-    """Parse command line arguments"""
+    """Parse command line arguments for Global Biases CLI."""
+    parser = argparse.ArgumentParser(description='Global Biases CLI')
+    parser.add_argument('-c', '--config', type=str, required=True, help='YAML configuration file')
+    parser.add_argument('-n', '--nworkers', type=int, help='Number of Dask distributed workers')
+    parser.add_argument("--loglevel", "-l", type=str, help="Logging level")
 
-    parser = argparse.ArgumentParser(description='Global biases CLI')
-    parser.add_argument('-c', '--config', type=str,
-                        help='yaml configuration file')
-    parser.add_argument('-n', '--nworkers', type=int,
-                        help='number of dask distributed workers')
-    parser.add_argument("--loglevel", "-l", type=str,
-                        required=False, help="loglevel")                  
-
-    # This arguments will override the configuration file if provided
-    parser.add_argument("--catalog", type=str,
-                        required=False, help="catalog name")
-    parser.add_argument('--model', type=str, help='model name',
-                        required=False)
-    parser.add_argument('--exp', type=str, help='experiment name',
-                        required=False)
-    parser.add_argument('--source', type=str, help='source name',
-                        required=False)
-    parser.add_argument('--outputdir', type=str, help='output directory',
-                        required=False)
+    # Arguments to override configuration file settings
+    parser.add_argument("--catalog", type=str, help="Catalog name")
+    parser.add_argument('--model', type=str, help='Model name')
+    parser.add_argument('--exp', type=str, help='Experiment name')
+    parser.add_argument('--source', type=str, help='Source name')
+    parser.add_argument('--outputdir', type=str, help='Output directory')
 
     return parser.parse_args(args)
 
-
-
-if __name__ == '__main__':
-
-    args = parse_arguments(sys.argv[1:])
-
-    loglevel = get_arg(args, "loglevel", "WARNING")
-    logger = log_configure(loglevel, 'CLI Global Biases')
-    logger.info("Running Global Biases diagnostic")
-
-    # Moving to the current directory so that relative paths work
-    abspath = os.path.abspath(__file__)
-    dname = os.path.dirname(abspath)
-    if os.getcwd() != dname:
-        os.chdir(dname)
-        logger.info(f"Changing directory to {dname}")
-
-    # Dask distributed cluster
-    nworkers = get_arg(args, 'nworkers', None)
+def initialize_dask(nworkers, logger):
+    """Initialize Dask distributed cluster if nworkers is specified."""
     if nworkers:
         cluster = LocalCluster(n_workers=nworkers, threads_per_worker=1)
         client = Client(cluster)
-        logger.info(f"Running with {nworkers} dask distributed workers.")
+        logger.info(f"Running with {nworkers} Dask distributed workers.")
+        return client
+    return None
 
-    # Aquiring the configuration
-    file = get_arg(args, "config", "global_bias_config.yaml")
-    logger.info(f"Reading configuration file {file}")
-    config = load_yaml(file)
+def main():
+    args = parse_arguments(sys.argv[1:])
+    loglevel = get_arg(args, "loglevel", "WARNING")
+    logger = log_configure(loglevel, 'CLI Global Biases')
+    logger.info("Starting Global Biases diagnostic")
 
-    # Acquiring model, experiment and source
+    # Set working directory to script location for relative paths
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    if os.getcwd() != script_dir:
+        os.chdir(script_dir)
+        logger.info(f"Changing working directory to {script_dir}")
+
+    client = initialize_dask(get_arg(args, 'nworkers', None), logger)
+
+    # Load configuration and set diagnostic attributes
+    config_file = get_arg(args, "config", "global_bias_config.yaml")
+    logger.info(f"Reading configuration file {config_file}")
+    config = load_yaml(config_file)
+
     catalog_data = get_arg(args, 'catalog', config['data']['catalog'])
     model_data = get_arg(args, 'model', config['data']['model'])
     exp_data = get_arg(args, 'exp', config['data']['exp'])
     source_data = get_arg(args, 'source', config['data']['source'])
-    startdate_data = config['diagnostic_attributes'].get('startdate_data', None) 
-    enddate_data = config['diagnostic_attributes'].get('enddate_data', None)
+    startdate_data = config['diagnostic_attributes'].get('startdate_data')
+    enddate_data = config['diagnostic_attributes'].get('enddate_data')
 
-    # Acquiring model, experiment and source for reference data
     catalog_obs = config['obs']['catalog']
     model_obs = config['obs']['model']
     exp_obs = config['obs']['exp']
     source_obs = config['obs']['source']
-    startdate_obs = config['diagnostic_attributes'].get('startdate_obs', None) 
-    enddate_obs = config['diagnostic_attributes'].get('enddate_obs', None)
+    startdate_obs = config['diagnostic_attributes'].get('startdate_obs')
+    enddate_obs = config['diagnostic_attributes'].get('enddate_obs')
 
-    #creating output directory
     outputdir = get_arg(args, "outputdir", config["outputdir"])
     out_pdf = os.path.join(outputdir, 'pdf')
-    out_netcdf = os.path.join(outputdir, 'netcdf')
-    create_folder(out_pdf, loglevel )
-    create_folder(out_netcdf, loglevel)
+    create_folder(out_pdf, loglevel)
 
     variables = config['diagnostic_attributes'].get('variables', [])
-    plev = config['diagnostic_attributes'].get('plev', None)
-    logger.info(f"plev: {plev}")
+    plev = config['diagnostic_attributes'].get('plev')
     seasons_bool = config['diagnostic_attributes'].get('seasons', False)
     seasons_stat = config['diagnostic_attributes'].get('seasons_stat', 'mean')
     vertical = config['diagnostic_attributes'].get('vertical', False)
 
     names = OutputSaver(diagnostic='global_biases', model=model_data, exp=exp_data, loglevel=loglevel)
 
+    # Retrieve data and handle potential errors
     try:
-        reader = Reader(catalog = catalog_data, model=model_data, exp=exp_data, source=source_data, startdate=startdate_data, enddate=enddate_data)
+        reader = Reader(catalog=catalog_data, model=model_data, exp=exp_data, source=source_data,
+                        startdate=startdate_data, enddate=enddate_data)
         data = reader.retrieve()
     except Exception as e:
         logger.error(f"No model data found: {e}")
-        logger.critical("Global mean biases diagnostic is terminated.")
-        sys.exit(0)
+        sys.exit("Global biases diagnostic terminated.")
 
     try:
-        reader_obs = Reader(catalog = catalog_obs, model=model_obs, exp=exp_obs, source=source_obs, startdate=startdate_obs, enddate=enddate_obs, loglevel=loglevel)
+        reader_obs = Reader(catalog=catalog_obs, model=model_obs, exp=exp_obs, source=source_obs,
+                            startdate=startdate_obs, enddate=enddate_obs, loglevel=loglevel)
         data_obs = reader_obs.retrieve()
     except Exception as e:
         logger.error(f"No observation data found: {e}")
 
+    # Update date attributes with data if not set in config
     startdate_data = startdate_data or data.time[0].values
     enddate_data = enddate_data or data.time[-1].values
     startdate_obs = startdate_obs or data_obs.time[0].values
     enddate_obs = enddate_obs or data_obs.time[-1].values
 
-    # Loop on varibables
+    # Loop over variables for diagnostics
     for var_name in variables:
-        logger.info(f"Running Global Biases diagnostic for {var_name}...")
+        logger.info(f"Running Global Biases diagnostic for variable: {var_name}")
         var_attributes = config["biases_plot_params"]['bias_maps'].get(var_name, {})
-        vmin = var_attributes.get('vmin', None)
-        vmax = var_attributes.get('vmax', None)
-        logger.debug(f"var: {var_name}, vmin: {vmin}, vmax: {vmax}")
+        vmin, vmax = var_attributes.get('vmin'), var_attributes.get('vmax')
 
         try:
             global_biases = GlobalBiases(data=data, data_ref=data_obs, var_name=var_name, plev=plev, loglevel=loglevel,
-                                        model= model_data, exp=exp_data, startdate_data=startdate_data, enddate_data=enddate_data,
-                                        model_obs=model_obs, startdate_obs=startdate_obs, enddate_obs=enddate_obs)                               
-            # Total bias
+                                         model=model_data, exp=exp_data, startdate_data=startdate_data,
+                                         enddate_data=enddate_data, model_obs=model_obs,
+                                         startdate_obs=startdate_obs, enddate_obs=enddate_obs)
+
+            # Total bias plot
             result = global_biases.plot_bias(vmin=vmin, vmax=vmax)
-            
-            if result is not None:
-                fig, ax = result 
+            if result:
+                fig, ax = result
                 names.generate_name(diagnostic_product='total_bias_map', var=var_name)
                 names.save_pdf(fig, path=out_pdf, var=var_name)
-                
             else:
-                logger.warning(f"No total bias plot generated for {var_name}. Skipping save.")
+                logger.warning(f"Total bias plot not generated for {var_name}.")
 
-            # Seasonal bias
-            result = global_biases.plot_seasonal_bias(vmin=vmin, vmax=vmax)
-            if result is not None:
-                fig, ax = result 
-                names.generate_name(diagnostic_product='seasonal_bias_map', var=var_name)
-                names.save_pdf(fig, path=out_pdf, var=var_name)
-            else:
-                logger.warning(f"No seasonal bias plot generated for {var_name}. Skipping save.") 
+            # Seasonal bias plot
+            if seasons_bool:
+                result = global_biases.plot_seasonal_bias(vmin=vmin, vmax=vmax)
+                if result:
+                    fig, ax = result
+                    names.generate_name(diagnostic_product='seasonal_bias_map', var=var_name)
+                    names.save_pdf(fig, path=out_pdf, var=var_name)
+                else:
+                    logger.warning(f"Seasonal bias plot not generated for {var_name}.")
 
-            # Vertical bias
+            # Vertical bias plot
             if vertical and 'plev' in data[var_name].dims:
                 var_attributes_vert = config["biases_plot_params"]['vertical_plev'].get(var_name, {})
-                vmin = var_attributes_vert.get('vmin', None)
-                vmax = var_attributes_vert.get('vmax', None)
-                logger.debug(f"var: {var_name}, vmin: {vmin}, vmax: {vmax}")
+                vmin, vmax = var_attributes_vert.get('vmin'), var_attributes_vert.get('vmax')
 
                 result = global_biases.plot_vertical_bias(var_name=var_name, vmin=vmin, vmax=vmax)
-                if result is not None:
-                    fig, ax = result  # Only unpack if result is not None
+                if result:
+                    fig, ax = result
                     names.generate_name(diagnostic_product='vertical_bias', var=var_name)
                     names.save_pdf(fig, path=out_pdf, var=var_name)
                 else:
-                    logger.warning(f"No vertical bias plot generated for {var_name}. Skipping save.")
+                    logger.warning(f"Vertical bias plot not generated for {var_name}.")
 
         except Exception as e:
-            logger.error(f"An unexpected error occurred while processing {var_name}: {e}")
+            logger.error(f"Error processing {var_name}: {e}")
 
-    logger.info("Global Biases diagnostic is terminated.")
+    logger.info("Global Biases diagnostic completed.")
 
+if __name__ == '__main__':
+    main()
--- src/aqua_diagnostics/radiation/cli/cli_radiation.py
+++ src/aqua_diagnostics/radiation/cli/cli_radiation.py
@@ -1,11 +1,9 @@
-# All necessary import for a cli diagnostic
+# Imports for CLI
 import sys
 import os
 import argparse
 from dask.distributed import Client, LocalCluster
-
 import numpy as np
-
 from aqua.util import load_yaml, get_arg, OutputSaver, create_folder
 from aqua import Reader
 from aqua.exceptions import NotEnoughDataError, NoDataError, NoObservationError
@@ -14,57 +12,50 @@ from aqua.diagnostics import Radiation
 
 def parse_arguments(args):
     """Parse command line arguments"""
-
     parser = argparse.ArgumentParser(description='Radiation CLI')
-    parser.add_argument('-c', '--config', type=str,
-                        help='yaml configuration file')
-    parser.add_argument('-n', '--nworkers', type=int,
-                        help='number of dask distributed workers')
-    parser.add_argument("--loglevel", "-l", type=str,
-                        required=False, help="loglevel")                  
-
-    # This arguments will override the configuration file if provided
-    parser.add_argument("--catalog", type=str,
-                        required=False, help="catalog name")
-    parser.add_argument('--model', type=str, help='model name',
-                        required=False)
-    parser.add_argument('--exp', type=str, help='experiment name',
-                        required=False)
-    parser.add_argument('--source', type=str, help='source name',
-                        required=False)
-    parser.add_argument('--outputdir', type=str, help='output directory',
-                        required=False)
-
+    parser.add_argument('-c', '--config', type=str, help='yaml configuration file')
+    parser.add_argument('-n', '--nworkers', type=int, help='number of Dask distributed workers')
+    parser.add_argument("--loglevel", "-l", type=str, required=False, help="loglevel")                  
+    # Override config file if provided
+    parser.add_argument("--catalog", type=str, required=False, help="catalog name")
+    parser.add_argument('--model', type=str, help='model name', required=False)
+    parser.add_argument('--exp', type=str, help='experiment name', required=False)
+    parser.add_argument('--source', type=str, help='source name', required=False)
+    parser.add_argument('--outputdir', type=str, help='output directory', required=False)
     return parser.parse_args(args)
 
+def initialize_dask(nworkers, logger):
+    """Set up a Dask distributed cluster with a specified number of workers."""
+    cluster = LocalCluster(n_workers=nworkers, threads_per_worker=1)
+    client = Client(cluster)
+    logger.info(f"Running with {nworkers} Dask distributed workers.")
+    return client
 
 if __name__ == '__main__':
-
     args = parse_arguments(sys.argv[1:])
-
     loglevel = get_arg(args, "loglevel", "WARNING")
     logger = log_configure(loglevel, 'CLI Radiation')
     logger.info("Running Radiation diagnostic")
 
-    # Moving to the current directory so that relative paths work
+    # Move to script directory
     abspath = os.path.abspath(__file__)
     dname = os.path.dirname(abspath)
     if os.getcwd() != dname:
         os.chdir(dname)
         logger.info(f"Changing directory to {dname}")
 
-    # Dask distributed cluster
+    # Initialize Dask if nworkers provided
     nworkers = get_arg(args, 'nworkers', None)
+    client = None
     if nworkers:
-        cluster = LocalCluster(n_workers=nworkers, threads_per_worker=1)
-        client = Client(cluster)
-        logger.info(f"Running with {nworkers} dask distributed workers.")
+        client = initialize_dask(nworkers, logger)
 
-    # Aquiring the configuration
+    # Load configuration
     file = get_arg(args, "config", "radiation_config.yaml")
     logger.info(f"Reading configuration file {file}")
     config = load_yaml(file)
 
+    # Process model information and datasets
     models = config['models']
     models[0]['catalog'] = get_arg(args, 'catalog', models[0]['catalog'])
     models[0]['model'] = get_arg(args, 'model', models[0]['model'])
@@ -72,14 +63,12 @@ if __name__ == '__main__':
     models[0]['source'] = get_arg(args, 'source', models[0]['source'])
 
     logger.debug("Analyzing models:")
-    models_list = []
-    exp_list = []
-    datasets = []
+    models_list, exp_list, datasets = [], [], []
     variables = config['diagnostic_attributes'].get('variables', ['-mtnlwrf', 'mtnswrf'])
 
     for model in models:
         try:
-            reader = Reader(catalog = model['catalog'], model=model['model'], exp=model['exp'], source=model['source'])
+            reader = Reader(catalog=model['catalog'], model=model['model'], exp=model['exp'], source=model['source'])
             dataset = reader.retrieve()   
         except Exception as e:
             logger.error(f"No model data found: {e}")
@@ -89,21 +78,17 @@ if __name__ == '__main__':
         models_list.append(model['model'])
         exp_list.append(model['exp'])
 
-    #creating output directory
+    # Create output directory
     outputdir = get_arg(args, "outputdir", config["outputdir"])
     out_pdf = os.path.join(outputdir, 'pdf')
-    out_netcdf = os.path.join(outputdir, 'netcdf')
-    create_folder(out_pdf, loglevel )
-    create_folder(out_netcdf, loglevel)
+    create_folder(out_pdf, loglevel)
 
-    variables = config['diagnostic_attributes'].get('variables', [])
-
-    names = OutputSaver(diagnostic='radiation', model= models_list[0], exp = exp_list[0], loglevel=loglevel)
-    
+    # Output naming and saving
+    names = OutputSaver(diagnostic='radiation', model=models_list[0], exp=exp_list[0], loglevel=loglevel)
     logger.info("Boxplot generation")
     radiation = Radiation()
     result = radiation.boxplot(datasets=datasets, model_names=models_list, variables=variables)
-    if result is not None:
+
+    if result:
         fig, ax = result  
-        names.generate_name(diagnostic_product='boxplot')
-        names.save_pdf(fig, path=out_pdf)
+       
