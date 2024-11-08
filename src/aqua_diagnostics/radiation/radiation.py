68e50da21118700cbce0f927d0d2287cd59751aa
silviacaprioli <silvietta.caprioli@gmail.com>
2024-11-08 13:24:49 +0200

    better labels for boxplots

--- src/aqua_diagnostics/radiation/radiation.py
+++ src/aqua_diagnostics/radiation/radiation.py
@@ -9,7 +9,7 @@ xr.set_options(keep_attrs=True)
 
 class Radiation:
 
-    def boxplot(self, datasets=None, model_names=None, variables=None):
+    def boxplot(self, datasets=None, model_names=None, variables=None, variable_names=None):
         """
         Generate a boxplot showing the uncertainty of a global variable for different models.
 
@@ -17,6 +17,7 @@ class Radiation:
             datasets (list of xarray.Dataset): A list of xarray Datasets to be plotted.
             model_names (list of str): Names for the plotting, corresponding to the datasets.
             variables (list of str): List of variables to be plotted.
+            variable_names (list of str): List of alternative names for the variables.
         """
 
         logger = log_configure(log_level='INFO', log_name='Boxplot')
@@ -28,13 +29,15 @@ class Radiation:
         # Initialize a dictionary to store data for the boxplot
         boxplot_data = {'Variables': [], 'Values': [], 'Datasets': []}
 
-        # Default model names if not provided
-        if model_names is None:
-            model_names = [f"Model {i+1}" for i in range(len(datasets))]
-
-        # Default variables if not provided
-        if variables is None:
-            variables = ['-mtnlwrf', 'mtnswrf']
+        label_map = {}
+        if variable_names is not None and len(variable_names) == len(variables):
+            for variable, name in zip(variables, variable_names):
+                var_name = variable[1:] if variable.startswith('-') else variable
+                label_map[variable] = name
+        else:
+            for variable in variables:
+                var_name = variable[1:] if variable.startswith('-') else variable
+                label_map[variable] = short_name_map.get(var_name, var_name)
 
         for dataset, model_name in zip(datasets, model_names):
             for variable in variables:
@@ -44,7 +47,7 @@ class Radiation:
                 values = gm[var_name].values.flatten()
                 if variable.startswith('-'):
                     values = -values
-                boxplot_data['Variables'].extend([variable] * len(values))
+                boxplot_data['Variables'].extend([label_map[variable]] * len(values))
                 boxplot_data['Values'].extend(values)
                 boxplot_data['Datasets'].extend([model_name] * len(values))
 
@@ -55,13 +58,12 @@ class Radiation:
         df = pd.DataFrame(boxplot_data)
         fig, ax = plt.subplots(figsize=(12, 8))
         sns.boxplot(data=df, x='Variables', y='Values', hue='Datasets', ax=ax)
-        ax.set_title('Global mean radiation for different models')
-        ax.set_xlabel('Variables')
-        ax.set_ylabel(f'Values [{units}]')
-        ax.tick_params(axis='both', labelsize=fontsize)
+        
+        ax.set_xlabel('Variables', fontsize=fontsize)
+        ax.set_ylabel(f'Values [{units}]', fontsize=fontsize + 2) 
+        ax.tick_params(axis='x', labelsize=fontsize - 4)  
+        ax.tick_params(axis='y', labelsize=fontsize)  
         ax.legend(loc='upper right', fontsize=fontsize)
         ax.grid(True)
 
-        return fig, ax  
-
-
+        return fig, ax
