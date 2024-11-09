edde6818ad4db839a736612469026b1197f211ec
maqsoodrajput <24833949+maqsoodrajput@users.noreply.github.com>
2024-11-09 22:45:23 +0100

    updating the ensemble module

--- diagnostics/ensemble/__init__.py
+++
@@ -1,9 +0,0 @@
-# """Ensemble Module"""
-
-from .ensemble_timeseries import EnsembleTimeseries
-from .ensemble_latlon import EnsembleLatLon
-from .ensemble_zonal import EnsembleZonal
-
-
-
-__all__ = ["EnsembleTimeseries","EnsembleLatLon","EnsembleZonal"]
---
+++ diagnostics/ensemble/ensemble/__init__.py
@@ -0,0 +1,7 @@
+# """Ensemble Module"""
+
+from .ensemble_class import EnsembleTimeseries, EnsembleLatLon, EnsembleZonal
+
+__version__ = '0.12.3'
+
+__all__ = ["EnsembleTimeseries","EnsembleLatLon","EnsembleZonal"]
