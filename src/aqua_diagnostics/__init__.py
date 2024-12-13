31ccd646b7b37838a343f473efc93738ab4e8cd3
silviacaprioli <silvietta.caprioli@gmail.com>
2024-12-13 17:32:13 +0200

    drop diagnostic version

--- src/aqua_diagnostics/__init__.py
+++ src/aqua_diagnostics/__init__.py
@@ -2,8 +2,6 @@ from .timeseries import GregoryPlot, SeasonalCycle, Timeseries
 from .global_biases import GlobalBiases
 from .radiation import Radiation
 
-__version__ = "0.0.1"
-
 __all__ = ["GregoryPlot", "SeasonalCycle", "Timeseries", "GlobalBiases", "Radiation"]
 
 
