6796d34742f492cb9ac7cff4c5a1c8852ad9e829
Jost von Hardenberg <hardenberg@gmail.com>
2024-12-15 15:00:53 +0200

    fixit

--- src/aqua/__init__.py
+++ src/aqua/__init__.py
@@ -1,5 +1,5 @@
 """AQUA module"""
-from .version import __version__
+from .version import __version__ as version
 
 from .graphics import plot_single_map, plot_maps, plot_single_map_diff, plot_timeseries
 from .graphics import plot_hovmoller
@@ -7,6 +7,8 @@ from .lra_generator import LRAgenerator
 from .reader import Reader, catalog, Streaming, inspect_catalog
 from .slurm import squeue, job, output_dir, scancel, max_resources_per_node
 from .accessor import AquaAccessor
+
+__version__ = version
 
 __all__ = ["plot_single_map", "plot_maps", "plot_single_map_diff", "plot_timeseries",
            "plot_hovmoller",
