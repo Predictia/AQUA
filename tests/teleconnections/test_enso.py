01db7facc9d1183d147c3da3d17ede52aed85b32
Matteo Nurisso <m.nurisso@isac.cnr.it>
2024-12-17 17:13:55 +0100

    update enso test to use healpix data

--- tests/teleconnections/test_enso.py
+++ tests/teleconnections/test_enso.py
@@ -7,10 +7,11 @@ approx_rel = 1e-4
 loglevel = 'DEBUG'
 
 telecname = 'ENSO'
-model = 'IFS'
-exp = 'test-tco79'
-source = 'teleconnections'
-interface = 'teleconnections-ci'
+model = 'ERA5'
+exp = 'era5-hpz3'
+source = 'monthly'
+interface = 'teleconnections-destine'
+regrid = 'r100'
 
 
 @pytest.mark.teleconnections
@@ -20,39 +21,25 @@ def test_class_ENSO():
     """
     telec = Teleconnection(model=model, exp=exp, source=source,
                            loglevel=loglevel, telecname=telecname,
-                           interface=interface)
+                           interface=interface, regrid=regrid)
 
     telec.evaluate_index()
-    assert telec.index[4].values == pytest.approx(-1.08063012, rel=approx_rel)
-
-    reg = telec.evaluate_regression()
-    assert reg.isel(lon=4, lat=23).values == pytest.approx(-0.13339995,
-                                                           rel=approx_rel)
-
-    cor = telec.evaluate_correlation()
-    assert cor.isel(lon=4, lat=23).values == pytest.approx(-0.06538111,
-                                                           rel=approx_rel)
-
-    cor_msl = telec.evaluate_correlation(var='msl')
-    assert cor_msl.isel(lon=4, lat=23).values == pytest.approx(-0.18467891,
-                                                               rel=approx_rel)
-
-
-@pytest.mark.teleconnections
-def test_teleconnections_retrieve():
-    """Test exceptions in the Teleconnection class"""
-    telec = Teleconnection(model=model, exp=exp, source=source,
-                           loglevel=loglevel, telecname=telecname,
-                           interface=interface, regrid='r100')
+    assert telec.index[4].values == pytest.approx(-0.29945775, rel=approx_rel)
 
     with pytest.raises(NoDataError):
         telec.retrieve(var='pippo')
 
-    telec.retrieve()
+    reg = telec.evaluate_regression()
+    assert reg.isel(lon=4, lat=23).values == pytest.approx(0.01764006,
+                                                           rel=approx_rel)
 
-    assert telec.data[telec.var].isel(time=0,
-                                      lon=10, lat=10).values == pytest.approx(245.42805154,
-                                                                              rel=approx_rel)
+    cor = telec.evaluate_correlation()
+    assert cor.isel(lon=4, lat=23).values == pytest.approx(0.009964,
+                                                           rel=approx_rel)
+
+    cor_tprate = telec.evaluate_correlation(var='tprate')
+    assert cor_tprate.isel(lon=4, lat=23).values == pytest.approx(-0.04669953,
+                                                                  rel=approx_rel)
 
 
 @pytest.mark.teleconnections
