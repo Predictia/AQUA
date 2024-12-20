7440d9d7f27dc81eb8fba420bface320ff314915
oloapinivad <p.davini@isac.cnr.it>
2024-12-20 17:06:09 +0100

    full exposure of both class and function

--- src/aqua_diagnostics/__init__.py
+++ src/aqua_diagnostics/__init__.py
@@ -1,8 +1,8 @@
 from .teleconnections import Teleconnection
 from .timeseries import GregoryPlot, SeasonalCycle, Timeseries
-from .ecmean import PerformanceIndices, GlobalMean
+from .ecmean import PerformanceIndices, GlobalMean, performance_indices, global_mean
 
 __all__ = ["Teleconnection",
            "GregoryPlot", "SeasonalCycle", "Timeseries",
-           "GlobalMean", "PerformanceIndices"]
+           "GlobalMean", "PerformanceIndices", "performance_indices", "global_mean"]
 
