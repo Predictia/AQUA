fdb521191644fb651ed471f2a2cc8fca158b77f1
Paolo Davini <p.davini@isac.cnr.it>
2025-01-28 16:43:34 +0200

    force test

--- tests/test_gsv_timeutil.py
+++ tests/test_gsv_timeutil.py
@@ -18,7 +18,7 @@ def test_floor_datetime_all_cases():
         ('2025-03-15 12:45:00', 'YS', "20250101T0000"),  # Year Start
         ('2025-01-28 15:30:45', 'T',  "20250128T1530"),  # Minute
         ('2025-01-28 15:30:45', 'S',  "20250128T1530"),  # Second
-        ('2025-01-28 15:30:45', '2W',  KeyError),  # Week (start of the week)
+        ('2025-01-28 15:30:45', '2W', KeyError),  # Week (start of the week)
         ('2025-01-28 15:30:45', '3H', "20250128T1500"),  # Custom frequency: every 3 hours
         ('2025-01-28 15:30:45', '1D', "20250128T0000"),  # Custom frequency: daily
         ('2025-01-28 15:30:45', 'ME', KeyError),  # Invalid frequency should raise a KeyError
