5de366d9bb9c32879629088a6c8cbf2cb7454c2f
Paolo Davini <p.davini@isac.cnr.it>
2025-02-13 15:51:26 +0200

    tests and fixes

--- tests/seaice/test_seaice.py
+++ tests/seaice/test_seaice.py
@@ -2,13 +2,26 @@ import pytest
 import xarray as xr
 from aqua.diagnostics import SeaIce
 
-def test_seaice_concentration():
+# pytest approximation, to bear with different machines
+approx_rel = 1e-4
+loglevel = 'DEBUG'
 
-    seaice = SeaIce(model='ERA5', exp='era5-hpz3', source='monthly', regions=['Arctic'], regrid='r100')
-    extent = seaice.compute_seaice(method='extent', var='tprate', threshold=0)
+@pytest.mark.seaice
+class TestSeaIce():
+    """Test the SeaIce class."""
 
-    assert isinstance(extent, xr.Dataset)
-    assert list(extent.coords) == ['time']
-    assert list(extent.data_vars) == ['sea_ice_extent_arctic']
-    assert extent.attrs['units'] == 'million km^2'
-    assert pytest.approx(extent['sea_ice_extent_arctic'][5].values) == 247.5706
+    @pytest.mark.parametrize(('region,value'), 
+                             [('Arctic', 247.5706),
+                              ('Weddell Sea', 56.79711387)])
+    def test_seaice_concentration(self, region, value):
+
+        seaice = SeaIce(model='ERA5', exp='era5-hpz3', source='monthly', regions=region, regrid='r100',
+                        loglevel=loglevel)
+        extent = seaice.compute_seaice(method='extent', var='tprate', threshold=0)
+        regionlower = region.lower().replace(" ", "_")
+
+        assert isinstance(extent, xr.Dataset)
+        assert list(extent.coords) == ['time']
+        assert list(extent.data_vars) == [f'sea_ice_extent_{regionlower}']
+        assert extent.attrs['units'] == 'million km^2'
+        assert extent[f'sea_ice_extent_{regionlower}'][5].values == pytest.approx(value, rel=approx_rel)
