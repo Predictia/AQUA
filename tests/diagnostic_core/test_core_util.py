b5d9fb5acdc6ec87d9ce1b4c92c555a7f7b99e35
Matteo Nurisso <m.nurisso@isac.cnr.it>
2025-02-14 15:53:05 +0100

    use mock to avoid dask in test

--- tests/diagnostic_core/test_core_util.py
+++ tests/diagnostic_core/test_core_util.py
@@ -1,5 +1,6 @@
 import pytest
 import argparse
+from unittest.mock import patch
 from aqua.diagnostics.core import template_parse_arguments, open_cluster, close_cluster
 
 loglevel = 'DEBUG'
@@ -21,25 +22,26 @@ def test_template_parse_arguments():
 
 
 @pytest.mark.aqua
-def test_cluster():
-    """Test the cluster functions"""
-    client, cluster, private_cluster = open_cluster(None, None, loglevel)
+@patch("aqua.diagnostics.core.util.Client")
+@patch("aqua.diagnostics.core.util.LocalCluster")
+def test_cluster(mock_cluster, mock_client):
+    """Test the cluster functions with mocking"""
 
+    # Test case 1: No workers specified
+    client, cluster, private_cluster = open_cluster(None, None, loglevel)
     assert client is None
     assert cluster is None
     assert private_cluster is False
 
+    # # Test case 2: New cluster creation
     client, cluster, private_cluster = open_cluster(2, None, loglevel)
-
     assert client is not None
     assert cluster is not None
     assert private_cluster is True
 
-    # Use the cluster generated in the previous test as an input to the next test
-
-    previous_cluster = cluster
-    client, cluster, private_cluster = open_cluster(2, cluster, loglevel)
-
+    # Test case 3: Using existing cluster
+    previous_cluster = mock_cluster
+    client, cluster, private_cluster = open_cluster(2, previous_cluster, loglevel)
     assert client is not None
     assert cluster is previous_cluster
     assert private_cluster is False
