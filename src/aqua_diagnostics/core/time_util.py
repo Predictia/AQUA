724f90c54caecd7970c57d66256955f3c5a2c4da
mnurisso <m.nurisso@isac.cnr.it>
2025-02-18 17:04:18 +0200

    better docstring

--- src/aqua_diagnostics/core/time_util.py
+++ src/aqua_diagnostics/core/time_util.py
@@ -6,12 +6,13 @@ def start_end_dates(startdate=None, enddate=None,
     """
     Evaluate start and end dates for the reference data retrieve,
     in the case both are provided, to minimize the Reader calls.
+    They should be all of the form 'YYYY-MM-DD' or all of the form 'YYYYMMDD'.
 
     Args:
-        startdate (datetime): start date for the data retrieve
-        enddate (datetime): end date for the data retrieve
-        start_std (datetime): start date for the standard deviation data retrieve
-        end_std (datetime): end date for the standard deviation data retrieve
+        startdate (str): start date for the data retrieve
+        enddate (str): end date for the data retrieve
+        start_std (str): start date for the standard deviation data retrieve
+        end_std (str): end date for the standard deviation data retrieve
 
     Returns:
         tuple: start and end dates for the data retrieve
