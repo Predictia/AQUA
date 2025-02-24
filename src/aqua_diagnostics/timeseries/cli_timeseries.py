cf74d4c1e55c1e73ba1311a9f9d93676818672d1
mnurisso <m.nurisso@isac.cnr.it>
2025-02-25 00:05:37 +0200

    Update timeseries CLI to use new argument parsing and load diagnostic configuration

--- src/aqua_diagnostics/timeseries/cli_timeseries.py
+++ src/aqua_diagnostics/timeseries/cli_timeseries.py
@@ -11,10 +11,12 @@ import argparse
 import sys
 
 from aqua.logger import log_configure
-from aqua.util import get_arg
+from aqua.util import get_arg, load_yaml
 from aqua.version import __version__ as aqua_version
-from aqua.diagnostics.core import template_parser, open_cluster, close_cluster
+from aqua.diagnostics.core import template_parse_arguments, open_cluster, close_cluster
 from aqua.diagnostics.timeseries import Timeseries
+# TODO: update import when #1750 is merged
+from aqua.diagnostics.core.util import load_diagnostic_config
 
 def parse_arguments(args):
     """
@@ -24,11 +26,11 @@ def parse_arguments(args):
         args (list): list of command-line arguments to parse.
     """
     parser = argparse.ArgumentParser(description='Timeseries CLI')
-    parser = template_parser(parser)
+    parser = template_parse_arguments(parser)
     return parser.parse_args(args)
 
 if __name__ == '__main__':
-    args = parse_arguments(sys.args[1:])
+    args = parse_arguments(sys.argv[1:])
 
     loglevel = get_arg(args, 'loglevel', 'WARNING')
     logger = log_configure(log_level=loglevel, log_name='Timeseries CLI')
@@ -38,6 +40,12 @@ if __name__ == '__main__':
     nworkers = get_arg(args, 'nworkers', None)
 
     client, cluster, private_cluster, = open_cluster(nworkers=nworkers, cluster=cluster, loglevel=loglevel)
+
+    config_dict = load_diagnostic_config(diagnostic='timeseries', args=args,
+                                         default_config='config_timeseries_atm.yaml',
+                                         loglevel=loglevel)
+    
+    logger.debug(f"Timeseries diagnostic configuration: {config_dict}")
 
     close_cluster(client=client, cluster=cluster, private_cluster=private_cluster, loglevel=loglevel)
 
