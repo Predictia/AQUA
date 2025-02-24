ae2487ab7461e538a26ad8df3fb784a5e19441d6
mcadau <marco.cadau@polito.it>
2025-02-24 11:16:39 +0200

    Adaptation of the new aqua style to timeseries (and further aesthetic adjustments to timseries plots)

--- src/aqua/graphics/timeseries.py
+++ src/aqua/graphics/timeseries.py
@@ -5,6 +5,7 @@ both with monthly and annual aggregation options
 import xarray as xr
 import matplotlib.pyplot as plt
 from aqua.logger import log_configure
+from .styles import ConfigStyle
 
 
 def plot_timeseries(monthly_data=None,
@@ -15,6 +16,7 @@ def plot_timeseries(monthly_data=None,
                     std_annual_data=None,
                     data_labels: list = None,
                     ref_label: str = None,
+                    style: str = None,
                     loglevel: str = 'WARNING',
                     **kwargs):
     """
@@ -31,6 +33,7 @@ def plot_timeseries(monthly_data=None,
         std_annual_data (xr.DataArray): standard deviation of the reference annual data
         data_labels (list of str): labels for the data
         ref_label (str): label for the reference data
+        style (str): style to use for the plot. By default the schema specified in the configuration file is used.
         loglevel (str): logging level
 
     Keyword Arguments:
@@ -41,6 +44,8 @@ def plot_timeseries(monthly_data=None,
         fig, ax (tuple): tuple containing the figure and axis objects
     """
     logger = log_configure(loglevel, 'PlotTimeseries')
+    ConfigStyle(style=style, loglevel=loglevel)
+
     fig_size = kwargs.get('figsize', (10, 5))
     fig, ax = plt.subplots(1, 1, figsize=fig_size)
 
@@ -91,6 +96,7 @@ def plot_timeseries(monthly_data=None,
                                 ref_monthly_data - 2.*std_monthly_data.sel(month=ref_monthly_data["time.month"]),
                                 ref_monthly_data + 2.*std_monthly_data.sel(month=ref_monthly_data["time.month"]),
                                 facecolor='grey', alpha=0.25)
+                ax.set(xlim=(ref_monthly_data.time[0], ref_monthly_data.time[-1]))
         except Exception as e:
             logger.debug(f"Error plotting monthly std data: {e}")
 
@@ -111,13 +117,11 @@ def plot_timeseries(monthly_data=None,
             logger.debug(f"Error plotting annual std data: {e}")
 
     ax.legend(fontsize='small')
-    ax.grid(axis="x", color="k")
-    ax.spines["right"].set_visible(False)
-    ax.spines["top"].set_visible(False)
+    ax.grid(True, axis="both")
 
     title = kwargs.get('title', None)
     if title:
-        ax.set_title(title)
+        ax.set_title(title, fontsize=13, fontweight='bold')
 
     return fig, ax
 
@@ -127,6 +131,7 @@ def plot_seasonalcycle(data=None,
                        std_data=None,
                        data_labels: list = None,
                        ref_label: str = None,
+                       style: str = None,
                        grid=True,
                        loglevel: str = 'WARNING',
                        **kwargs):
@@ -139,6 +144,7 @@ def plot_seasonalcycle(data=None,
         std_data (xr.DataArray): standard deviation of the reference data
         data_labels (list of str): labels for the data
         ref_label (str): label for the reference data
+        style (str): style to use for the plot. By default the schema specified in the configuration file is used.
         grid (bool): if True, plot grid
         loglevel (str): logging level
 
@@ -149,7 +155,10 @@ def plot_seasonalcycle(data=None,
     Returns:
         fig, ax (tuple): tuple containing the figure and axis objects
     """
+
     logger = log_configure(loglevel, 'PlotSeasonalCycle')
+    ConfigStyle(style=style, loglevel=loglevel)
+
     fig_size = kwargs.get('figsize', (6, 4))
     fig, ax = plt.subplots(1, 1, figsize=fig_size)
 
@@ -172,6 +181,7 @@ def plot_seasonalcycle(data=None,
             try:
                 mon_data = _extend_cycle(data[i], loglevel)
                 mon_data.plot(ax=ax, label=label, color=color, lw=3)
+                ax.set(xlim=(data.time[0], data.time[-1]))
             except Exception as e:
                 logger.debug(f"Error plotting data: {e}")
 
@@ -196,11 +206,11 @@ def plot_seasonalcycle(data=None,
     ax.set_axisbelow(True)
 
     if grid:
-        ax.grid()
+        ax.grid(axis="both", color="dimgrey", linestyle="--", alpha=0.8)
 
     title = kwargs.get('title', None)
     if title is not None:
-        ax.set_title(title)
+        ax.set_title(title, fontsize=13, fontweight='bold')
 
     return fig, ax
 
