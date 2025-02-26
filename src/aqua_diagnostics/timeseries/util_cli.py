161825af2a3d24f0a072538b664ab92ce1d76326
mnurisso <m.nurisso@isac.cnr.it>
2025-02-26 18:37:51 +0200

    add SeasonalCyclesCLI class and implement seasonal cycles diagnostics functionality. load_var_config fix

--- src/aqua_diagnostics/timeseries/util_cli.py
+++ src/aqua_diagnostics/timeseries/util_cli.py
@@ -1,5 +1,5 @@
 from aqua.logger import log_configure
-from aqua.diagnostics.timeseries import Timeseries
+from aqua.diagnostics.timeseries import Timeseries, SeasonalCycles
 
 
 class MixinCLI():
@@ -22,47 +22,109 @@ class MixinCLI():
         # We need the lengths to have a list of results with the correct length
         self.len_datasets = len(self.config_dict['datasets'])
         self.len_references = len(self.config_dict['references'])
+        self.diag_datasets = []
+        self.diag_references = []
         self.var = var
         self.formula = formula
 
 class TimeseriesCLI(MixinCLI):
-    
+    """
+    Class that helps to run the timeseries diagnostics for a given variable and configuration.
+    It runs many Timeseries instances, one for each dataset and reference.
+    """
     def run(self, regrid: str = None, std_startdate: str = None, std_enddate: str = None,
             region: str = None, long_name: str = None, units: str = None,
             standard_name: str = None, freq: list = ['monthly', 'annual'],
             outputdir: str = './', rebuild: bool = True):
         """
-        Run the timeseries diagnostics for the given variable and configuration."
+        Run the timeseries diagnostics for the given variable and configuration.
+
+        Args:
+            regrid (str): The regridding resolution. Default is None.
+            std_startdate (str): The start date for the standardization. Default is None.
+            std_enddate (str): The end date for the standardization. Default is None.
+            region (str): The region to run the diagnostics for. Default is None.
+            long_name (str): The long name of the variable, if different from the variable name.
+            units (str): The units of the variable, if different from the original units.
+            standard_name (str): The standard name of the variable, if different from the variable name.
+            freq (list): The frequency of the diagnostics. Default is ['monthly', 'annual'].
+            outputdir (str): The output directory. Default is './'.
+            rebuild (bool): If True, rebuild the data.
         """
         init_args = {'region': region, 'loglevel': self.loglevel}
         run_args = {'var': self.var, 'formula': self.formula, 'long_name': long_name,
                     'units': units, 'standard_name': standard_name, 'freq': freq,
                     'outputdir': outputdir, 'rebuild': rebuild}
+
         # Run the datasets
-        for dataset in self.config_dict['datasets']:
+        for i, dataset in enumerate(self.config_dict['datasets']):
             self.logger.debug(f'Running dataset: {dataset}, variable: {self.var}')
             dataset_args = {'catalog': dataset['catalog'], 'model': dataset['model'],
-                            'exp': dataset['exp'], 'source': dataset['source'],
-                            'regrid': dataset.get('regrid', regrid)}
+                    'exp': dataset['exp'], 'source': dataset['source'],
+                    'regrid': dataset.get('regrid', regrid)}
             timeseries = Timeseries(**dataset_args, **init_args)
-            timeseries.run(**run_args)
+            self.diag_datasets.append(timeseries)
+            self.diag_datasets[i].run(**run_args)
         
         # Run the references
-        for reference in self.config_dict['references']:
+        for i, reference in enumerate(self.config_dict['references']):
             self.logger.debug(f'Running reference: {reference}, variable: {self.var}')
             reference_args = {'catalog': reference['catalog'], 'model': reference['model'],
                               'exp': reference['exp'], 'source': reference['source'],
                               'regrid': reference.get('regrid', regrid),
                               'std_startdate': std_startdate, 'std_enddate': std_enddate}
             timeseries = Timeseries(**reference_args, **init_args)
-            timeseries.run(std=True, **run_args)
-
+            self.diag_references.append(timeseries)
+            self.diag_references[i].run(**run_args)
 
 
 class SeasonalCyclesCLI(MixinCLI):
+    """
+    Class that helps to run the seasonal cycles diagnostics for a given variable and configuration.
+    It runs many SeasonalCycles instances, one for each dataset and reference.
+    """
+    def run(self, regrid: str = None, std_startdate: str = None, std_enddate: str = None,
+            region: str = None, long_name: str = None, units: str = None,
+            standard_name: str = None, outputdir: str = './', rebuild: bool = True):
+        """
+        Run the seasonal cycles diagnostics for the given variable and configuration.
 
-    def run(self):
-        pass
+        Args:ß
+            regrid (str): The regridding resolution. Default is None.
+            std_startdate (str): The start date for the standardization. Default is None.
+            std_enddate (str): The end date for the standardization. Default is None.
+            region (str): The region to run the diagnostics for. Default is None.
+            long_name (str): The long name of the variable, if different from the variable name.
+            units (str): The units of the variable, if different from the original units.
+            standard_name (str): The standard name of the variable, if different from the variable name.
+            outputdir (str): The output directory. Default is './'.
+            rebuild (bool): If True, rebuild the data.
+        """
+        init_args = {'region': region, 'loglevel': self.loglevel}
+        run_args = {'var': self.var, 'formula': self.formula, 'long_name': long_name,
+                    'units': units, 'standard_name': standard_name,
+                    'outputdir': outputdir, 'rebuild': rebuild}
+
+        # Run the datasets
+        for i, dataset in enumerate(self.config_dict['datasets']):
+            self.logger.debug(f'Running dataset: {dataset}, variable: {self.var}')
+            dataset_args = {'catalog': dataset['catalog'], 'model': dataset['model'],
+                            'exp': dataset['exp'], 'source': dataset['source'],
+                            'regrid': dataset.get('regrid', regrid)}
+            seasonal_cycles = SeasonalCycles(**dataset_args, **init_args)
+            self.diag_datasets.append(seasonal_cycles)
+            self.diag_datasets[i].run(**run_args)
+        
+        # Run the references
+        for i, reference in enumerate(self.config_dict['references']):
+            self.logger.debug(f'Running reference: {reference}, variable: {self.var}')
+            reference_args = {'catalog': reference['catalog'], 'model': reference['model'],
+                              'exp': reference['exp'], 'source': reference['source'],
+                              'regrid': reference.get('regrid', regrid),
+                              'std_startdate': std_startdate, 'std_enddate': std_enddate}
+            seasonal_cycles = SeasonalCycles(**reference_args, **init_args)
+            self.diag_references.append(seasonal_cycles)
+            self.diag_references[i].run(**run_args)
 
 
 def load_var_config(config_dict: dict, var: str):
@@ -100,7 +162,7 @@ def load_var_config(config_dict: dict, var: str):
     # Get the regions
     regions = [None]
     if var_config.get('regions') is not None:
-        regions.append(region for region in regions if region is not None)
+        regions.extend([region for region in var_config['regions'] if region is not None])
         del var_config['regions']
 
     return var_config, regions
