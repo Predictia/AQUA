b6df389e051e77509a129563bdb85487a985f8e4
Paolo Davini <p.davini@isac.cnr.it>
2025-02-28 11:30:27 +0100

    refactor TurboRegrid class to improve grid path handling and integrate smmregrid for weight generation

--- src/aqua/reader/turboregrid.py
+++ src/aqua/reader/turboregrid.py
@@ -1,11 +1,11 @@
 """New Regrid class independent from the Reader"""
 import os
 import xarray as xr
+from smmregrid import CdoGenerate, Regridder, GridInspector
 from aqua.logger import log_configure
-from aqua.util import to_list
-from smmregrid import GridType, GridInspector, CdoGenerate, Regridder
 
 class TurboRegrid():
+    """Refactor of regridder class"""
     
     def __init__(self, cfg_grid_dict: dict,
                  src_grid_name: str,
@@ -32,43 +32,66 @@ class TurboRegrid():
         # check that a path exists
         self.src_grid_path = self.src_grid_dict.get('path', None)
         if not self.src_grid_path:
-            self.logger.warning("Grid file path not found in the configuration. Need a sample data.")
-            return
+            if data is not None:
+                #gridtype = GridInspector(data).get_grid_info()[0]
+                self.src_grid_path = {'2d': data}
+                # this has to be expanded. It should create a dictionary for the src_grid_path. 
+            else:
+                self.logger.warning("Grid file path not found in the configuration. Need sample data.")
+                return
+
+        # convert to dictionary if it is a single path. Assume is a 2d grid.
+        if not isinstance(self.src_grid_path, dict):
+            self.src_grid_path = {'2d': self.src_grid_path}
         
         # verify that all provided paths are valid
         if isinstance(self.src_grid_path, dict):
             for key in self.src_grid_path:
                 if not os.path.exists(self.src_grid_path[key]):
                     raise FileNotFoundError(f"Grid file {self.src_grid_path} does not exist.")
-        elif not os.path.exists(self.src_grid_path):
-            raise FileNotFoundError(f"Grid file {self.src_grid_path} does not exist.")
+    
+        self.logger.info("Grid name: %s", self.src_grid_name)
+        self.logger.info("Grid file path: %s", self.src_grid_path)
         
     def load_generate_weights(self, dst_grid_name, rebuild=False, reader_kwargs=None, **kwargs):
         """
-        Load or generate regridding weights.
+        Load or generate regridding weights calling smmregrid
         """
-        #self._make_filename(dst_grid_name, self.grid_name, model, exp, source, **reader_kwargs)     
-        for v in self.src_grid_path:
 
-            weights_filename = f'tmp_{v}.nc'
+        # get the cdo options from the configuration 
+        cdo_extra = self.src_grid_dict.get('cdo_extra', None)
+        cdo_options = self.src_grid_dict.get('cdo_options', None)
+
+        # loop over the vertical coordinates: 2d, 2dm, or any other
+        for vertical_dim in self.src_grid_path:
+
+            weights_filename = f'tmp_{vertical_dim}.nc'
+            #weights_filename = self._make_filename(dst_grid_name, self.grid_name, **reader_kwargs)
+            #keep fallback option for filename when weights/areas block is not present in the configuration
+
+            # check if weights already exist, if not, generate them
             if rebuild or os.path.exists(weights_filename):
                 weights = xr.open_dataset(weights_filename)
-                
-            generator = CdoGenerate(source_grid=self.src_grid_path[v],
+
+            # smmregrid call
+            generator = CdoGenerate(source_grid=self.src_grid_path[vertical_dim],
                             target_grid=self.cfg_grid_dict['grids'][dst_grid_name],
                             cdo_download_path=None,
                             cdo_icon_grids=None,
-                            cdo_extra=None,
-                            cdo_options=None,
+                            cdo_extra=cdo_extra,
+                            cdo_options=cdo_options,
                             cdo='cdo',
                             loglevel=self.loglevel)
-            if v in ['2d', '2dm']:
-                weights = generator.weights(method="con", nproc=1)
-            else:
-                weights = generator.weights(method="con", vert_coord=v, nproc=1)
+            
+            # define the vertical coordinate in the smmregrid world
+            smm_vert_coord = None if vertical_dim in ['2d', '2dm'] else vertical_dim
+
+            # generate and save the weights
+            weights = generator.weights(method="con", vert_coord=smm_vert_coord, nproc=1)
             weights.to_netcdf(weights_filename)
 
-            self.regridder[v] = Regridder(weights=weights)
+            # initialize the regridder
+            self.regridder[vertical_dim] = Regridder(weights=weights)
         
     # def _configure_gridtype(self, data=None):
     #     """
