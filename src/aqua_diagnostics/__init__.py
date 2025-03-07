1bc1d45738b0d8893142076f9d00433d1bac2c10
silviacaprioli <silvietta.caprioli@gmail.com>
2025-03-07 21:08:04 +0200

    Add PlotGlobalBiases to global_biases module and update __all__ exports

--- src/aqua_diagnostics/__init__.py
+++ src/aqua_diagnostics/__init__.py
@@ -1,12 +1,12 @@
 from .teleconnections import Teleconnection
 from .timeseries import GregoryPlot, SeasonalCycle, Timeseries
-from .global_biases import GlobalBiases
+from .global_biases import GlobalBiases, PlotGlobalBiases 
 from .radiation import Radiation
 from .ensemble import EnsembleTimeseries, EnsembleLatLon, EnsembleZonal
 from .ecmean import PerformanceIndices, GlobalMean
 __all__ = ["Teleconnection",
            "GregoryPlot", "SeasonalCycle", "Timeseries", 
-           "GlobalBiases",
+           "GlobalBiases", "PlotGlobalBiases",
            "Radiation", 
            "EnsembleTimeseries", "EnsembleLatLon", "EnsembleZonal",
            "GlobalMean", "PerformanceIndices"]
--- src/aqua_diagnostics/global_biases/__init__.py
+++ src/aqua_diagnostics/global_biases/__init__.py
@@ -1,3 +1,5 @@
 from .global_biases import GlobalBiases
+from .plot_global_biases import PlotGlobalBiases
 
-__all__ = ['GlobalBiases']
+__all__ = ['GlobalBiases', 'PlotGlobalBiases']
+
