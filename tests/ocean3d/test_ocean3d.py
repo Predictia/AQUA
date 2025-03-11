d6c1f1e72395d4f076e9b44418b5e41e23578a9d
ghosh97 <supriyo.ghosh@bsc.es>
2025-03-11 21:09:32 +0100

    time series tests

--- tests/ocean3d/test_ocean3d.py
+++ tests/ocean3d/test_ocean3d.py
@@ -17,14 +17,13 @@ from pathlib import Path
 @pytest.fixture
 def common_setup(tmp_path):
     """Fixture to set up common configuration and test data."""
-    Configurer = ConfigPath()
     loglevel = 'warning'
     catalog = 'ci'
     exp = 'hpz3'
     model = 'FESOM'
     source = 'monthly-3d'
     region = 'Indian Ocean'
-    output_dir = "./tmp"
+    output_dir = tmp_path
     output = True
     reader = Reader(model=model, exp=exp, source= source, catalog= catalog, regrid='r100')
     data = reader.retrieve(var=["thetao", "so"])
@@ -32,52 +31,68 @@ def common_setup(tmp_path):
 
     return {
         "loglevel": loglevel,
-        "outputdir": tmp_path,
         "model": model,
         "exp": exp,
         "source": source,
         "data": data,
         "region": region,
-        "output_dir" : "./tmp",
-        "output" : True
+        "output_dir" : output_dir,
+        "output" : output
     }
 
-@pytest.mark.diagnostics
-def test_check_variable_name(common_setup):
-    """Test variable name checking and transformations."""
-    setup = common_setup
-    data = check_variable_name(setup["data"], loglevel=setup["loglevel"])
+# @pytest.mark.diagnostics
+# def test_check_variable_name(common_setup):
+#     """Test variable name checking and transformations."""
+#     setup = common_setup
+#     data = check_variable_name(setup["data"], loglevel=setup["loglevel"])
+#     # Ensure required variables exist
+#     assert 'so' in data, "Variable 'so' not found in dataset"
+#     assert 'thetao' in data, "Variable 'thetao' not found in dataset"
+#     assert data['thetao'].attrs.get('units') == 'degC', "Units not converted to Celsius"
+#     assert 'lev' in data.dims, "Dimension 'lev' missing in dataset"
 
-    # Ensure required variables exist
-    assert 'so' in data, "Variable 'so' not found in dataset"
-    assert 'thetao' in data, "Variable 'thetao' not found in dataset"
-
-    # Ensure units are converted correctly
-    assert data['thetao'].attrs.get('units') == 'degC', "Units not converted to Celsius"
-
-    # Ensure expected dimensions exist
-    assert 'lev' in data.dims, "Dimension 'lev' missing in dataset"
-
-
+# # Hovmoller Function
+# @pytest.fixture
+# def hovmoller_instance(common_setup):
+#     """Fixture to initialize hovmoller plot instance"""
+#     setup = common_setup
+#     setup["data"] = check_variable_name(setup["data"], loglevel=setup["loglevel"])
+#     hovmoller_plot_init = hovmoller_plot(setup)
+#     return hovmoller_plot_init
+    
+# @pytest.mark.diagnostics
+# def test_hovmoller_data(hovmoller_instance):
+#     """Test data loading for hovmoller plot."""
+#     hovmoller_instance.data_for_hovmoller_lev_time_plot()
+#     for i in range(1, len(hovmoller_instance.plot_info) + 1):
+#         assert hovmoller_instance.plot_info[i]["data"] is not None, f"Data not loaded for hovmoller plot hovmoller_instance.plot_info[1]['type']"
+    
+# @pytest.mark.diagnostics
+# def test_hovmoller_plot(hovmoller_instance):
+#     """Test hovmoller plot generation."""
+#     hovmoller_instance.plot()
+    # output_file = Path(f"{time_series_instance.output_dir}/pdf/{time_series_instance.model}-{time_series_instance.exp}-{time_series_instance.source}_hovmoller_plots_indian_ocean.pdf")
+#     assert output_file.exists(), "Plot not generated"
+    
+#Time_series Function
 @pytest.fixture
-def hovmoller_instance(common_setup):
-    """Fixture to initialize hovmoller plot instance"""
+def time_series_instance(common_setup):
+    """Fixture to initialize time series plot instance"""
     setup = common_setup
     setup["data"] = check_variable_name(setup["data"], loglevel=setup["loglevel"])
-    hovmoller_plot_init = hovmoller_plot(setup)
-    return hovmoller_plot_init
+    time_series_init = time_series(setup)
+    return time_series_init
+
+@pytest.mark.diagnostics
+def test_time_series(time_series_instance):
+    """Test data loading for time series plot."""
+    time_series_instance.data_for_hovmoller_lev_time_plot()
+    for i in range(1, len(time_series_instance.plot_info) + 1):
+        assert time_series_instance.plot_info[i]["data"] is not None, f"Data not loaded for hovmoller plot hovmoller_instance.plot_info[1]['type']"
     
 @pytest.mark.diagnostics
-def test_hovmoller_data(hovmoller_instance):
-    """Test data loading for hovmoller plot."""
-    hovmoller_instance.data_for_hovmoller_lev_time_plot()
-    for i in range(1, len(hovmoller_instance.plot_info) + 1):
-        assert hovmoller_instance.plot_info[i]["data"] is not None, f"Data not loaded for hovmoller plot hovmoller_instance.plot_info[1]['type']"
-    print("Test passed successfully!")
-    
-@pytest.mark.diagnostics
-def test_hovmoller_plot(hovmoller_instance):
-    """Test hovmoller plot generation."""
-    hovmoller_instance.plot()
-    output_file = Path(f"{hovmoller_instance.output_dir}/pdf/FESOM-hpz3-monthly-3d_hovmoller_plot_indian_ocean.pdf")
+def test_time_series(time_series_instance):
+    """Test time series plot generation."""
+    time_series_instance.plot()
+    output_file = Path(f"{time_series_instance.output_dir}/pdf/{time_series_instance.model}-{time_series_instance.exp}-{time_series_instance.source}_time_series_indian_ocean.pdf")
     assert output_file.exists(), "Plot not generated"
