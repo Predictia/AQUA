415ebf72d948e12609a1b4b96f344d30b7eaf90a
Matteo Nurisso <m.nurisso@isac.cnr.it>
2025-03-13 15:55:35 +0100

    test for paths in config file

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 
 from aqua.util import ConfigPath
@@ -8,8 +9,19 @@ def test_config_plain():
     assert config.filename == 'config-aqua.yaml'
     assert config.catalog == 'ci'
 
-# @pytest.mark.aqua
-# def test_config_ci():
-#     config = ConfigPath(configdir='./config', catalog='ci')
-#     assert config.filename == 'config-aqua.yaml'
-#     assert config.catalog == 'ci'
+@pytest.mark.aqua
+def test_config_paths():
+    configfile = 'tests/config/config-aqua-custom.yaml'
+
+    configdir = ConfigPath().get_config_dir()
+
+    # Copy the file to the config directory
+    os.system(f'cp {configfile} {configdir}')
+
+    config = ConfigPath(catalog='ci', filename='config-aqua-custom.yaml', configdir=configdir)
+    paths, _ = config.get_machine_info()
+
+    assert paths['paths']['grids'] == 'pluto'
+
+    # Remove the copied file
+    os.system(f'rm {configdir}/config-aqua-custom.yaml')
