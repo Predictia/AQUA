55ae343cd69636ac764879db40d763da72402b33
Matteo Nurisso <m.nurisso@isac.cnr.it>
2025-03-18 18:56:16 +0100

    working tests

--- tests/test_configstyle.py
+++ tests/test_configstyle.py
@@ -1,22 +1,29 @@
-import os
-import matplotlib.pyplot as plt
 import pytest
 from aqua.graphics import ConfigStyle
 
-@pytest.mark.configstyle
-def test_ConfigStyle():
-    """
-    Test that ConfigStyle class is correctly initialized
-    """
-    style = 'aqua.mplstyle'
+@pytest.mark.graphics
+class TestConfigStyle:
 
-    cs = ConfigStyle(style=style)
-    assert cs.style == style
+    def test_ConfigStyle_maptplotlib(self):
+        """
+        Test that ConfigStyle class is correctly initialized
+        with a matplotlib style
+        """
+        style = 'ggplot'
 
-@pytest.mark.configstyle_default
-def test_ConfigStyle_default():
-    """
-    Test that ConfigStyle class is correctly initialized with default style "
-    """
-    cs = ConfigStyle()
-    assert cs.style is not None
+        cs = ConfigStyle(style=style)
+        assert cs.style == style
+
+    def test_ConfigStyle_default(self):
+        """
+        Test that ConfigStyle class is correctly initialized with default style
+        """
+        cs = ConfigStyle()
+        assert cs.style == 'aqua'
+
+    def test_wrong_style(self):
+        """
+        Test that ConfigStyle class is correctly initialized with wrong style
+        """
+        with pytest.raises(OSError):
+            ConfigStyle(style='wrong_style')
