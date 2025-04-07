08e4d535c45adbfce5b138df6cb09ad9e5047f35
Paolo Davini <p.davini@isac.cnr.it>
2025-04-07 13:14:59 +0300

    adding back

--- src/aqua/util/coord.py
+++ src/aqua/util/coord.py
@@ -1,8 +1,8 @@
 """Utilities to flip latitude coordinates if needed. DEPRECATED. """
 
 # # List of latitude unit strings (the same used by cf2cdm)
-# latitude_units = ["degrees_north", "degree_north", "degree_N", "degrees_N", "degreeN", "degreesN"]
-# time_coords = ["time"]
+latitude_units = ["degrees_north", "degree_north", "degree_N", "degrees_N", "degreeN", "degreesN"]
+time_coords = ["time"]
 
 # def find_lat_dir(dataset):
 #     """
@@ -62,35 +62,35 @@
 #             dataset.coords[coord].attrs["flipped"] = old_dir
 
 
-# def flip_lat_dir(dataset):
-#     """
-#     Finds a latitude coordinate and flips it back if it has a 'flipped' attribute.
+def flip_lat_dir(dataset):
+    """
+    Finds a latitude coordinate and flips it back if it has a 'flipped' attribute.
 
-#     Args:
-#         dataset (xarray.Dataset): Dataset to check
+    Args:
+        dataset (xarray.Dataset): Dataset to check
 
-#     Return:
-#         None
-#     """
+    Return:
+        None
+    """
 
-#     # Iterate through coordinates to find the latitude coordinate
-#     latitude_coord = None
-#     for coord_name in dataset.coords:
-#         un = dataset.coords[coord_name].attrs.get('units')
-#         if un in latitude_units:
-#             latitude_coord = coord_name
-#             break
+    # Iterate through coordinates to find the latitude coordinate
+    latitude_coord = None
+    for coord_name in dataset.coords:
+        un = dataset.coords[coord_name].attrs.get('units')
+        if un in latitude_units:
+            latitude_coord = coord_name
+            break
 
-#     # If a latitude coordinate is found, determine if it has been flipped
-#     # in that case, reverse it back
-#     new_dataset = dataset
-#     if latitude_coord is not None:
-#         if dataset.coords[latitude_coord].attrs.get("flipped"):
-#             new_dataset = dataset.copy()
-#             new_dataset = new_dataset.isel({latitude_coord: slice(None, None, -1)})
-#             new_dataset.coords[latitude_coord].attrs.pop("flipped")  # remove the flipped attribute
+    # If a latitude coordinate is found, determine if it has been flipped
+    # in that case, reverse it back
+    new_dataset = dataset
+    if latitude_coord is not None:
+        if dataset.coords[latitude_coord].attrs.get("flipped"):
+            new_dataset = dataset.copy()
+            new_dataset = new_dataset.isel({latitude_coord: slice(None, None, -1)})
+            new_dataset.coords[latitude_coord].attrs.pop("flipped")  # remove the flipped attribute
 
-#     return new_dataset
+    return new_dataset
 
 # # NOT IN USE, LEAVING FOR FUTURE REFERENCE
 # # In case this is uncommented, a test is available in tests/test_util.py
