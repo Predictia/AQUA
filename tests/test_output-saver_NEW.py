edf4c186bc1f10e01ab9696cac555f682b43560a
silviacaprioli <silvietta.caprioli@gmail.com>
2025-04-12 23:13:25 +0300

    update tests

--- tests/test_output-saver_NEW.py
+++ tests/test_output-saver_NEW.py
@@ -12,7 +12,7 @@ log_configure(log_level='DEBUG', log_name='OutputSaverTest')
 # Fixture for OutputSaver instance
 @pytest.fixture
 def output_saver():
-    return OutputSaver(diagnostic='dummy', model='MSWEP', exp='past',
+    return OutputSaver(diagnostic='dummy', model='IFS-NEMO', exp='historical',
                        catalog='lumi-phase2', loglevel='DEBUG')
 
 @pytest.mark.aqua
@@ -20,17 +20,23 @@ def test_generate_name(output_saver):
     """Test the generation of output filenames with and without additional parameters."""
     # Test filename generation without additional parameters
     filename = output_saver.generate_name(diagnostic_product='mean')
-    assert filename == 'dummy.mean.lumi-phase2.MSWEP.past'
+    assert filename == 'dummy.mean.lumi-phase2.IFS-NEMO.historical'
 
-    # Test with generic multi-model keyword
-    extra_keys = {'model': 'multi', 'var' : 'tprate'}
-    filename = output_saver.generate_name(diagnostic_product='mean', extra_keys=extra_keys)
-    assert filename == 'dummy.mean.multi.tprate'
+    # Test with generic multimodel keyword
+    extra_keys = {'var' : 'tprate'}
+    filename = output_saver.generate_name(diagnostic_product='mean', model='multimodel', extra_keys=extra_keys)
+    assert filename == 'dummy.mean.multimodel.tprate'
+
+    #Test with multiple references
+    extra_keys = {'var': 'tprate', 'region' : 'indian_ocean'}
+    filename = output_saver.generate_name(diagnostic_product='mean', model='IFS-NEMO', model_ref=['ERA5', 'CERES'], extra_keys=extra_keys)
+    assert filename == 'dummy.mean.lumi-phase2.IFS-NEMO.historical.multiref.tprate.indian_ocean'
 
     # Test with multiple models
-    extra_keys = {'model': ['IFS-NEMO', 'ICON', 'ERA5'], 'var': 'tprate', 'region' : 'indian_ocean'}
-    filename = output_saver.generate_name(diagnostic_product='mean', extra_keys=extra_keys)
-    assert filename == 'dummy.mean.IFS-NEMO_ICON_ERA5.tprate.indian_ocean'
+    extra_keys = {'var': 'tprate', 'region' : 'indian_ocean'}
+    filename = output_saver.generate_name(diagnostic_product='mean', model=['IFS-NEMO','ICON'], model_ref='ERA5', extra_keys=extra_keys)
+    assert filename == 'dummy.mean.multimodel.ERA5.tprate.indian_ocean'
+
 
 @pytest.mark.aqua
 def test_save_netcdf(output_saver):
@@ -54,7 +60,7 @@ def test_save_png(output_saver, tmpdir):
     path = output_saver.save_png(fig=fig, diagnostic_product='mean', extra_keys=extra_keys)
     
     # Check if the file was created
-    path = './png/dummy.mean.lumi-phase2.MSWEP.past.tprate.png'
+    path = './png/dummy.mean.lumi-phase2.IFS-NEMO.historical.tprate.png'
     assert os.path.exists(path)
 
 @pytest.mark.aqua
@@ -65,10 +71,10 @@ def test_save_pdf(output_saver, tmpdir):
     ax.plot([0, 1], [0, 1])
     
     # Save the PDF file
-    extra_keys = {'model': 'multi', 'var' : 'tprate'}
+    extra_keys = {'var' : 'tprate'}
     output_saver.save_pdf(fig=fig, diagnostic_product='mean', extra_keys=extra_keys)
     
     # Check if the file was created
-    path = './pdf/dummy.mean.multi.tprate.pdf'
+    path = './pdf/dummy.mean.lumi-phase2.IFS-NEMO.historical.tprate.pdf'
     assert os.path.exists(path)
     
