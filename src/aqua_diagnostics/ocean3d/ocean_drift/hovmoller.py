1ad09e8cbd82c3342c2c4fadc0718d8a94609fda
ghosh97 <supriyo.ghosh@bsc.es>
2025-04-16 15:36:57 +0200

    moved function into util, config files into config

--- src/aqua_diagnostics/ocean3d/ocean_drift/hovmoller.py
+++ src/aqua_diagnostics/ocean3d/ocean_drift/hovmoller.py
@@ -3,10 +3,10 @@ import xarray as xr
 from aqua.logger import log_configure
 from aqua.diagnostics.core import Diagnostic
 from aqua.util import area_selection
-from .util import data_process_by_type, predefined_regions
-from itertools import product
+from .util import predefined_regions, _data_process_for_drift
 
 xr.set_options(keep_attrs=True)
+
 
 class Hovmoller(Diagnostic):
     """
@@ -14,9 +14,18 @@ class Hovmoller(Diagnostic):
     This class inherits from the Diagnostic class and provides methods to retrieve,
     process, and save the netcdf for Hovmoller diagrams.
     """
-    def __init__(self, catalog=None, model = None, exp = None, source = None,
-                regrid = None, startdate = None, enddate = None,
-                loglevel='WARNING'):
+
+    def __init__(
+        self,
+        catalog=None,
+        model=None,
+        exp=None,
+        source=None,
+        regrid=None,
+        startdate=None,
+        enddate=None,
+        loglevel="WARNING",
+    ):
         """
         Initialize the Hovmoller class.
 
@@ -25,56 +34,55 @@ class Hovmoller(Diagnostic):
         config : dict
             Configuration dictionary containing the parameters for the Hovmoller diagram.
         """
-        super().__init__(catalog=catalog, model=model, exp=exp, source=source,
-                        regrid=regrid, startdate=startdate, enddate=enddate,
-                        loglevel=loglevel)
-        self.logger = log_configure(log_name='Hovmoller', log_level=loglevel)
-    
-    def run(self, outputdir = None, region = None,  var = ['thetao', 'so']):
+        super().__init__(
+            catalog=catalog,
+            model=model,
+            exp=exp,
+            source=source,
+            regrid=regrid,
+            startdate=startdate,
+            enddate=enddate,
+            loglevel=loglevel,
+        )
+        self.logger = log_configure(log_name="Hovmoller", log_level=loglevel)
+
+    def run(self, outputdir=None, region=None, var=["thetao", "so"]):
         self.outputdir = outputdir
         self.region = region
         self.var = var
         self.logger.info("Running Hovmoller diagram generation")
         self.retrieve()
         self.area_select()
-        self.process_data(dim_mean = ["lat","lon"])
-        self.save_netcdf(diagnostic = "Hovmoller", diagnostic_product = "Hovmoller")
+        self.stacked_data = _data_process_for_drift(
+            data=self.data, dim_mean=["lat", "lon"]
+        )
+        self.save_netcdf(diagnostic="Hovmoller", diagnostic_product="Hovmoller")
         self.logger.info("Hovmoller diagram saved to netcdf file")
-    
+
     def retrieve(self):
-        self.data, self.reader, self.catalog = super()._retrieve(catalog=self.catalog, model=self.model,
-                                                        exp=self.exp, source=self.source, var=self.var,
-                                                        regrid=self.regrid, startdate=self.startdate,
-                                                        enddate=self.enddate)
+        self.data, self.reader, self.catalog = super()._retrieve(
+            catalog=self.catalog,
+            model=self.model,
+            exp=self.exp,
+            source=self.source,
+            var=self.var,
+            regrid=self.regrid,
+            startdate=self.startdate,
+            enddate=self.enddate,
+        )
+
     def area_select(self):
         if self.region is not None:
             lat_s, lat_n, lon_w, lon_e = predefined_regions(self.region)
-            self.data = area_selection(data = self.data, lat = [lat_n, lat_s], lon = [lon_w, lon_e], drop = True)
-    
-    def process_data(self, dim_mean : None):
-
-        
-        if dim_mean is not None:
-            self.data = self.data.mean(dim=dim_mean)
-        data_list = []
-
-        for anomaly, standardise, anomaly_ref in product([False, True], [False, True], ["t0", "tmean"]):
-            data_proc, type, cmap = data_process_by_type(
-                data = self.data,
-                anomaly = anomaly,
-                standardise = standardise,
-                anomaly_ref = anomaly_ref,
-                loglevel = self.loglevel,
+            self.data = area_selection(
+                data=self.data, lat=[lat_n, lat_s], lon=[lon_w, lon_e], drop=True
             )
 
-            data_proc.attrs["cmap"] = cmap
-            data_proc = data_proc.expand_dims(dim={"type": [type]})
-            
-            data_list.append(data_proc)
-        
-        self.stacked_data = xr.concat(data_list, dim="type")
-                    
-    def save_netcdf(self, diagnostic, diagnostic_product, rebuild = True):
-        super().save_netcdf(data=self.stacked_data, diagnostic=diagnostic,
-                            diagnostic_product = diagnostic_product, outputdir = self.outputdir, rebuild = rebuild)
-        
+    def save_netcdf(self, diagnostic, diagnostic_product, rebuild=True):
+        super().save_netcdf(
+            data=self.stacked_data,
+            diagnostic=diagnostic,
+            diagnostic_product=diagnostic_product,
+            outputdir=self.outputdir,
+            rebuild=rebuild,
+        )
--- src/aqua_diagnostics/ocean3d/ocean_drift/util.py
+++ src/aqua_diagnostics/ocean3d/ocean_drift/util.py
@@ -1,22 +1,28 @@
 import xarray as xr
 import numpy as np
 from aqua.logger import log_configure
+from itertools import product
 from aqua.util import find_vert_coord, load_yaml, add_pdf_metadata
 import os
+
 
 def process_region_config(regions_yaml):
     """
     Converts keys at the top level of the dictionary to lowercase and removes spaces, underscores, and dashes.
     """
     if isinstance(regions_yaml, dict):
-        return {key.replace(" ", "").replace("_", "").replace("-", "").lower(): v for key, v in regions_yaml.items()}
+        return {
+            key.replace(" ", "").replace("_", "").replace("-", "").lower(): v
+            for key, v in regions_yaml.items()
+        }
     else:
         return regions_yaml
-    
-def predefined_regions(region, loglevel= "WARNING"):
+
+
+def predefined_regions(region, loglevel="WARNING"):
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    processed_region = region.replace(" ", "").replace("_","").replace("-","").lower()
-    regions_yaml = f"{current_dir}/config/regions.yaml"
+    processed_region = region.replace(" ", "").replace("_", "").replace("-", "").lower()
+    regions_yaml = f"{current_dir}/../../../../config/diagnostics/ocean3d/regions.yaml"
     regions_dict = load_yaml(regions_yaml)
     try:
         regions_dict = process_region_config(regions_dict["regions"])
@@ -27,12 +33,13 @@ def predefined_regions(region, loglevel= "WARNING"):
         lon_w = region_boundary.get("LonW")
     except KeyError:
         raise ValueError(
-            f"Invalid region name: {region}. Check the region name in config file or update it: {regions_yaml}")
+            f"Invalid region name: {region}. Check the region name in config file or update it: {regions_yaml}"
+        )
 
     return lat_s, lat_n, lon_w, lon_e
 
 
-def data_process_by_type(**kwargs):
+def _data_process_by_type(**kwargs):
     """
     Selects the type of timeseries and colormap based on the given parameters.
 
@@ -47,61 +54,87 @@ def data_process_by_type(**kwargs):
         type (str): Type of preprocessing approach applied
         cmap (str): Colormap to be used for the plot.
     """
-    loglevel= kwargs.get('loglevel',"WARNING")
-    logger = log_configure(loglevel, 'data_process_by_type')
+    loglevel = kwargs.get("loglevel", "WARNING")
+    logger = log_configure(loglevel, "data_process_by_type")
     data = kwargs["data"]
     anomaly = kwargs["anomaly"]
     anomaly_ref = kwargs["anomaly_ref"]
     standardise = kwargs["standardise"]
-    
+
     process_data = xr.Dataset()
 
-    
     if anomaly:
         anomaly_ref = anomaly_ref.lower().replace(" ", "").replace("_", "")
         if not standardise:
-            if anomaly_ref in ['tmean', "meantime", "timemean"]:
-                cmap = "coolwarm" #"PuOr"
+            if anomaly_ref in ["tmean", "meantime", "timemean"]:
+                cmap = "coolwarm"  # "PuOr"
                 for var in list(data.data_vars.keys()):
-                    process_data[var] = data[var] - data[var].mean(dim='time')
+                    process_data[var] = data[var] - data[var].mean(dim="time")
                 type = "anomaly wrt temporal mean"
-            elif anomaly_ref in ['t0', "intialtime", "firsttime"]:
-                cmap = "coolwarm" #"PuOr"
+            elif anomaly_ref in ["t0", "intialtime", "firsttime"]:
+                cmap = "coolwarm"  # "PuOr"
                 for var in list(data.data_vars.keys()):
                     process_data[var] = data[var] - data[var].isel(time=0)
                 type = "anomaly wrt initial time"
             else:
                 raise ValueError(
-                    "Select proper value of anomaly_ref: t0 or tmean, when anomaly = True ")
+                    "Select proper value of anomaly_ref: t0 or tmean, when anomaly = True "
+                )
             logger.debug(f"Data processed for {type}")
         if standardise:
-            if anomaly_ref in ['t0', "intialtime", "firsttime"]:
-                cmap = "coolwarm" #"PuOr"
+            if anomaly_ref in ["t0", "intialtime", "firsttime"]:
+                cmap = "coolwarm"  # "PuOr"
                 for var in list(data.data_vars.keys()):
                     var_data = data[var] - data[var].isel(time=0)
-                    var_data.attrs['units'] = 'Stand. Units'
+                    var_data.attrs["units"] = "Stand. Units"
                     # Calculate the standard anomaly by dividing the anomaly by its standard deviation along the time dimension
                     process_data[var] = var_data / var_data.std(dim="time")
                 type = "Std. anomaly wrt initial time"
-            elif anomaly_ref in ['tmean', "meantime", "timemean"]:
-                cmap = "coolwarm" #"PuOr"
+            elif anomaly_ref in ["tmean", "meantime", "timemean"]:
+                cmap = "coolwarm"  # "PuOr"
                 for var in list(data.data_vars.keys()):
-                    var_data = data[var] - data[var].mean(dim='time')
-                    var_data.attrs['units'] = 'Stand. Units'
+                    var_data = data[var] - data[var].mean(dim="time")
+                    var_data.attrs["units"] = "Stand. Units"
                     # Calculate the standard anomaly by dividing the anomaly by its standard deviation along the time dimension
                     process_data[var] = var_data / var_data.std(dim="time")
                 type = "Std. anomaly wrt temporal mean"
             else:
                 raise ValueError(
-                    "Select proper value of type: t0 or tmean, when anomaly = True ")
-            logger.debug(
-                f"Data processed for {type}")
+                    "Select proper value of type: t0 or tmean, when anomaly = True "
+                )
+            logger.debug(f"Data processed for {type}")
 
     else:
-        cmap = 'jet'
+        cmap = "jet"
         logger.debug("Data processed for Full values as anomaly = False")
         type = "Full values"
 
         process_data = data
     # logger.debug(f"Data processed for {type}")
     return process_data, type, cmap
+
+
+def _data_process_for_drift(data, dim_mean: None, loglevel="WARNING"):
+
+    if dim_mean is not None:
+        data = data.mean(dim=dim_mean)
+    data_list = []
+
+    for anomaly, standardise, anomaly_ref in product(
+        [False, True], [False, True], ["t0", "tmean"]
+    ):
+        data_proc, type, cmap = _data_process_by_type(
+            data=data,
+            anomaly=anomaly,
+            standardise=standardise,
+            anomaly_ref=anomaly_ref,
+            loglevel=loglevel,
+        )
+
+        data_proc.attrs["cmap"] = cmap
+        data_proc = data_proc.expand_dims(dim={"type": [type]})
+
+        data_list.append(data_proc)
+
+    stacked_data = xr.concat(data_list, dim="type")
+    return stacked_data
