3d736c9729573571d6113779496c0f2f1f2e3ad1
mnurisso <m.nurisso@isac.cnr.it>
2025-04-24 17:09:31 +0300

    interface loading in basemixin for telecs

--- src/aqua_diagnostics/teleconnections/base.py
+++ src/aqua_diagnostics/teleconnections/base.py
@@ -1,19 +1,23 @@
+import os
 import xarray as xr
 from aqua.diagnostics.core import Diagnostic
-from .tools import TeleconnectionsConfig
+from aqua.util import ConfigPath, load_yaml
 
 xr.set_options(keep_attrs=True)
 
 
 class BaseMixin(Diagnostic):
-    def __init__(self, catalog: str = None, model: str = None,
+    def __init__(self, telecname: str, catalog: str = None, model: str = None,
                  exp: str = None, source: str = None,
                  regrid: str = None,
                  startdate: str = None, enddate: str = None,
+                 configdir: str = None,
+                 interface: str = 'teleconnections-destine',
                  loglevel: str = 'WARNING'):
         """
         Initialize the Base class.
         Args:
+            telecname (str): The name of the teleconnection.
             catalog (str): The catalog to be used. If None, the catalog will be determined by the Reader.
             model (str): The model to be used.
             exp (str): The experiment to be used.
@@ -23,11 +27,17 @@ class BaseMixin(Diagnostic):
                              If None, all available data will be retrieved.
             enddate (str): The end date of the data to be retrieved.
                            If None, all available data will be retrieved.
+            configdir (str): The directory where the interface file is located.
+                             If None, the default directory will be used.
+            interface (str): The filename of the interface file.
+                             Default is 'teleconnections-destine'.
             loglevel (str): The log level to be used. Default is 'WARNING'.
         """
         super().__init__(catalog=catalog, model=model, exp=exp, source=source, regrid=regrid,
                          startdate=startdate, enddate=enddate, loglevel=loglevel)
 
+        self.interface = self.load_interface(configdir=configdir, interface=interface,
+                                             telecname=telecname)
         # Initialize the possible results
         self.index = None
 
@@ -39,8 +49,31 @@ class BaseMixin(Diagnostic):
                              dim: str = 'time', season: str = None):
         pass
 
-    def load_namelist(self, configdir: str = None, interface: str = None):
-        config = TeleconnectionsConfig(configdir=configdir, interface=interface)
+    def load_interface(self, configdir: str = None, interface: str = 'teleconnections-destine',
+                       telecname: str = None):
+        """
+        Load the interface for the teleconnections.
 
-        self.namelist = config.load_namelist()
-        self.logger.info('Namelist loaded')
+        Args:
+            configdir (str): The directory where the interface file is located.
+                              If None, the default directory will be used.
+            interface (str): The filename of the interface file.
+                             Default is 'teleconnections-destine'.
+            telecname (str): The name of the teleconnection. It selects the subset of the interface.
+        
+        Returns:
+            dict: The interface file as a dictionary.
+        """
+        # Add yaml to interface if not present
+        if not interface.endswith('.yaml'):
+            interface = f'{interface}.yaml'
+        if not configdir:
+            configdir = ConfigPath().get_config_dir()
+            configdir = os.path.join(configdir, 'diagnostics', 'teleconnections', 'config')
+        
+        interface_file = os.path.join(configdir, interface)
+        self.logger.debug(f'Loading interface file: {interface_file}')
+
+        interface_dict = load_yaml(interface_file)
+
+        return interface_dict[telecname] if telecname else interface_dict
