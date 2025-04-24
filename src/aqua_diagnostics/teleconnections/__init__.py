1e3bb45bd886e005eb8d04539cb5efe4d096249a
mnurisso <m.nurisso@isac.cnr.it>
2025-04-24 23:31:03 +0300

    ENSO in init

--- src/aqua_diagnostics/teleconnections/__init__.py
+++ src/aqua_diagnostics/teleconnections/__init__.py
@@ -1,15 +1,5 @@
 """Teleconnections module"""
-# from .bootstrap import bootstrap_teleconnections, build_confidence_mask
-# from .index import regional_mean_index
-# from .index import regional_mean_anomalies
-# from .mjo import mjo_hovmoller
-# from .plots import index_plot, indexes_plot
+from .enso import ENSO
 from .nao import NAO
 
-__all__ = [
-    # 'bootstrap_teleconnections', 'build_confidence_mask',
-    #        'regional_mean_index',
-    #        'regional_mean_anomalies',
-    #        'mjo_hovmoller',
-    #        'index_plot', 'indexes_plot',
-           'NAO']
+__all__ = ['ENSO','NAO']
