400c37a79c3be83b0db5e11872a8e69ffeb3e568
mnurisso <m.nurisso@isac.cnr.it>
2025-04-25 00:25:56 +0300

    removed index plot from teleconnections

--- src/aqua_diagnostics/teleconnections/plots/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Module for plotting teleconnection indices and maps.
-A function for Hovmoller plot is added.
-"""
-from .index_plot import index_plot, indexes_plot
-
-__all__ = ['index_plot', 'indexes_plot']
