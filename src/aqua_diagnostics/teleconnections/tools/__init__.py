cfd18db65515d343f5abbfe67f612790a16ca41e
mnurisso <m.nurisso@isac.cnr.it>
2025-04-24 17:56:14 +0300

    removed area weight

--- src/aqua_diagnostics/teleconnections/tools/__init__.py
+++ src/aqua_diagnostics/teleconnections/tools/__init__.py
@@ -1,6 +1,4 @@
 """Tools for teleconnections diagnostics."""
 from .cli_tools import set_figs
-from .sci_tools import wgt_area_mean
 
-__all__ = ['set_figs',
-           'wgt_area_mean']
+__all__ = ['set_figs']
