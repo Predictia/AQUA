b5e31d7e310d595caa234eb8bf93fb1cb84909c5
mnurisso <m.nurisso@isac.cnr.it>
2025-04-24 23:29:10 +0300

    removing useless import

--- src/aqua_diagnostics/__init__.py
+++ src/aqua_diagnostics/__init__.py
@@ -1,11 +1,9 @@
-from .teleconnections import Teleconnection
 from .timeseries import GregoryPlot, SeasonalCycle, Timeseries
 from .global_biases import GlobalBiases
 from .radiation import Radiation
 from .ensemble import EnsembleTimeseries, EnsembleLatLon, EnsembleZonal
 from .ecmean import PerformanceIndices, GlobalMean
-__all__ = ["Teleconnection",
-           "GregoryPlot", "SeasonalCycle", "Timeseries", 
+__all__ = ["GregoryPlot", "SeasonalCycle", "Timeseries", 
            "GlobalBiases",
            "Radiation", 
            "EnsembleTimeseries", "EnsembleLatLon", "EnsembleZonal",
--- src/aqua_diagnostics/teleconnections/__init__.py
+++ src/aqua_diagnostics/teleconnections/__init__.py
@@ -1,16 +1,15 @@
 """Teleconnections module"""
-from .bootstrap import bootstrap_teleconnections, build_confidence_mask
-from .index import regional_mean_index
-from .index import regional_mean_anomalies
-from .mjo import mjo_hovmoller
-from .plots import index_plot, indexes_plot
-from .tc_class import Teleconnection
+# from .bootstrap import bootstrap_teleconnections, build_confidence_mask
+# from .index import regional_mean_index
+# from .index import regional_mean_anomalies
+# from .mjo import mjo_hovmoller
+# from .plots import index_plot, indexes_plot
 from .nao import NAO
 
-__all__ = ['bootstrap_teleconnections', 'build_confidence_mask',
-           'regional_mean_index',
-           'regional_mean_anomalies',
-           'mjo_hovmoller',
-           'index_plot', 'indexes_plot',
-           'Teleconnection',
+__all__ = [
+    # 'bootstrap_teleconnections', 'build_confidence_mask',
+    #        'regional_mean_index',
+    #        'regional_mean_anomalies',
+    #        'mjo_hovmoller',
+    #        'index_plot', 'indexes_plot',
            'NAO']
