c1a31e53e0bdf1fff40b4b68d02cffc4f18a7bc8
mnurisso <m.nurisso@isac.cnr.it>
2025-04-24 17:58:03 +0300

    removed multiple maps duplicate in teleconnections

--- src/aqua_diagnostics/teleconnections/__init__.py
+++ src/aqua_diagnostics/teleconnections/__init__.py
@@ -4,10 +4,7 @@ from .index import regional_mean_index
 from .index import regional_mean_anomalies
 from .mjo import mjo_hovmoller
 from .plots import index_plot, indexes_plot
-from .plots import maps_diffs_plot
-from .tc_statistics import reg_evaluation, cor_evaluation
 from .tc_class import Teleconnection
-from .tools import wgt_area_mean
 from .nao import NAO
 
 __all__ = ['bootstrap_teleconnections', 'build_confidence_mask',
@@ -15,8 +12,5 @@ __all__ = ['bootstrap_teleconnections', 'build_confidence_mask',
            'regional_mean_anomalies',
            'mjo_hovmoller',
            'index_plot', 'indexes_plot',
-           'maps_diffs_plot',
-           'reg_evaluation', 'cor_evaluation',
            'Teleconnection',
-           'wgt_area_mean',
            'NAO']
