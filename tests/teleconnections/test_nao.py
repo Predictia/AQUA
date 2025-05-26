1cd9dcf25b4ddd8a7a44cf844ddce4b1c2935ab1
Matteo Nurisso <m.nurisso@isac.cnr.it>
2025-05-26 13:08:18 +0300

    new nao tests

--- tests/teleconnections/test_nao.py
+++ tests/teleconnections/test_nao.py
@@ -1,37 +1,50 @@
 import pytest
-from aqua.diagnostics import Teleconnection
+from aqua.diagnostics.teleconnections import NAO, PlotNAO
 
 # pytest approximation, to bear with different machines
 approx_rel = 1e-4
 loglevel = 'DEBUG'
 
-
 @pytest.mark.diagnostics
-def test_class_NAO():
+def test_NAO():
     """
     Test that the NAO class works
     """
-    telecname = 'NAO'
-    model = 'IFS'
-    exp = 'test-tco79'
-    source = 'teleconnections'
-    interface = 'teleconnections-ci'
+    init_dict = {
+        'model': 'IFS',
+        'exp': 'test-tco79',
+        'source': 'teleconnections',
+        'loglevel': loglevel
+    }
 
-    telec = Teleconnection(model=model, exp=exp, source=source,
-                           loglevel=loglevel, telecname=telecname,
-                           interface=interface)
+    nao = NAO(**init_dict)
+    nao.retrieve()
 
-    telec.evaluate_index()
-    assert telec.index[4].values == pytest.approx(0.21909582, rel=approx_rel)
+    assert nao.data is not None, "Data should not be None"
 
-    reg = telec.evaluate_regression()
-    assert reg.isel(lon=4, lat=23).values == pytest.approx(0.77142069,
-                                                           rel=approx_rel)
+    nao.compute_index()
+    assert nao.index is not None, "Index should not be None"
+    assert nao.index[4].values == pytest.approx(0.21909582, rel=approx_rel)
 
-    cor = telec.evaluate_correlation()
-    assert cor.isel(lon=4, lat=23).values == pytest.approx(0.00220419,
-                                                           rel=approx_rel)
+    reg_DJF = nao.compute_regression(season='DJF')
+    assert reg_DJF.isel(lon=4, lat=23).values == pytest.approx(80.99873476, rel=approx_rel)
 
-    reg_DJF = telec.evaluate_regression(season='DJF')
-    assert reg_DJF.isel(lon=4, lat=23).values == pytest.approx(80.99873476,
-                                                               rel=approx_rel)
+    cor = nao.compute_correlation()
+    assert cor.isel(lon=4, lat=23).values == pytest.approx(0.00220419, rel=approx_rel)
+
+    init_dict_hpx = {
+        'model': 'ERA5',
+        'exp': 'era5-hpx3',
+        'source': 'monthly',
+        'loglevel': loglevel
+    }
+
+    nao_hpx = NAO(**init_dict_hpx)
+
+    reg_DJF_hpx = nao_hpx.compute_regression(season='DJF')
+    cor_hpx = nao_hpx.compute_correlation()
+
+    plot_ref = PlotNAO(loglevel=loglevel, indexes=nao.index,
+                       ref_indexes=nao_hpx.index)
+
+    fig, _ = plot_ref.plot_index()
