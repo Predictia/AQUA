3330572bb318a0e8541132f1f636fb615fac3fc8
Matteo Nurisso <m.nurisso@isac.cnr.it>
2025-05-26 20:45:04 +0300

    mjo skeleton for plot

--- src/aqua_diagnostics/teleconnections/mjo.py
+++ src/aqua_diagnostics/teleconnections/mjo.py
@@ -1,9 +1,11 @@
 import xarray as xr
 from aqua.logger import log_configure
+from aqua.util import area_selection, to_list
 from .base import BaseMixin
 
 # set default options for xarray
 xr.set_options(keep_attrs=True)
+
 
 class MJO(BaseMixin):
     """
@@ -38,81 +40,94 @@ class MJO(BaseMixin):
         self.logger = log_configure(log_name='MJO', log_level=loglevel)
 
         self.var = self.interface.get('field')
+        self.data_hovmoller = None
 
         # Delete the self.index attribute if it exists
         if hasattr(self, 'index'):
             del self.index
 
+    def retrieve(self):
+        # Assign self.data, self.reader, self.catalog
+        super().retrieve(var=self.var)
 
-def mjo_hovmoller(data=None,
-                  var: str = None,
-                  loglevel: str = "WARNING",
-                  telecname: str = "MJO",
-                  namelist: dict = None,
-                  day_window: int = None,
-                  flip_sign: bool = False,
-                  **kwargs) -> xr.DataArray:
+        self.reader.timmean(self.data, freq='D')
+
+    def compute_hovmoller(self, day_window: int = None):
+        """
+        Compute the Hovmoller plot for the MJO index.
+        This method prepares the data for a Hovmoller plot by selecting the MJO box,
+        evaluating anomalies, and smoothing the data if required.
+
+        Args:
+            day_window (int, optional): Number of days to be used in the smoothing window.
+                                        If None, no smoothing is performed. Default is None.
+        """
+        if self.interface.get('flip_sign', True):
+            self.logger.info("Flipping the sign of the variable.")
+            self.data = -self.data
+        
+        # Acquiring MJO box
+        lat = [self.interface['latS'], self.interface['latN']]
+        lon = [self.interface['lonW'], self.interface['lonE']]
+
+        # Selecting the MJO box
+        data_sel = area_selection(self.data, lat=lat, lon=lon, drop=True)
+
+        # Evaluating anomalies
+        data_mean = data_sel.mean(dim='time')
+        data_anom = data_sel - data_mean
+
+        # Smoothing the data
+        if day_window:
+            self.logger.info("Smoothing the data with a window of " + str(day_window) + " days.")
+            self.data_hovmoller = data_anom.rolling(time=day_window, center=True).mean()
+        else:
+            self.data_hovmoller = data_anom
+
+
+class PlotMJO:
     """
-    Prepare the data for a MJO Hovmoller plot.
-
-    Args:
-        data: (xr.DataArray or xr.Dataset): Data to be used in the Hovmoller plot.
-        var (str, opt): Name of the variable to be used in the Hovmoller plot.
-                        It is only required if data is a dataset.
-                        If not provided, it will be extracted from the namelist
-        loglevel (str):   Log level to be used. Default is "WARNING".
-        telecname (str):  Name of the teleconnection to be evaluated.
-                          Default is "MJO".
-        namelist (dict):  Namelist with the teleconnection informations.
-        day_window (int): Number of days to be used in the smoothing window.
-                          Default is not performed.
-        flip_sign (bool): If True, it will flip the sign of the variable.
-                          Will be extracted from the namelist if not provided. Default is False.
-
-    KwArgs:
-        configdir (str):  Path to the configuration directory.
-
-    Returns:
-        xr.DataArray: DataArray with the data to be used in the Hovmoller plot.
+    PlotMJO class for plotting the MJO Hovmoller data.
+    This class is a placeholder for future plotting methods.
     """
-    logger = log_configure(log_level=loglevel, log_name='MJO')
+    def __init__(self, data, outputdir: str = './', rebuild: bool = True, loglevel: str = 'WARNING'):
+        """
+        Initialize the PlotMJO class.
 
-    if data is None:
-        raise ValueError("No data provided.")
+        Args:
+            data (list or xarray.DataArray): The list of data to be plot.
+            outputdir (str): Directory where the plots will be saved. Default is './'.
+            rebuild (bool): If True, the plots will be rebuilt. Default is True.
+            loglevel (str): Logging level. Default is 'WARNING'.
+        """
+        # Data info initalized as empty
+        self.loglevel = loglevel
+        self.logger = log_configure(self.loglevel, 'PlotMJO')
+        self.catalogs = None
+        self.models = None
+        self.exps = None
 
-    if namelist is None:
-        logger.info("No namelist provided. Trying to load default namelist.")
-        config = TeleconnectionsConfig(**kwargs)
-        namelist = config.load_namelist()
+        self.data = to_list(data)
+        self.len_data = len(self.data)
 
-    # Subselect field if data is a dataset
-    if var is None:
-        var = namelist[telecname]['field']
-    if isinstance(data, xr.Dataset):
-        logger.info("Subselecting var " + var)
-        data = data[var]
+        self.get_data_info()
 
-    flip_sign = namelist[telecname].get('flip_sign', flip_sign)
-    if flip_sign:
-        logger.info("Flipping the sign of the variable.")
-        data = -data
+    def get_data_info(self):
+        """
+        We extract the data needed for labels, description etc
+        from the data arrays attributes.
 
-    # Acquiring MJO box
-    lat = [namelist[telecname]['latS'], namelist[telecname]['latN']]
-    lon = [namelist[telecname]['lonW'], namelist[telecname]['lonE']]
+        The attributes are:
+        - AQUA_catalog
+        - AQUA_model
+        - AQUA_exp
+        """
+        self.catalogs = [d.AQUA_catalog for d in self.data]
+        self.models = [d.AQUA_model for d in self.data]
+        self.exps = [d.AQUA_exp for d in self.data]
 
-    # Selecting the MJO box
-    data_sel = area_selection(data, lat=lat, lon=lon, drop=True)
-
-    # Evaluating anomalies
-    data_mean = data_sel.mean(dim='time')
-    data_anom = data_sel - data_mean
-
-    # Smoothing the data
-    if day_window:
-        logger.info("Smoothing the data with a window of " + str(day_window) + " days.")
-        data_anom_smooth = data_anom.rolling(time=day_window, center=True).mean()
-    else:
-        data_anom_smooth = data_anom
-
-    return data_anom_smooth
+    def plot(self):
+        """
+        Placeholder method for plotting the MJO Hovmoller data.
+        """
+        raise NotImplementedError("Plotting method not implemented yet.")
