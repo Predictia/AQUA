7aed9b87bfbe80e2316199db98070424784ffcae
mnurisso <m.nurisso@isac.cnr.it>
2025-05-26 19:17:07 +0200

    MJO in new init

--- src/aqua_diagnostics/teleconnections/__init__.py
+++ src/aqua_diagnostics/teleconnections/__init__.py
@@ -1,8 +1,9 @@
 """Teleconnections module"""
 from .enso import ENSO
+from .mjo import MJO
 from .nao import NAO
 from .plot_enso import PlotENSO
 from .plot_nao import PlotNAO
 
-__all__ = ['ENSO', 'NAO',
+__all__ = ['ENSO', 'MJO', 'NAO',
            'PlotENSO', 'PlotNAO']
