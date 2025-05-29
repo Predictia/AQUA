f46732e66acb5964dea4fa115df7f9308f7173f2
Matteo Nurisso <m.nurisso@isac.cnr.it>
2025-05-29 16:57:58 +0300

    teleconnections functions exposed in main init

--- src/aqua_diagnostics/__init__.py
+++ src/aqua_diagnostics/__init__.py
@@ -1,10 +1,14 @@
+from .teleconnections import NAO, ENSO, MJO
+from .teleconnections import PlotNAO, PlotENSO, PlotMJO
 from .timeseries import Gregory, SeasonalCycles, Timeseries
 from .global_biases import GlobalBiases
 from .radiation import Radiation
 from .ensemble import EnsembleTimeseries, EnsembleLatLon, EnsembleZonal
 from .ecmean import PerformanceIndices, GlobalMean
 
-__all__ = ["Gregory", "SeasonalCycles", "Timeseries",
+__all__ = ["NAO", "ENSO", "MJO",
+           "PlotNAO", "PlotENSO", "PlotMJO",
+           "Gregory", "SeasonalCycles", "Timeseries",
            "GlobalBiases",
            "Radiation",
            "EnsembleTimeseries", "EnsembleLatLon", "EnsembleZonal",
