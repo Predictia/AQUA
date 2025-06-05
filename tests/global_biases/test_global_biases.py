6820795411ab2a8eaf1005b899be292cb437b995
silviacaprioli <silvietta.caprioli@gmail.com>
2025-06-05 16:52:58 +0300

    tests for globalbiases diagnostic

--- tests/global_biases/test_global_biases.py
+++ tests/global_biases/test_global_biases.py
@@ -1,61 +1,63 @@
 import pytest
+import os
 import numpy as np
-from aqua import Reader
-from aqua.diagnostics import GlobalBiases
+import xarray as xr
+from aqua.diagnostics import GlobalBiases, PlotGlobalBiases
 
 # Tolerance for numerical comparisons
 approx_rel = 1e-4
 loglevel = 'DEBUG'
 
-reader = Reader(model='ERA5', exp='era5-hpz3', source='monthly', catalog='ci', regrid='r100')
-data = reader.retrieve()
-data = reader.regrid(data)
+tmp_path = "./"
+var = 'q'
+gb = GlobalBiases(catalog='ci', model='ERA5', exp='era5-hpz3', source='monthly', regrid='r100')
+plotgb = PlotGlobalBiases()
+gb.retrieve(var=var)
+
+@pytest.mark.diagnostics
+def test_climatology():
+    gb.compute_climatology(var=var, seasonal=True)
+    assert hasattr(gb, "climatology")
+    assert hasattr(gb, "seasonal_climatology")
+    assert isinstance(gb.climatology, xr.Dataset)
+    assert isinstance(gb.seasonal_climatology, xr.Dataset)
+    assert var in gb.climatology
+    assert var in gb.seasonal_climatology
+    assert "season" in gb.seasonal_climatology[var].dims
+    assert set(gb.seasonal_climatology["season"].values) == {"DJF", "MAM", "JJA", "SON"}
+    
+    nc = os.path.join(tmp_path, 'netcdf', 'globalbiases.climatology.ci.ERA5.era5-hpz3.q.nc')
+    assert os.path.exists(nc)
+
+    nc_seasonal = os.path.join(tmp_path, 'netcdf', 'globalbiases.seasonal_climatology.ci.ERA5.era5-hpz3.q.nc')
+    assert os.path.exists(nc_seasonal)
+
+    plotgb.plot_climatology(data=gb.climatology, var=var, plev=85000)
+    
+    pdf = os.path.join(tmp_path, 'pdf', 'globalbiases.climatology.ci.ERA5.era5-hpz3.q.85000.pdf')
+    assert os.path.exists(pdf)
+
+    png = os.path.join(tmp_path, 'png', 'globalbiases.climatology.ci.ERA5.era5-hpz3.q.85000.png')
+    assert os.path.exists(png)
+
+def test_bias():
+    plotgb.plot_bias(data=gb.climatology, data_ref=gb.climatology, var=var, plev=85000)
+    pdf = os.path.join(tmp_path, 'pdf', 'globalbiases.bias.ci.ERA5.era5-hpz3.ERA5.era5-hpz3.q.85000.pdf')
+    assert os.path.exists(pdf)
+    png = os.path.join(tmp_path, 'png', 'globalbiases.bias.ci.ERA5.era5-hpz3.ERA5.era5-hpz3.q.85000.png')
+    assert os.path.exists(png)
 
 
-#@pytest.mark.diagnostics
-def test_global_bias():
-    """
-    Test for global bias computation.
-    """
-    global_biases = GlobalBiases(data=data, data_ref=data, var_name='tprate')
-    fig, ax, bias = global_biases.plot_bias(vmin=None, vmax=None)
-    assert fig is not None
-    assert ax is not None
-    assert np.allclose(np.nanmean(bias.values), 0, atol=approx_rel)
-
-
-#@pytest.mark.diagnostics
 def test_seasonal_bias():
-    """
-    Test for seasonal bias computation.
-    """
-    global_biases = GlobalBiases(data=data, data_ref=data, var_name='2t')
-    fig, bias = global_biases.plot_seasonal_bias(vmin=-15, vmax=15)
-    assert fig is not None
-    for season in ['DJF', 'MAM', 'JJA', 'SON']:
-        assert np.allclose(np.nanmean(bias[season].values), 0, atol=approx_rel)
+    plotgb.plot_seasonal_bias(data=gb.seasonal_climatology, data_ref=gb.seasonal_climatology, var=var, plev=85000)
+    pdf = os.path.join(tmp_path, 'pdf', 'globalbiases.seasonal_bias.ci.ERA5.era5-hpz3.ERA5.era5-hpz3.q.85000.pdf')
+    assert os.path.exists(pdf)
+    png = os.path.join(tmp_path, 'png', 'globalbiases.seasonal_bias.ci.ERA5.era5-hpz3.ERA5.era5-hpz3.q.85000.png')
+    assert os.path.exists(png)
 
-
-#@pytest.mark.diagnostics
-def test_plev():
-    """
-    Test for 3D variable bias computation (e.g., specific humidity at 850 hPa).
-    """
-    global_biases = GlobalBiases(data=data, data_ref=data, var_name='q', plev=85000)
-    fig, ax, bias = global_biases.plot_bias(vmin=-0.002, vmax=0.002)
-    assert fig is not None
-    assert ax is not None
-    assert np.allclose(np.nanmean(bias.values), 0, atol=approx_rel)
-    assert getattr(bias, "plev", None) == 85000
-
-
-# @pytest.mark.diagnostics
 def test_vertical_bias():
-    """
-    Test for vertical bias computation.
-    """
-    global_biases = GlobalBiases(data=data, data_ref=data, var_name='q')
-    fig, ax, bias = global_biases.plot_vertical_bias(vmin=-0.002, vmax=0.002)
-    assert fig is not None
-    assert ax is not None
-    assert np.allclose(np.nanmean(bias.values), 0, atol=approx_rel)
+    plotgb.plot_vertical_bias(data=gb.climatology, data_ref=gb.climatology, var=var, vmin= -0.002, vmax= 0.002)
+    pdf = os.path.join(tmp_path, 'pdf', 'globalbiases.vertical_bias.ci.ERA5.era5-hpz3.ERA5.era5-hpz3.q.pdf')
+    assert os.path.exists(pdf)
+    png = os.path.join(tmp_path, 'png', 'globalbiases.vertical_bias.ci.ERA5.era5-hpz3.ERA5.era5-hpz3.q.png')
+    assert os.path.exists(png)
