ab4d1bfb7043fb9d56db0edcfe61255dcf7f4323
mcadau <marco.cadau@polito.it>
2025-06-05 18:21:03 +0300

    implementation of lon/lat mean on fldstat

--- src/aqua/fldstat/fldstat.py
+++ src/aqua/fldstat/fldstat.py
@@ -57,7 +57,11 @@ class FldStat():
         """
         return self.fldstat(data, stat="mean", **kwargs)
 
-    def fldstat(self, data, stat="mean", lon_limits=None, lat_limits=None, **kwargs):
+    def fldstat(self, data, 
+                stat="mean", 
+                lon_limits=None, lat_limits=None, 
+                dims = None,
+                **kwargs):
         """
         Perform a weighted global average.
         If a subset of the data is provided, the average is performed only on the subset.
@@ -67,6 +71,7 @@ class FldStat():
             stat (str):  the statistic to compute, only supported is "mean"
             lon_limits (list, optional):  the longitude limits of the subset
             lat_limits (list, optional):  the latitude limits of the subset
+            dims (list, optional):  the dimensions to average over, if not provided, horizontal_dims are used
 
         Kwargs:
             - box_brd (bool,opt): choose if coordinates are comprised or not in area selection.
@@ -91,6 +96,18 @@ class FldStat():
             self.horizontal_dims = data_gridtype[0].horizontal_dims
             self.logger.debug('Horizontal dimensions guessed from data are %s', self.horizontal_dims)
 
+        # Determine which dims to average over based on mean_type
+        if not dims:
+            dims = self.horizontal_dims
+        else:
+            if not isinstance(dims, list):
+                raise ValueError("dims must be a list of dimension names.")
+            # check if dims are in horizontal_dims
+            for dim in dims:
+                if dim not in self.horizontal_dims:
+                    raise ValueError(f"Dimension {dim} not found in horizontal dimensions: {self.horizontal_dims}")
+ 
+        
         #if area is not provided, return the raw mean
         if self.area is None:
             self.logger.warning("No area provided, no area-weighted stat can be provided.")
@@ -102,9 +119,6 @@ class FldStat():
 
         # align dimensions naming of area to match data
         self.area = self.align_area_dimensions(data)
-
-        # align coordinates values of area to match data
-        self.area = self.align_area_coordinates(data)
 
         if lon_limits is not None or lat_limits is not None:
             data = area_selection(data, lon=lon_limits, lat=lat_limits,
@@ -118,7 +132,7 @@ class FldStat():
         if stat in ["mean"]:
             weighted_data = data.weighted(weights=self.area.fillna(0))
             self.logger.info("Computing area-weighted %s on %s dimensions", stat, self.horizontal_dims)
-            out = getattr(weighted_data, stat)(dim=self.horizontal_dims)
+            out = getattr(weighted_data, stat)(dim=dims)
 
         if self.grid_name is not None:
             log_history(out, f"Spatially reduced by fld{stat} from {self.grid_name} grid")
