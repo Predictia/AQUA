ee00c8a0a59aae848db4ce665f8c045aba46687c
silviacaprioli <silvietta.caprioli@gmail.com>
2025-06-06 18:24:11 +0300

    switch to class structure

--- tests/global_biases/test_global_biases.py
+++ tests/global_biases/test_global_biases.py
@@ -8,80 +8,75 @@ from aqua.diagnostics import GlobalBiases, PlotGlobalBiases
 approx_rel = 1e-4
 loglevel = 'DEBUG'
 
-tmp_path = "./"
-var = 'q'
-gb = GlobalBiases(catalog='ci', model='ERA5', exp='era5-hpz3', source='monthly', regrid='r100')
-plotgb = PlotGlobalBiases()
-gb.retrieve()
-
 @pytest.mark.diagnostics
-def test_climatology():
-    gb.compute_climatology(var=var, seasonal=True)
-    assert hasattr(gb, "climatology")
-    assert hasattr(gb, "seasonal_climatology")
-    assert isinstance(gb.climatology, xr.Dataset)
-    assert isinstance(gb.seasonal_climatology, xr.Dataset)
-    assert var in gb.climatology
-    assert var in gb.seasonal_climatology
-    assert "season" in gb.seasonal_climatology[var].dims
-    assert set(gb.seasonal_climatology["season"].values) == {"DJF", "MAM", "JJA", "SON"}
-    
-    nc = os.path.join(tmp_path, 'netcdf', 'globalbiases.climatology.ci.ERA5.era5-hpz3.q.nc')
-    assert os.path.exists(nc)
+class TestGlobalBiases:
+    @classmethod
+    def setup_class(cls):
+        cls.tmp_path = "./"
+        cls.var = 'q'
+        cls.gb = GlobalBiases(catalog='ci', model='ERA5', exp='era5-hpz3', source='monthly', regrid='r100')
+        cls.plotgb = PlotGlobalBiases()
+        cls.gb.retrieve()
 
-    nc_seasonal = os.path.join(tmp_path, 'netcdf', 'globalbiases.seasonal_climatology.ci.ERA5.era5-hpz3.q.nc')
-    assert os.path.exists(nc_seasonal)
+    def test_climatology(self):
+        self.gb.compute_climatology(var=self.var, seasonal=True)
+        assert hasattr(self.gb, "climatology")
+        assert hasattr(self.gb, "seasonal_climatology")
+        assert isinstance(self.gb.climatology, xr.Dataset)
+        assert isinstance(self.gb.seasonal_climatology, xr.Dataset)
+        assert self.var in self.gb.climatology
+        assert self.var in self.gb.seasonal_climatology
+        assert "season" in self.gb.seasonal_climatology[self.var].dims
+        assert set(self.gb.seasonal_climatology["season"].values) == {"DJF", "MAM", "JJA", "SON"}
 
-    plotgb.plot_climatology(data=gb.climatology, var=var, plev=85000)
-    
-    pdf = os.path.join(tmp_path, 'pdf', 'globalbiases.climatology.ci.ERA5.era5-hpz3.q.85000.pdf')
-    assert os.path.exists(pdf)
+        nc = os.path.join(self.tmp_path, 'netcdf', f'globalbiases.climatology.ci.ERA5.era5-hpz3.{self.var}.nc')
+        assert os.path.exists(nc)
 
-    png = os.path.join(tmp_path, 'png', 'globalbiases.climatology.ci.ERA5.era5-hpz3.q.85000.png')
-    assert os.path.exists(png)
+        nc_seasonal = os.path.join(self.tmp_path, 'netcdf', f'globalbiases.seasonal_climatology.ci.ERA5.era5-hpz3.{self.var}.nc')
+        assert os.path.exists(nc_seasonal)
 
-@pytest.mark.diagnostics
-def test_bias():
-    plotgb.plot_bias(data=gb.climatology, data_ref=gb.climatology, var=var, plev=85000)
-    pdf = os.path.join(tmp_path, 'pdf', 'globalbiases.bias.ci.ERA5.era5-hpz3.ERA5.era5-hpz3.q.85000.pdf')
-    assert os.path.exists(pdf)
-    png = os.path.join(tmp_path, 'png', 'globalbiases.bias.ci.ERA5.era5-hpz3.ERA5.era5-hpz3.q.85000.png')
-    assert os.path.exists(png)
+        self.plotgb.plot_climatology(data=self.gb.climatology, var=self.var, plev=85000)
 
-@pytest.mark.diagnostics
-def test_seasonal_bias():
-    plotgb.plot_seasonal_bias(data=gb.seasonal_climatology, data_ref=gb.seasonal_climatology, var=var, plev=85000)
-    pdf = os.path.join(tmp_path, 'pdf', 'globalbiases.seasonal_bias.ci.ERA5.era5-hpz3.ERA5.era5-hpz3.q.85000.pdf')
-    assert os.path.exists(pdf)
-    png = os.path.join(tmp_path, 'png', 'globalbiases.seasonal_bias.ci.ERA5.era5-hpz3.ERA5.era5-hpz3.q.85000.png')
-    assert os.path.exists(png)
+        pdf = os.path.join(self.tmp_path, 'pdf', f'globalbiases.climatology.ci.ERA5.era5-hpz3.{self.var}.85000.pdf')
+        assert os.path.exists(pdf)
 
-@pytest.mark.diagnostics
-def test_vertical_bias():
-    plotgb.plot_vertical_bias(data=gb.climatology, data_ref=gb.climatology, var=var, vmin= -0.002, vmax= 0.002)
-    pdf = os.path.join(tmp_path, 'pdf', 'globalbiases.vertical_bias.ci.ERA5.era5-hpz3.ERA5.era5-hpz3.q.pdf')
-    assert os.path.exists(pdf)
-    png = os.path.join(tmp_path, 'png', 'globalbiases.vertical_bias.ci.ERA5.era5-hpz3.ERA5.era5-hpz3.q.png')
-    assert os.path.exists(png)
+        png = os.path.join(self.tmp_path, 'png', f'globalbiases.climatology.ci.ERA5.era5-hpz3.{self.var}.85000.png')
+        assert os.path.exists(png)
 
-@pytest.mark.diagnostics
-def test_plev_selection():
-    gb.retrieve(var='q', plev=85000)
-    gb.compute_climatology(var=var, plev=85000)
-    assert gb.climatology['q'].coords['plev'] == 85000
+    def test_bias(self):
+        self.plotgb.plot_bias(data=self.gb.climatology, data_ref=self.gb.climatology, var=self.var, plev=85000)
+        pdf = os.path.join(self.tmp_path, 'pdf', f'globalbiases.bias.ci.ERA5.era5-hpz3.ERA5.era5-hpz3.{self.var}.85000.pdf')
+        assert os.path.exists(pdf)
+        png = os.path.join(self.tmp_path, 'png', f'globalbiases.bias.ci.ERA5.era5-hpz3.ERA5.era5-hpz3.{self.var}.85000.png')
+        assert os.path.exists(png)
 
-    with pytest.raises(ValueError):
-       gb.retrieve('tprate', plev=85000)
-    
-@pytest.mark.diagnostics
-def test_variables():
-    gb = GlobalBiases(catalog='ci', model='ERA5', exp='era5-hpz3', source='monthly')
-    with pytest.raises(ValueError):  
-        gb.retrieve(var='pippo')
+    def test_seasonal_bias(self):
+        self.plotgb.plot_seasonal_bias(data=self.gb.seasonal_climatology, data_ref=self.gb.seasonal_climatology, var=self.var, plev=85000)
+        pdf = os.path.join(self.tmp_path, 'pdf', f'globalbiases.seasonal_bias.ci.ERA5.era5-hpz3.ERA5.era5-hpz3.{self.var}.85000.pdf')
+        assert os.path.exists(pdf)
+        png = os.path.join(self.tmp_path, 'png', f'globalbiases.seasonal_bias.ci.ERA5.era5-hpz3.ERA5.era5-hpz3.{self.var}.85000.png')
+        assert os.path.exists(png)
 
-    gb.retrieve(var='tprate', units='mm/day')
-    gb.compute_climatology(var='tprate')
-    assert gb.climatology['tprate'].attrs.get('units') == 'mm/day' 
+    def test_vertical_bias(self):
+        self.plotgb.plot_vertical_bias(data=self.gb.climatology, data_ref=self.gb.climatology, var=self.var, vmin=-0.002, vmax=0.002)
+        pdf = os.path.join(self.tmp_path, 'pdf', f'globalbiases.vertical_bias.ci.ERA5.era5-hpz3.ERA5.era5-hpz3.{self.var}.pdf')
+        assert os.path.exists(pdf)
+        png = os.path.join(self.tmp_path, 'png', f'globalbiases.vertical_bias.ci.ERA5.era5-hpz3.ERA5.era5-hpz3.{self.var}.png')
+        assert os.path.exists(png)
 
+    def test_plev_selection(self):
+        self.gb.retrieve(var='q', plev=85000)
+        self.gb.compute_climatology(var=self.var, plev=85000)
+        assert self.gb.climatology['q'].coords['plev'] == 85000
 
- 
+        with pytest.raises(ValueError):
+            self.gb.retrieve('tprate', plev=85000)
+
+    def test_variables(self):
+        gb_local = GlobalBiases(catalog='ci', model='ERA5', exp='era5-hpz3', source='monthly')
+        with pytest.raises(ValueError):
+            gb_local.retrieve(var='pippo')
+
+        gb_local.retrieve(var='tprate', units='mm/day')
+        gb_local.compute_climatology(var='tprate')
+        assert gb_local.climatology['tprate'].attrs.get('units') == 'mm/day'
