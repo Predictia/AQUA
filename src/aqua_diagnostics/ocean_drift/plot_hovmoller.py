470ab9c4892c5ec0a93f3925267f66af06e7f263
ghosh97 <supriyo.ghosh@bsc.es>
2025-06-10 15:03:08 +0200

    add PlotHovmoller class for Hovmoller plot generation and configuration

---
+++ src/aqua_diagnostics/ocean_drift/plot_hovmoller.py
@@ -0,0 +1,123 @@
+import xarray as xr
+from aqua.logger import log_configure
+from .multiple_hovmoller import plot_multi_hovmoller
+from aqua.diagnostics.core import OutputSaver
+
+xr.set_options(keep_attrs=True)
+
+class PlotHovmoller:
+    def __init__(self,
+                 data: list [xr.Dataset],
+                 diagnostic: str = "ocean_drift",
+                 outputdir: str = ".",
+                 rebuild: bool = True,
+                 loglevel: str = "WARNING"):
+        self.data = data
+        self.loglevel = loglevel
+        self.logger = log_configure(self.loglevel, "PlotHovmoller")
+        self.diagnostic = diagnostic
+        self.vars = list(self.data[0].data_vars)
+        self.logger.debug("Variables in data: %s", self.vars)
+        self.catalog = self.data[0][self.vars[0]].AQUA_catalog
+        self.model = self.data[0][self.vars[0]].AQUA_model
+        self.exp = self.data[0][self.vars[0]].AQUA_exp
+        self.region = self.data[0].AQUA_region
+        self.outputsaver = OutputSaver(
+            diagnostic=self.diagnostic,
+            catalog=self.catalog,
+            model=self.model,
+            exp=self.exp, 
+            outdir=outputdir, 
+            rebuild=rebuild, 
+            loglevel=self.loglevel)
+            
+    
+    def plot_hovmoller(self):
+        self.set_suptitle()
+        self.set_title()
+        self.set_description()
+        self.set_data_type()
+        self.set_vmax_vmin()
+        self.logger.debug("Plotting Hovmoller for variables: %s", self.vars)
+        fig = plot_multi_hovmoller(
+            maps=self.data,
+            variables=self.vars,
+            loglevel=self.loglevel,
+            title=self.suptitle,
+            titles=self.title_list,
+            vars=self.vars,
+            vmax=self.vmax,
+            
+            
+        )
+        self.outputsaver.save_pdf(fig, diagnostic_product="Hovmoller", metadata= self.description)
+
+    def set_suptitle(self):
+        """
+        Set the title for the Hovmoller plot.
+        """
+        self.logger.debug("Setting title")
+        self.suptitle = f"{self.catalog} {self.model} {self.exp} {self.region}"
+
+    def set_title(self):
+        """
+        Set the title for the Hovmoller plot.
+        This method can be extended to set specific titles based on the data.
+        """
+        self.logger.debug("Setting suptitle")
+        self.title_list = []
+        for var in self.vars:
+            title = f"{var} ({self.data[0][var].attrs.get('units')})"
+            self.title_list.append(title)
+        
+    def set_description(self):
+        self.description = {}
+        self.description['description'] = {f'{self.region} {self.diagnostic} {self.catalog} {self.model} {self.exp}'}
+    
+    def set_vmax_vmin(self):
+        """
+        Set the vmax and vmin for the Hovmoller plot.
+        This method can be extended to set specific vmax and vmin values.
+        """
+        self.logger.debug("Setting vmax and vmin")
+        hovmoller_plot_dic = {
+            'thetao' :
+                {
+                    'full': {'vmax': -10, 'vmin': 30 },
+                    'anom_t0': {'vmax': -5, 'vmin': 5, 'cbar': 'coolwarm'},
+                    'std_anom_t0': {'vmax': -1, 'vmin': 1, 'cbar': 'coolwarm'},
+                    
+                    'anom_tmean': {'vmax': 0.0, 'vmin': 0.0, 'cbar': 'coolwarm'},
+                    'std_anom_tmean': {'vmax': -1, 'vmin': 1, 'cbar': 'coolwarm'},
+                },
+            'so' :
+                {
+                    'full': {'vmax': 0.0, 'vmin': 0.0, 'cbar': 'coolwarm'},
+                    'anom_t0': {'vmax': 0.0, 'vmin': 0.0, 'cbar': 'coolwarm'},
+                    'std_anom_t0': {'vmax': -1, 'vmin': 1, 'cbar': 'coolwarm'},
+                    
+                    'anom_tmean': {'vmax': 0.0, 'vmin': 0.0, 'cbar': 'coolwarm'},
+                    'std_anom_tmean': {'vmax': -1, 'vmin': 1, 'cbar': 'coolwarm'},
+                }
+        }
+        self.vmax = []
+        self.vmin = []
+        self.cmap = []
+        for var in self.vars:
+            for type in self.data_type:
+                self.vmax.append(hovmoller_plot_dic[var][type]['vmax'])
+                self.vmin.append(hovmoller_plot_dic[var][type]['vmin'])
+                self.cmap.append(hovmoller_plot_dic[var][type].get('cbar', 'jet'))
+                
+        
+    def set_data_type(self):
+        """
+        Set the texts for the Hovmoller plot.
+        This method can be extended to set specific texts.
+        """
+        self.logger.debug("Setting texts")
+        self.data_type = []
+        for data in self.data:
+            type = data.attrs.get('AQUA_ocean_drift_type', 'NA')
+            self.data_type.append(type)
+    
