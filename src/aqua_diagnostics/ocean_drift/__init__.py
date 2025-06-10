d2a1436ca0dc4247b7d96c4193084402da96f29a
ghosh97 <supriyo.ghosh@bsc.es>
2025-06-10 15:03:24 +0200

    update __init__.py to include PlotHovmoller and update __all__ exports

--- src/aqua_diagnostics/ocean_drift/__init__.py
+++ src/aqua_diagnostics/ocean_drift/__init__.py
@@ -1,3 +1,5 @@
 from .hovmoller import Hovmoller
+from .multiple_hovmoller import plot_multi_hovmoller
+from .plot_hovmoller import PlotHovmoller
 
-__all__ = ["Hovmoller"]
+__all__ = ["Hovmoller", "HovmollerPlot", "plot_multi_hovmoller"]
