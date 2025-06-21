e2aed56987b62b4eef0d127270a25fa200e2fdc9
silviacaprioli <silvietta.caprioli@gmail.com>
2025-06-21 17:31:20 +0200

    include boxplot in init

--- src/aqua_diagnostics/__init__.py
+++ src/aqua_diagnostics/__init__.py
@@ -2,7 +2,7 @@ from .teleconnections import NAO, ENSO, MJO
 from .teleconnections import PlotNAO, PlotENSO, PlotMJO
 from .timeseries import Gregory, SeasonalCycles, Timeseries
 from .global_biases import GlobalBiases, PlotGlobalBiases 
-from .radiation import Radiation
+from .boxplots import Boxplots, PlotBoxplots
 from .ensemble import EnsembleTimeseries, EnsembleLatLon, EnsembleZonal
 from .ecmean import PerformanceIndices, GlobalMean
 
@@ -10,6 +10,6 @@ __all__ = ["NAO", "ENSO", "MJO",
            "PlotNAO", "PlotENSO", "PlotMJO",
            "Gregory", "SeasonalCycles", "Timeseries",
            "GlobalBiases", "PlotGlobalBiases",
-           "Radiation",
+           "Boxplots", "PlotBoxplots",
            "EnsembleTimeseries", "EnsembleLatLon", "EnsembleZonal",
            "GlobalMean", "PerformanceIndices"]
