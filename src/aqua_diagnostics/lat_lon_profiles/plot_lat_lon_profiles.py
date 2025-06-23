1dcd085940daf27fc3fd69a70d395cff82d6c4b3
mcadau <marco.cadau@polito.it>
2025-06-23 20:45:55 +0300

    PlotLatLonTimeseries draft update

--- src/aqua_diagnostics/lat_lon_profiles/plot_lat_lon_profiles.py
+++ src/aqua_diagnostics/lat_lon_profiles/plot_lat_lon_profiles.py
@@ -1,17 +1,66 @@
-class PlotLatLonProfiles:
+import xarray as xr
+
+from aqua.logger import log_configure
+from aqua.util import to_list
+from aqua.graphics import plot_lat_lon_profiles
+from aqua.diagnostics.core import OutputSaver
+
+class PlotLatLonProfiles():
     
-    def __init__(self, loglevel: str = 'WARNING'):
-    
-        # Data info initalized as empty
+    def __init__(self, hourly_data=None, daily_data=None,
+                 monthly_data=None, annual_data=None,
+                 std_hourly_data=None, std_daily_data=None,
+                 std_monthly_data=None, std_annual_data=None,
+                 loglevel: str = 'WARNING'):
+        """
+        Initialize the PlotTimeseries class.
+        This class is used to plot time series data previously processed
+        by the Timeseries class.
+
+        Any subset of frequency can be provided, however the order and length
+        of the list of data arrays must be the same for each frequency.
+
+        Note: Currently, only monthly and annual data are supported.
+
+        Args:
+            hourly_data (list): List of hourly data arrays.
+            daily_data (list): List of daily data arrays.
+            monthly_data (list): List of monthly data arrays.
+            annual_data (list): List of annual data arrays.
+            std_hourly_data (xr.DataArray): Standard deviation hourly data array.
+            std_daily_data (xr.DataArray): Standard deviation daily data array.
+            std_monthly_data (xr.DataArray): Standard deviation monthly data array.
+            std_annual_data (xr.DataArray): Standard deviation annual data array.
+            loglevel (str): Logging level. Default is 'WARNING'.
+        """
         self.loglevel = loglevel
-        self.catalogs = None
-        self.models = None
-        self.exps = None
-        self.ref_catalogs = None
-        self.ref_models = None
-        self.ref_exps = None
-        self.std_startdate = None
-        self.std_enddate = None
+        self.logger = log_configure(log_level=loglevel, log_name='PlotLatLonProfiles')
+
+        # TODO: support hourly and daily data
+        for data in [hourly_data, daily_data,
+                     std_hourly_data, std_daily_data]:
+            if data is not None:
+                self.logger.warning('Hourly and daily data are not yet supported, they will be ignored')
+
+        self.monthly_data = to_list(monthly_data)
+        self.annual_data = to_list(annual_data)
+
+        # self.std_hourly_data = to_list(std_hourly_data)
+        # self.std_daily_data = to_list(std_daily_data)
+        self.std_monthly_data = (
+            std_monthly_data if (std_monthly_data is not None and isinstance(std_monthly_data, xr.DataArray))
+            else (std_monthly_data[0] if std_monthly_data is not None else None)
+        )
+
+        self.std_annual_data = (
+            std_annual_data if (std_annual_data is not None and isinstance(std_annual_data, xr.DataArray))
+            else (std_annual_data[0] if std_annual_data is not None else None)
+        )
+
+        self.len_data = self._check_data_length()
+
+        # Filling them
+        self.get_data_info()
 
     def set_data_labels(self):
         """
@@ -28,7 +77,34 @@ class PlotLatLonProfiles:
 
         self.logger.debug('Data labels: %s', data_labels)
         return data_labels
+        
+    def set_description(self, region: str = None, diagnostic: str = None):
+        """
+        Set the caption for the plot.
+        The caption is extracted from the data arrays attributes.
+        The caption is stored as 'Description' in the metadata dictionary.
 
+        Args:
+            region (str): Region to be used in the caption.
+            diagnostic (str): Diagnostic name to be used in the caption.
+
+        Returns:
+            description (str): Caption for the plot.
+        """
+
+        description = f'{diagnostic} '
+        if region is not None:
+            description += f'for region {region} '
+
+        for i in range(self.len_data):
+            description += f'for {self.catalogs[i]} {self.models[i]} {self.exps[i]} '
+
+        if self.std_startdate is not None and self.std_enddate is not None:
+            description += f'with standard deviation from {self.std_startdate} to {self.std_enddate} '
+
+        self.logger.debug('Description: %s', description)
+        return description
+    
     def set_title(self, region: str = None, var: str = None, units: str = None, diagnostic: str = None):
         """
         Set the title for the plot.
@@ -57,82 +133,7 @@ class PlotLatLonProfiles:
 
         self.logger.debug('Title: %s', title)
         return title
-    
-    def set_description(self, region: str = None, diagnostic: str = None):
-        """
-        Set the caption for the plot.
-        The caption is extracted from the data arrays attributes and the
-        reference data arrays attributes.
-        The caption is stored as 'Description' in the metadata dictionary.
 
-        Args:
-            region (str): Region to be used in the caption.
-            diagnostic (str): Diagnostic name to be used in the caption.
-
-        Returns:
-            description (str): Caption for the plot.
-        """
-
-        description = f'{diagnostic} '
-        if region is not None:
-            description += f'for region {region} '
-
-        for i in range(self.len_data):
-            description += f'for {self.catalogs[i]} {self.models[i]} {self.exps[i]} '
-
-        for i in range(self.len_ref):
-            if self.ref_models[i] == 'ERA5':
-                description += f'with reference {self.ref_models[i]} '
-            else:
-                description += f'with reference {self.ref_models[i]} {self.ref_exps[i]} '
-
-        if self.std_startdate is not None and self.std_enddate is not None:
-            description += f'with standard deviation from {self.std_startdate} to {self.std_enddate} '
-
-        self.logger.debug('Description: %s', description)
-        return description
-    
-    def save_plot(self, fig, var: str = None, description: str = None, region: str = None, rebuild: bool = True,
-                    outputdir: str = './', dpi: int = 300, format: str = 'png', diagnostic: str = None):
-        """
-        Save the plot to a file.
-
-        Args:
-            fig (matplotlib.figure.Figure): Figure object.
-            var (str): Variable name to be used in the title and description.
-            description (str): Description of the plot.
-            region (str): Region to be used in the title and description.
-            rebuild (bool): If True, rebuild the plot even if it already exists.
-            outputdir (str): Output directory to save the plot.
-            dpi (int): Dots per inch for the plot.
-            format (str): Format of the plot ('png' or 'pdf'). Default is 'png'.
-            diagnostic (str): Diagnostic name to be used in the filename as diagnostic_product.
-        """
-        outputsaver = OutputSaver(diagnostic='timeseries', 
-                                catalog=self.catalogs[0],
-                                model=self.models[0],
-                                exp=self.exps[0],
-                                outdir=outputdir,
-                                rebuild=rebuild,
-                                loglevel=self.loglevel)
-
-        metadata = {"Description": description, "dpi": dpi }
-        extra_keys = {'diagnostic_product': diagnostic}
-
-        if var is not None:
-            extra_keys.update({'var': var})
-        if region is not None:
-            region = region.replace(' ', '').lower() if region is not None else None
-            extra_keys.update({'region': region})
-
-        if format == 'png':
-            outputsaver.save_png(fig, diagnostic_product=diagnostic, extra_keys=extra_keys, metadata=metadata)
-        elif format == 'pdf':
-            outputsaver.save_pdf(fig, diagnostic_product=diagnostic, extra_keys=extra_keys, metadata=metadata)
-        else:
-            raise ValueError(f'Format {format} not supported. Use png or pdf.')
-        
-        
     def run(self, var: str, units: str = None, region: str = None, outputdir: str = './',
             rebuild: bool = True, dpi: int = 300, format: str = 'png'):
         """
@@ -148,16 +149,15 @@ class PlotLatLonProfiles:
             format (str): Format of the plot ('png' or 'pdf'). Default is 'png'.
         """
 
-        self.logger.info('Running PlotTimeseries')
+        self.logger.info('Running PlotLatLonProfiles')
         data_label = self.set_data_labels()
-        ref_label = self.set_ref_label()
         description = self.set_description(region=region)
         title = self.set_title(region=region, var=var, units=units)
-        fig, _ = self.plot_lat_lon_profiles(data_labels=data_label, ref_label=ref_label, title=title)
+        fig, _ = self.plot_lat_lon_profiles(data_labels=data_label, title=title)
         region_short = region.replace(' ', '').lower() if region is not None else None
         self.save_plot(fig, var=var, description=description, region=region_short, rebuild=rebuild,
                        outputdir=outputdir, dpi=dpi, format=format)
-        self.logger.info('PlotTimeseries completed successfully')
+        self.logger.info('PlotLatLonProfiles completed successfully')
 
     def get_data_info(self):
         """
@@ -182,14 +182,8 @@ class PlotLatLonProfiles:
         self.logger.debug(f'Models: {self.models}')
         self.logger.debug(f'Experiments: {self.exps}')
 
-        # TODO: support ref list
-        for ref in [self.ref_monthly_data, self.ref_annual_data]:
-            if ref is not None:
-                self.ref_catalogs = ref.AQUA_catalog
-                self.ref_models = ref.AQUA_model
-                self.ref_exps = ref.AQUA_exp
-                break
-        self.logger.debug(f'Reference: {self.ref_catalogs} {self.ref_models} {self.ref_exps}')
+        self.std_startdate = None
+        self.std_enddate = None
 
         for std in [self.std_monthly_data, self.std_annual_data]:
             if std is not None:
@@ -198,63 +192,29 @@ class PlotLatLonProfiles:
                 break
         self.logger.debug(f'Standard deviation dates: {self.std_startdate} - {self.std_enddate}')
 
-    def set_title(self, region: str = None, var: str = None, units: str = None):
+    def plot_lat_lon_profiles(self, data_labels=None, title=None):
         """
-        Set the title for the plot.
-
-        Args:
-            region (str): Region to be used in the title.
-            var (str): Variable name to be used in the title.
-            units (str): Units of the variable to be used in the title.
-
-        Returns:
-            title (str): Title for the plot.
-        """
-        title = super().set_title(region=region, var=var, units=units, diagnostic='Time series')
-        return title
-
-    def set_description(self, region: str = None):
-        """
-        Set the caption for the plot.
-        The caption is extracted from the data arrays attributes and the
-        reference data arrays attributes.
-        The caption is stored as 'Description' in the metadata dictionary.
-
-        Args:
-            region (str): Region to be used in the caption.
-
-        Returns:
-            description (str): Caption for the plot.
-        """
-        description = super().set_description(region=region, diagnostic='Time series')
-        return description
-    
-    def plot_timeseries(self, data_labels=None, ref_label=None, title=None):
-        """
-        Plot the time series data.
+        Plot the lat lon profiles data.
 
         Args:
             data_labels (list): List of data labels.
-            ref_label (str): Reference label.
             title (str): Title of the plot.
 
         Returns:
             fig (matplotlib.figure.Figure): Figure object.
             ax (matplotlib.axes.Axes): Axes object.
         """
-        fig, ax = plot_timeseries(monthly_data=self.monthly_data,
-                                  ref_monthly_data=self.ref_monthly_data,
-                                  std_monthly_data=self.std_monthly_data,
-                                  annual_data=self.annual_data,
-                                  ref_annual_data=self.ref_annual_data,
-                                  std_annual_data=self.std_annual_data,
-                                  data_labels=data_labels, ref_label=ref_label,
-                                  title=title, return_fig=True, loglevel=self.loglevel)
+        fig, ax = plot_lat_lon_profiles(mean_type='zonal',
+                                        monthly_data=self.monthly_data, 
+                                        annual_data=self.annual_data,
+                                        #fig=fig, ax=ax,
+                                        data_labels= data_labels,
+                                        )
 
         return fig, ax
 
-    def save_plot(self, fig, var: str, description: str = None, region: str = None, rebuild: bool = True,
-                  outputdir: str = './', dpi: int = 300, format: str = 'png'):
+    def save_plot(self, fig, var: str = None, description: str = None, region: str = None, rebuild: bool = True,
+                  outputdir: str = './', dpi: int = 300, format: str = 'png', diagnostic: str = None):
         """
         Save the plot to a file.
 
@@ -267,23 +227,41 @@ class PlotLatLonProfiles:
             outputdir (str): Output directory to save the plot.
             dpi (int): Dots per inch for the plot.
             format (str): Format of the plot ('png' or 'pdf'). Default is 'png'.
+            diagnostic (str): Diagnostic name to be used in the filename as diagnostic_product.
         """
-        super().save_plot(fig=fig, var=var, description=description,
-                          region=region, rebuild=rebuild,
-                          outputdir=outputdir, dpi=dpi, format=format, diagnostic='timeseries')
+        outputsaver = OutputSaver(diagnostic='timeseries', 
+                                  catalog=self.catalogs[0],
+                                  model=self.models[0],
+                                  exp=self.exps[0],
+                                  outdir=outputdir,
+                                  rebuild=rebuild,
+                                  loglevel=self.loglevel)
+
+        metadata = {"Description": description, "dpi": dpi }
+        extra_keys = {'diagnostic_product': diagnostic}
+
+        if var is not None:
+            extra_keys.update({'var': var})
+        if region is not None:
+            region = region.replace(' ', '').lower() if region is not None else None
+            extra_keys.update({'region': region})
+
+        if format == 'png':
+            outputsaver.save_png(fig, diagnostic_product=diagnostic, extra_keys=extra_keys, metadata=metadata)
+        elif format == 'pdf':
+            outputsaver.save_pdf(fig, diagnostic_product=diagnostic, extra_keys=extra_keys, metadata=metadata)
+        else:
+            raise ValueError(f'Format {format} not supported. Use png or pdf.')
 
     def _check_data_length(self):
         """
         Check if all data arrays have the same length.
-        Does the same for the reference data.
         If not, raise a ValueError.
 
         Return:
             data_length (int): Length of the data arrays.
-            ref_length (int): Length of the reference data arrays.
         """
         data_length = 0
-        ref_length = 0
 
         if self.monthly_data and self.annual_data:
             if len(self.monthly_data) != len(self.annual_data):
@@ -291,20 +269,4 @@ class PlotLatLonProfiles:
             else:
                 data_length = len(self.monthly_data)
 
-        if self.ref_monthly_data is not None or self.ref_annual_data is not None:
-            ref_length = 1
-        # # TODO: uncomment when support to list is implemented
-        # if self.ref_monthly_data and self.ref_annual_data:
-        #     if len(self.ref_monthly_data) != len(self.ref_annual_data):
-        #         raise ValueError('Reference monthly and annual data list must have the same length')
-        #     else:
-        #         ref_length = len(self.ref_monthly_data)
-
-        # if self.std_monthly_data and self.std_annual_data:
-        #     if len(self.std_monthly_data) != len(self.std_annual_data):
-        #         raise ValueError('Standard deviation monthly and annual data list must have the same length')
-        #     else:
-        #         if len(self.std_monthly_data) != ref_length:
-        #             raise ValueError('Standard deviation monthly and annual data list must have the same length as reference data')
-
-        return data_length, ref_length
+        return data_length
