230e908271e29c5c2ada8fdffcf630981d3bb2c4
Matteo Nurisso <m.nurisso@isac.cnr.it>
2025-06-26 17:14:03 +0300

    leaving only the two classes as exposed

--- src/aqua_diagnostics/ocean_drift/__init__.py
+++ src/aqua_diagnostics/ocean_drift/__init__.py
@@ -1,5 +1,4 @@
 from .hovmoller import Hovmoller
-from .multiple_hovmoller import plot_multi_hovmoller
 from .plot_hovmoller import PlotHovmoller
 
-__all__ = ["Hovmoller", "HovmollerPlot", "plot_multi_hovmoller"]
+__all__ = ["Hovmoller", "PlotHovmoller"]
