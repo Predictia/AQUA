d88b5bd16f0986703c007e9ea1376c003f5b370d
mcadau <marco.cadau@polito.it>
2025-07-05 18:10:50 +0300

    delete dixtincion between different frequencies keeping only "data" input in PlotLatLonProfiles

--- src/aqua_diagnostics/lat_lon_profiles/plot_lat_lon_profiles.py
+++ src/aqua_diagnostics/lat_lon_profiles/plot_lat_lon_profiles.py
@@ -9,73 +9,36 @@ class PlotLatLonProfiles():
     """
     Class for plotting Lat-Lon Profiles diagnostics.
     This class provides methods to set data labels, description, title,
-    and to plot the data. It also handles the initialization of the class
-    with various data arrays for different frequencies (hourly, daily,
-    monthly, annual, seasonal annual) and their standard deviations.
+    and to plot the data. It handles data arrays regardless of their original
+    temporal frequency, as temporal averaging is handled upstream.
     """
-    def __init__(self, hourly_data=None, daily_data=None,
-             monthly_data=None, annual_data=None,
-             seasonal_annual_data=None,
-             seasonal_annual_ref_data=None,
-             ref_hourly_data=None, ref_daily_data=None,
-             ref_monthly_data=None, ref_annual_data=None,
-             std_hourly_data=None, std_daily_data=None,
-             std_monthly_data=None, std_annual_data=None,
-             loglevel: str = 'WARNING'):
+    def __init__(self, data=None, ref_data=None,
+                seasonal_annual_data=None,
+                seasonal_annual_ref_data=None,
+                std_data=None,
+                loglevel: str = 'WARNING'):
         """
         Initialise the PlotLatLonProfiles class.
         This class is used to plot lat lon profiles data previously processed
         by the LatLonProfiles class.
 
-        Any subset of frequency can be provided, however the order and length
-        of the list of data arrays must be the same for each frequency.
-
-        Note: Currently, only monthly and annual data are supported.
-        Additionally, only one reference data array is supported for each frequency.
-
         Args:
-            hourly_data (list): List of hourly data arrays.
-            daily_data (list): List of daily data arrays.
-            monthly_data (list): List of monthly data arrays.
-            annual_data (list): List of annual data arrays.
+            data (list): List of data arrays (any temporal frequency).
+            ref_data (xr.DataArray): Reference data array.
             seasonal_annual_data (list): List of seasonal and annual means [DJF, MAM, JJA, SON, Annual].
-            ref_hourly_data (xr.DataArray): Reference hourly data array.
-            ref_daily_data (xr.DataArray): Reference daily data array.
-            ref_monthly_data (xr.DataArray): Reference monthly data array.
-            ref_annual_data (xr.DataArray): Reference annual data array.
-            std_hourly_data (xr.DataArray): Standard deviation hourly data array.
-            std_daily_data (xr.DataArray): Standard deviation daily data array.
-            std_monthly_data (xr.DataArray): Standard deviation monthly data array.
-            std_annual_data (xr.DataArray): Standard deviation annual data array.
+            seasonal_annual_ref_data (list): Reference seasonal and annual data.
+            std_data (xr.DataArray): Standard deviation data array.
             loglevel (str): Logging level. Default is 'WARNING'.
         """
         self.loglevel = loglevel
         self.logger = log_configure(loglevel, 'PlotLatLonProfiles')
 
-        # TODO: support hourly and daily data
-        for data in [hourly_data, daily_data, ref_hourly_data, ref_daily_data,
-                    std_hourly_data, std_daily_data]:
-            if data is not None:
-                raise NotImplementedError("Hourly and daily data are not supported yet.")
-
-        self.monthly_data = to_list(monthly_data) if monthly_data is not None else []
-        self.annual_data = to_list(annual_data) if annual_data is not None else []
+        # Store the simplified data structure
+        self.data = to_list(data) if data is not None else []
+        self.ref_data = ref_data
         self.seasonal_annual_data = seasonal_annual_data  
         self.seasonal_annual_ref_data = seasonal_annual_ref_data 
-
-        # TODO: support ref list
-        self.ref_monthly_data = ref_monthly_data if isinstance(ref_monthly_data, xr.DataArray) else (ref_monthly_data[0] if ref_monthly_data is not None else None)
-        self.ref_annual_data = ref_annual_data if isinstance(ref_annual_data, xr.DataArray) else (ref_annual_data[0] if ref_annual_data is not None else None)
-
-        self.std_monthly_data = (
-            std_monthly_data if (std_monthly_data is not None and isinstance(std_monthly_data, xr.DataArray))
-            else (std_monthly_data[0] if std_monthly_data is not None else None)
-        )
-
-        self.std_annual_data = (
-            std_annual_data if (std_annual_data is not None and isinstance(std_annual_data, xr.DataArray))
-            else (std_annual_data[0] if std_annual_data is not None else None)
-        )
+        self.std_data = std_data
 
         self.len_data, self.len_ref = self._check_data_length()
         self.get_data_info()
@@ -112,12 +75,9 @@ class PlotLatLonProfiles():
         """
         ref_label = None
         
-        # Check for reference data in order of priority: monthly, annual
-        ref_data = self.ref_monthly_data if self.ref_monthly_data is not None else self.ref_annual_data
-        
-        if ref_data is not None:
-            model = ref_data.attrs.get('AQUA_model', 'Unknown')
-            exp = ref_data.attrs.get('AQUA_exp', 'Unknown')
+        if self.ref_data is not None:
+            model = self.ref_data.attrs.get('AQUA_model', 'Unknown')
+            exp = self.ref_data.attrs.get('AQUA_exp', 'Unknown')
             ref_label = f'{model} {exp}'
         
         self.logger.debug('Reference label: %s', ref_label)
@@ -185,57 +145,51 @@ class PlotLatLonProfiles():
         - std_startdate
         - std_enddate
         """
-        # Check monthly and annual data first
-        for data in [self.monthly_data, self.annual_data]:
-            if data is not None and len(data) > 0:
-                if len(data) == 1:
-                    if hasattr(data[0], 'AQUA_catalog'):
-                        self.catalogs = [data[0].AQUA_catalog]
-                        self.models = [data[0].AQUA_model]
-                        self.exps = [data[0].AQUA_exp]
-                    else:
-                        self.catalogs = []
-                        self.models = []
-                        self.exps = []
+        # Check main data first
+        if self.data and len(self.data) > 0:
+            if len(self.data) == 1:
+                if hasattr(self.data[0], 'AQUA_catalog'):
+                    self.catalogs = [self.data[0].AQUA_catalog]
+                    self.models = [self.data[0].AQUA_model]
+                    self.exps = [self.data[0].AQUA_exp]
                 else:
-                    self.catalogs = [d.AQUA_catalog for d in data]
-                    self.models = [d.AQUA_model for d in data]
-                    self.exps = [d.AQUA_exp for d in data]
-                break
-        else:
-            # If no monthly or annual data, check seasonal_annual_data
-            if self.seasonal_annual_data is not None and len(self.seasonal_annual_data) > 0:
-                # Check if seasonal_annual_data is a simple list of DataArrays
-                # or a nested list (multi-variable case)
-                first_item = self.seasonal_annual_data[0]
-                
-                if isinstance(first_item, list):
-                    # Multi-variable case: [[DJF_data1, DJF_data2, ...], [MAM_data1, ...], ...]
-                    # Take metadata from the first DataArray of the first season
-                    if len(first_item) > 0 and hasattr(first_item[0], 'AQUA_catalog'):
-                        self.catalogs = [first_item[0].AQUA_catalog]
-                        self.models = [first_item[0].AQUA_model]
-                        self.exps = [first_item[0].AQUA_exp]
-                    else:
-                        self.catalogs = []
-                        self.models = []
-                        self.exps = []
-                else:
-                    # Simple case: [DJF_data, MAM_data, JJA_data, SON_data, Annual_data]
-                    if hasattr(first_item, 'AQUA_catalog'):
-                        self.catalogs = [first_item.AQUA_catalog]
-                        self.models = [first_item.AQUA_model]
-                        self.exps = [first_item.AQUA_exp]
-                    else:
-                        self.catalogs = []
-                        self.models = []
-                        self.exps = []
+                    self.catalogs = []
+                    self.models = []
+                    self.exps = []
             else:
-                # Fallback if no data is available
-                self.logger.warning('No data available for metadata extraction, using empty lists')
-                self.catalogs = []
-                self.models = []
-                self.exps = []
+                self.catalogs = [d.AQUA_catalog for d in self.data]
+                self.models = [d.AQUA_model for d in self.data]
+                self.exps = [d.AQUA_exp for d in self.data]
+        elif self.seasonal_annual_data is not None and len(self.seasonal_annual_data) > 0:
+            # If no main data, check seasonal_annual_data
+            first_item = self.seasonal_annual_data[0]
+            
+            if isinstance(first_item, list):
+                # Multi-variable case: [[DJF_data1, DJF_data2, ...], [MAM_data1, ...], ...]
+                if len(first_item) > 0 and hasattr(first_item[0], 'AQUA_catalog'):
+                    self.catalogs = [first_item[0].AQUA_catalog]
+                    self.models = [first_item[0].AQUA_model]
+                    self.exps = [first_item[0].AQUA_exp]
+                else:
+                    self.catalogs = []
+                    self.models = []
+                    self.exps = []
+            else:
+                # Simple case: [DJF_data, MAM_data, JJA_data, SON_data, Annual_data]
+                if hasattr(first_item, 'AQUA_catalog'):
+                    self.catalogs = [first_item.AQUA_catalog]
+                    self.models = [first_item.AQUA_model]
+                    self.exps = [first_item.AQUA_exp]
+                else:
+                    self.catalogs = []
+                    self.models = []
+                    self.exps = []
+        else:
+            # Fallback if no data is available
+            self.logger.warning('No data available for metadata extraction, using empty lists')
+            self.catalogs = []
+            self.models = []
+            self.exps = []
         
         self.logger.debug(f'Catalogs: {self.catalogs}')
         self.logger.debug(f'Models: {self.models}')
@@ -244,11 +198,10 @@ class PlotLatLonProfiles():
         self.std_startdate = None
         self.std_enddate = None
 
-        for std in [self.std_monthly_data, self.std_annual_data]:
-            if std is not None:
-                self.std_startdate = std.std_startdate if std.std_startdate is not None else None
-                self.std_enddate = std.std_enddate if std.std_enddate is not None else None
-                break
+        if self.std_data is not None:
+            self.std_startdate = self.std_data.std_startdate if hasattr(self.std_data, 'std_startdate') else None
+            self.std_enddate = self.std_data.std_enddate if hasattr(self.std_data, 'std_enddate') else None
+        
         self.logger.debug(f'Standard deviation dates: {self.std_startdate} - {self.std_enddate}')
 
     def plot_lat_lon_profiles(self, data_labels=None, ref_label=None, title=None):
@@ -263,37 +216,27 @@ class PlotLatLonProfiles():
         Returns:
             tuple: Matplotlib figure and axes objects.
         """
-        # Get the appropriate data (monthly has priority over annual)
-        if self.monthly_data:
-            data_to_plot = self.monthly_data
-            ref_data_to_plot = self.ref_monthly_data
-        elif self.annual_data:
-            data_to_plot = self.annual_data
-            ref_data_to_plot = self.ref_annual_data
-        else:
+        # Use the main data
+        if not self.data or len(self.data) == 0:
             raise ValueError("No data available for plotting")
         
-        # Validate data before plotting
-        if not data_to_plot or len(data_to_plot) == 0:
-            raise ValueError("No valid data arrays found for plotting")
-        
         # Log data information for debugging
-        self.logger.debug(f"Data to plot: {len(data_to_plot)} arrays")
-        for i, d in enumerate(data_to_plot):
+        self.logger.debug(f"Data to plot: {len(self.data)} arrays")
+        for i, d in enumerate(self.data):
             if d is not None:
                 self.logger.debug(f"  Data {i}: shape={d.shape}, dims={d.dims}")
             else:
                 self.logger.debug(f"  Data {i}: None")
         
-        if ref_data_to_plot is not None:
-            self.logger.debug(f"Ref data: shape={ref_data_to_plot.shape}, dims={ref_data_to_plot.dims}")
+        if self.ref_data is not None:
+            self.logger.debug(f"Ref data: shape={self.ref_data.shape}, dims={self.ref_data.dims}")
         else:
             self.logger.debug("Ref data: None")
         
         # Call the graphics function with reference data
         return plot_lat_lon_profiles(
-            data=data_to_plot,
-            ref_data=ref_data_to_plot,
+            data=self.data,
+            ref_data=self.ref_data,
             data_labels=data_labels,
             ref_label=ref_label,
             title=title,
@@ -312,20 +255,14 @@ class PlotLatLonProfiles():
         Returns:
             tuple: Matplotlib figure and axes objects.
         """
-        # Get the appropriate data (monthly has priority over annual)
-        if self.monthly_data:
-            data_to_plot = self.monthly_data
-            ref_data_to_plot = self.ref_monthly_data
-        elif self.annual_data:
-            data_to_plot = self.annual_data
-            ref_data_to_plot = self.ref_annual_data
-        else:
+        # Use the main data
+        if not self.data or len(self.data) == 0:
             raise ValueError("No data available for plotting")
         
         # Call the graphics function with reference data
         return plot_lat_lon_profiles(
-            data=data_to_plot,
-            ref_data=ref_data_to_plot,
+            data=self.data,
+            ref_data=self.ref_data,
             data_labels=data_labels,
             ref_label=ref_label,
             title=title,
@@ -394,17 +331,8 @@ class PlotLatLonProfiles():
         Returns:
             tuple: (length of data arrays, length of reference data)
         """
-        len_data = 0
-        len_ref = 0
-        
-        # Check main data
-        for data_list in [self.monthly_data, self.annual_data]:
-            if data_list:
-                len_data = max(len_data, len(data_list))
-        
-        # Check reference data
-        if self.ref_monthly_data is not None or self.ref_annual_data is not None:
-            len_ref = 1
+        len_data = len(self.data) if self.data else 0
+        len_ref = 1 if self.ref_data is not None else 0
         
         self.logger.debug(f'Data length: {len_data}, Reference length: {len_ref}')
         return len_data, len_ref
@@ -413,6 +341,14 @@ class PlotLatLonProfiles():
         """
         Set the title for the plot.
         Specialized for Lat-Lon Profiles diagnostic.
+
+        Args:
+            region (str): Region to be used in the title.
+            var (str): Variable name to be used in the title.
+            units (str): Units of the variable to be used in the title.
+            
+        Returns:
+            title (str): Title for the plot.
         """
         title = 'Lat-Lon Profiles '
         if var is not None:
