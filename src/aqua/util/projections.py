d2f485d006439213041d6fb6ffafe457b1c37aa0
Emanuele Tovazzi <tovaz92@gmail.com>
2025-07-09 15:23:37 +0300

    Add: new functions

--- src/aqua/util/projections.py
+++ src/aqua/util/projections.py
@@ -2,12 +2,18 @@ import cartopy.crs as ccrs
 
 projections_maps = {
     "plate_carree": ccrs.PlateCarree,
+    "lambert_cylindrical": ccrs.LambertCylindrical,
+    "lambert_cylindrical_equal_area": ccrs.LambertCylindricalEqualArea,
     "mercator": ccrs.Mercator,
+    "nearside": ccrs.NearsidePerspective,
+    "geostationary": ccrs.Geostationary,
+    "interrupt_gh": ccrs.InterruptedGoodeHomolosine,
     "robinson": ccrs.Robinson,
     "mollweide": ccrs.Mollweide,
     "orthographic": ccrs.Orthographic,
     "lambert_conformal": ccrs.LambertConformal,
     "albers_equal_area": ccrs.AlbersEqualArea,
+    "lambert_equal_area": ccrs.LambertAzimuthalEqualArea,
     "nh_polar_stereo": ccrs.NorthPolarStereo,
     "sh_polar_stereo": ccrs.SouthPolarStereo,
     "eckert_iv": ccrs.EckertIV,
@@ -16,11 +22,14 @@ projections_maps = {
     "azimuthal_equidistant": ccrs.AzimuthalEquidistant,
     "gnomonic": ccrs.Gnomonic,
     "sinusoidal": ccrs.Sinusoidal,
-}
+    "stereographic": ccrs.Stereographic
+    }
 
 def get_projection(projname: str, **kwargs) -> ccrs.Projection:
     """
-    Return a Cartopy projection by name.
+    Return a Cartopy projection by name. Refer to the Cartopy 
+    documentation (https://scitools.org.uk/cartopy/) to review the 
+    supported keyword arguments for each projection class.
 
     Args:
         projname (str): Name of the projection.
