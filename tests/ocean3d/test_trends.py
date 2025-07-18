a753436897ae45067b669cd4570984f5464a7e67
ghosh97 <supriyo.ghosh@bsc.es>
2025-07-18 19:16:45 +0200

    Update test_trends.py: refine trend instance creation and enhance run parameters for improved testing

--- tests/ocean3d/test_trends.py
+++ tests/ocean3d/test_trends.py
@@ -9,9 +9,11 @@ def test_trends():
     # Create an instance of the trends class
     trend = Trends(catalog='ci', model='FESOM',
                           exp='hpz3', source='monthly-3d',
-                          startdate='1990-01-01', enddate='1990-03-31',
-                          regrid='r200', loglevel='DEBUG')
+                          regrid='r100', loglevel='DEBUG')
     
-    trend.run(region='sss')
+    trend.run(dim_mean="lat",
+              var=['thetao', 'so'],
+              region='io')
 
     assert trend is not None, "trend instance should not be None"
+    assert trend.trend_coef.thetao.isel(level=0).mean('lon').values == pytest.approx(-0.06689141, rel=1e-1), "Coefficient of trend should be approximately zero"
