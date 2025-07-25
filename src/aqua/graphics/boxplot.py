effbf65fcf74ca50f9668926ce791da2788ea88f
silviacaprioli <silvietta.caprioli@gmail.com>
2025-07-25 12:35:11 +0200

    rm unit handling from boxplot graphics

--- src/aqua/graphics/boxplot.py
+++ src/aqua/graphics/boxplot.py
@@ -3,31 +3,9 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
+from aqua.util import to_list
 from aqua.logger import log_configure
 from .styles import ConfigStyle
-import re
-
-
-def normalize_unit_string(unit):
-    """
-    Normalize unit strings to a consistent format.
-    Args:
-        unit (str): The unit string to normalize.
-    Returns:
-        str: Normalized unit string.
-    """
-    if not unit:
-        return None
-    # Remove leading and trailing whitespace, and replace '**' with '^'
-    unit = unit.replace(' ', '').replace('**', '^')
-    # Handle common unit formats
-    unit = re.sub(r'm-2', 'm^-2', unit)
-    unit = re.sub(r'm\^\(-?2\)', 'm^-2', unit)
-    
-    if unit in ['Wm^-2', 'W/m^2', 'W/m^-2']:
-        unit = 'W/m²'
-
-    return unit
 
 def boxplot(fldmeans, model_names, variables, variable_names=None, title=None, style=None, loglevel='WARNING'):
     """
@@ -52,36 +30,21 @@ def boxplot(fldmeans, model_names, variables, variable_names=None, title=None, s
     sns.set_palette("pastel")
     fontsize = 18
 
+    fldmeans, model_names, variables = to_list(fldmeans), to_list(model_names), to_list(variables)
+
     # Map internal variable names to display names
     label_map = {
         var_name: name for var_name, name in zip(variables, variable_names)
     } if variable_names and len(variable_names) == len(variables) else {var: var for var in variables}
 
-    # check units consistency
-    unit_map = {}  
-    global_units_found = set()
-
-    for var_name in variables:
-        var = var_name[1:] if var_name.startswith('-') else var_name
-        units_for_var = set()
-
-        for model_ds in fldmeans:
-            unit = normalize_unit_string(model_ds[var].attrs.get('units')) if var in model_ds else None
-            if unit:
-                units_for_var.add(unit)
-            else:
-                logger.warning(f"No units found for variable '{var}' in a dataset")
-
-        unit_map[var_name] = units_for_var
-        if len(units_for_var) > 1:
-            logger.warning(f"Inconsistent units for variable '{var}': {units_for_var}")
-
     # Collect data
     boxplot_data = {'Variables': [], 'Values': [], 'Datasets': []}
     for model_ds, model_name in zip(fldmeans, model_names):
         for var_name in variables:
+            logger.info(f"Processing variable '{var_name}' for model '{model_name}'")
             var = var_name[1:] if var_name.startswith('-') else var_name  # Remove leading '-' if present
             if var not in model_ds:
+                logger.warning(f"Variable '{var}' not found in dataset '{model_name}'. Skipping.")
                 continue
             values = model_ds[var].values.flatten()
             if var_name.startswith('-'):
@@ -90,6 +53,22 @@ def boxplot(fldmeans, model_names, variables, variable_names=None, title=None, s
             boxplot_data['Variables'].extend([label_map[var_name]] * len(values))
             boxplot_data['Values'].extend(values)
             boxplot_data['Datasets'].extend([model_name] * len(values))
+
+    # Check units consistency
+    units_for_var = set()
+    unit_map = {}  
+    for var_name in variables:
+        var = var_name[1:] if var_name.startswith('-') else var_name
+        for model_ds in fldmeans:
+            unit = model_ds[var].attrs.get('units') if var in model_ds else None 
+            if unit:
+                units_for_var.add(unit)
+            else:
+                logger.warning(f"No units found for variable '{var}' in a dataset")
+
+        unit_map[var] = units_for_var
+        if len(units_for_var) > 1:
+            logger.warning(f"Inconsistent units for variable '{var}': {units_for_var}")
 
     # Build DataFrame
     df = pd.DataFrame(boxplot_data)
@@ -100,6 +79,10 @@ def boxplot(fldmeans, model_names, variables, variable_names=None, title=None, s
 
     if title:
         ax.set_title(title, fontsize=fontsize + 4)
+    else:
+        vars_str = ', '.join(label_map[v] for v in variables)
+        models_str = ', '.join(model_names)
+        ax.set_title(f'Boxplot of {vars_str}\nAcross Models: {models_str}', fontsize=fontsize + 2)
 
     ax.set_xlabel('Variables', fontsize=fontsize)
 
