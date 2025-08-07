ab949bfa54465d3bd6cf8697d8ee9dcf4a22edac
ghosh97 <supriyo.ghosh@bsc.es>
2025-08-07 16:51:22 +0200

    revert  return element

--- src/aqua_diagnostics/core/diagnostic.py
+++ src/aqua_diagnostics/core/diagnostic.py
@@ -226,7 +226,8 @@ class Diagnostic():
             tuple: (region, lon_limits, lat_limits)
         """
         res_dict = self._select_region(data=self.data, region=region, diagnostic=diagnostic, drop=drop)
-        return res_dict['data'], res_dict['region'], res_dict['lon_limits'], res_dict['lat_limits']
+        self.data = res_dict['data']
+        return res_dict['region'], res_dict['lon_limits'], res_dict['lat_limits']
 
     def _select_region(self, data: xr.Dataset, region: str = None, diagnostic: str = None, drop: bool = True, **kwargs):
         """
