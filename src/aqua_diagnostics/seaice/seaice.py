2df2e785fd140a4122de25a8251925f09135587d
Emanuele Tovazzi <tovaz92@gmail.com>
2025-08-20 12:10:16 +0300

    clean imports

--- src/aqua_diagnostics/seaice/seaice.py
+++ src/aqua_diagnostics/seaice/seaice.py
@@ -2,9 +2,9 @@
 import xarray as xr
 
 from aqua.diagnostics.core import Diagnostic
-from aqua.exceptions import NoDataError, NotEnoughDataError
+from aqua.exceptions import NoDataError
 from aqua.logger import log_configure, log_history
-from aqua.util import ConfigPath, OutputSaver, area_selection, to_list, merge_attrs
+from aqua.util import area_selection, to_list, merge_attrs
 from aqua.diagnostics.seaice.util import ensure_istype
 
 xr.set_options(keep_attrs=True)
