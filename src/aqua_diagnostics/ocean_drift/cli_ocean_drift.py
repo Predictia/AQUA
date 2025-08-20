42d5c2958f25d7ae6094573ac7a59ae3177ebef5
ghosh97 <supriyo.ghosh@bsc.es>
2025-08-20 16:37:16 +0200

    fix: correct argument handling in load_diagnostic_config call

--- src/aqua_diagnostics/ocean_drift/cli_ocean_drift.py
+++ src/aqua_diagnostics/ocean_drift/cli_ocean_drift.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
 
     # Load the configuration file and then merge itTimeseries with the command-line arguments,
     # overwriting the configuration file values with the command-line arguments.
-    config_dict = load_diagnostic_config(diagnostic='ocean3d', args=args,
+    config_dict = load_diagnostic_config(diagnostic='ocean3d',
                                          default_config='config_ocean_drift.yaml',
                                          loglevel=loglevel)
     config_dict = merge_config_args(config=config_dict, args=args, loglevel=loglevel)
