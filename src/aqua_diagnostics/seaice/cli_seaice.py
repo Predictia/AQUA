2eccc1c806ecd64ff5c0121bf0a7bb221e35b46f
Emanuele Tovazzi <tovaz92@gmail.com>
2025-08-26 19:53:12 +0300

    Fix: default config path which was missing in merged  https://github.com/DestinE-Climate-DT/AQUA/pull/2178

--- src/aqua_diagnostics/seaice/cli_seaice.py
+++ src/aqua_diagnostics/seaice/cli_seaice.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
     # Load the configuration file and then merge it with the command-line arguments,
     # overwriting the configuration file values with the command-line arguments.
     config_dict = load_diagnostic_config(diagnostic='seaice', config=args.config,
-                                         default_config='config/config_seaice.yaml',
+                                         default_config='config_seaice.yaml',
                                          loglevel=loglevel)
     config_dict = merge_config_args(config=config_dict, args=args, loglevel=loglevel)
     logger.info("Loaded config_dict")
