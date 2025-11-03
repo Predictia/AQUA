a619d671727608e42bbcc778bf4f426837c9315b
Jost von Hardenberg <hardenberg@gmail.com>
2025-11-03 16:47:08 +0200

    set in config file

--- src/aqua/cli/analysis.py
+++ src/aqua/cli/analysis.py
@@ -116,9 +116,11 @@ def analysis_execute(args):
 
     # Set Dask timeouts if not already defined in the environment
     if "DASK_DISTRIBUTED__COMM__TIMEOUTS__CONNECT" not in os.environ:
-        os.environ["DASK_DISTRIBUTED__COMM__TIMEOUTS__CONNECT"] = "120s"  # increase timeout (certainly needed on lumi, possibly good anyway)
+        connect_timeout = config.get('cluster', {}).get('connect_timeout', 120)
+        os.environ["DASK_DISTRIBUTED__COMM__TIMEOUTS__CONNECT"] = f"{connect_timeout}s"  # increase timeout (certainly needed on lumi, possibly good anyway)
     if "DASK_DISTRIBUTED__COMM__TIMEOUTS__TCP" not in os.environ:
-        os.environ["DASK_DISTRIBUTED__COMM__TIMEOUTS__TCP"] = "60s"  # optional, might be good
+        tcp_timeout = config.get('cluster', {}).get('tcp_timeout', 60)
+        os.environ["DASK_DISTRIBUTED__COMM__TIMEOUTS__TCP"] = f"{tcp_timeout}s"  # optional, might be good
 
     os.environ["OUTPUT"] = output_dir
     os.environ["AQUA"] = aqua_path
