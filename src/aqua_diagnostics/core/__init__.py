fcc079e343d05054fb95f3a7a676e94c7079ca66
Emanuele Tovazzi <tovaz92@gmail.com>
2025-11-05 16:47:28 +0200

    fix import

--- src/aqua_diagnostics/core/__init__.py
+++ src/aqua_diagnostics/core/__init__.py
@@ -2,10 +2,10 @@ from .diagnostic import Diagnostic
 from .time_util import start_end_dates, round_startdate, round_enddate
 from .util import template_parse_arguments, open_cluster, close_cluster
 from .util import load_diagnostic_config, merge_config_args, get_diagnostic_configpath
-from .output_saver import OutputSaver, save_figure_core
+from .output_saver import OutputSaver
 
 __all__ = ['Diagnostic',
            'start_end_dates', 'round_startdate', 'round_enddate',
            'template_parse_arguments', 'open_cluster', 'close_cluster',
            'load_diagnostic_config', 'merge_config_args', 'get_diagnostic_configpath',
-           'OutputSaver', 'save_figure_core']
+           'OutputSaver']
