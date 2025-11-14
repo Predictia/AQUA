bc738ebe1747942bbddaad6a4c5e2d0d446c16e5
Emanuele Tovazzi <tovaz92@gmail.com>
2025-11-14 19:07:51 +0100

    put back deleted doctrs

--- src/aqua/util/config.py
+++ src/aqua/util/config.py
@@ -298,6 +298,14 @@ class ConfigPath():
 
 
     def scan_catalog(self, cat, model=None, exp=None, source=None):
+        """
+        Check if the model, experiment and source are in the catalog.
+
+        Returns:
+            status (bool): True if the triplet is found
+            level (str): The level at which the triplet is failing
+            info (str): The available catalog entries at the level of the triplet
+        """
         status = False
         avail = None
         level = None
